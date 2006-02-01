f71d86013447679966237e635da986512ede1ae7
no_author <no_author>
2006-02-01 12:15:54 +0000

    Added Visual Studio project files

---
+++ trunk/proj/vs/vsscons.bat
@@ -0,0 +1,42 @@
+
+SET PATH=%PATH%;C:\Program Files\Python24
+SET PATH=%PATH%;C:\Program Files\doxygen\bin
+SET PATH=%PATH%;C:\Program Files\Graphviz\Graphviz\bin
+SET PATH=%PATH%;C:\Program Files\texmf\miktex\bin
+
+cd ../../..
+cd
+ECHO --------------------------
+ECHO vsscons %1 %2 %3 %4 %5 %6 %7 %8 %9
+ECHO --------------------------
+
+
+
+IF %1 == clean GOTO CLEAN
+
+IF %1 == build GOTO BUILD
+
+IF %1 == rebuild GOTO REBUILD
+
+GOTO END
+
+
+
+:CLEAN
+CALL scons -c %2 %3 %4 %5 %6 %7 %8 %9
+GOTO END
+
+:BUILD
+echo scons %2 %3 %4 %5 %6 %7 %8 %9
+CALL scons %2 %3 %4 %5 %6 %7 %8 %9
+GOTO END
+
+
+:REBUILD
+CALL scons -c %2 %3 %4 %5 %6 %7 %8 %9
+GOTO BUILD
+
+
+
+:END
+
