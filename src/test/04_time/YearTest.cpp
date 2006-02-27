93f5efd2cf6a59dc217063e3fbe8b88402b4dcbb
no_author <no_author>
2006-02-27 18:55:07 +0000

    tcp module and time module

---
+++ src/test/04_time/YearTest.cpp
@@ -0,0 +1,58 @@
+#include "YearTest.h"
+#include "Year.h"
+
+#include <iostream>
+
+
+namespace synthese
+{
+namespace time
+{
+
+
+
+  void 
+  YearTest::setUp () 
+  {
+
+  }
+
+
+  void 
+  YearTest::tearDown() 
+  {
+
+  } 
+
+
+  void
+  YearTest::testLeapYears ()
+  {
+    {
+      Year y (2000); // divisible by 400
+      CPPUNIT_ASSERT (y.isLeapYear () == true);
+      CPPUNIT_ASSERT_EQUAL (366, y.getDaysCount ());
+    }
+    {
+      Year y (1000); // not divisible by 400 but divisible by 100
+      CPPUNIT_ASSERT (y.isLeapYear () == false);
+      CPPUNIT_ASSERT_EQUAL (365, y.getDaysCount ());
+    }
+    {
+      Year y (2004); // not divisible by 400 nor 100, but divisible by 4
+      CPPUNIT_ASSERT (y.isLeapYear () == true);
+      CPPUNIT_ASSERT_EQUAL (366, y.getDaysCount ());
+    }
+    {
+      Year y (2006); // not divisible by 400 nor 100, but divisible by 4
+      CPPUNIT_ASSERT (y.isLeapYear () == false);
+      CPPUNIT_ASSERT_EQUAL (365, y.getDaysCount ());
+    }
+
+
+  }
+
+
+}
+}
+
