0dbadb4cdaa13cf1326056a090c7c33c25467c74
no_author <no_author>
2006-04-18 18:18:40 +0000

    html map renderer

---
+++ src/main/39_carto/Renderer.cpp
@@ -0,0 +1,22 @@
+#include "Renderer.h"
+
+namespace synthese
+{
+
+namespace carto
+{
+
+
+Renderer::Renderer(const RenderingConfig& config)
+    : _config (config)
+{
+}
+
+
+Renderer::~Renderer()
+{
+}
+
+
+}
+}
