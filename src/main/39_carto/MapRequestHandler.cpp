b0a28f4337793a9e6b45117b1d50ec974f6cae7f
no_author <no_author>
2006-04-25 20:09:05 +0000

    MapInfo

--- src/main/39_carto/MapRequestHandler.cpp
+++ src/main/39_carto/MapRequestHandler.cpp
@@ -8,7 +8,7 @@
 #include <stdlib.h>
 
 #include <boost/filesystem/operations.hpp>
-#include <boost/date_time/microsec_time_clock.hpp>
+// not needed under linux #include <boost/date_time/microsec_time_clock.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 
 #include "01_util/XmlParser.h"
@@ -23,8 +23,11 @@
 
 #include "39_carto/Map.h"
 #include "39_carto/RenderingConfig.h"
+
 #include "39_carto/PostscriptRenderer.h"
 #include "39_carto/HtmlMapRenderer.h"
+#include "39_carto/MapInfoRenderer.h"
+
 #include "40_carto_ls_xml/MapLS.h"
 
 #include "01_util/Log.h"
@@ -62,6 +65,7 @@ const int MapRequestHandler::REQUEST_MODE_HTTP (2);
 const std::string MapRequestHandler::REQUEST_OUTPUT_PS ("ps");
 const std::string MapRequestHandler::REQUEST_OUTPUT_PNG ("png");
 const std::string MapRequestHandler::REQUEST_OUTPUT_HTML ("html");
+const std::string MapRequestHandler::REQUEST_OUTPUT_MAPINFO ("mapinfo");
 
 
 MapRequestHandler::MapRequestHandler ()
@@ -99,11 +103,12 @@ MapRequestHandler::handleRequest (const synthese::server::Request& request,
     std::string output = request.getParameter (OUTPUT_PARAMETER);
     if ((output != REQUEST_OUTPUT_PS) && 
 	(output != REQUEST_OUTPUT_PNG) &&
-	(output != REQUEST_OUTPUT_HTML))
+	(output != REQUEST_OUTPUT_HTML) &&
+	(output != REQUEST_OUTPUT_MAPINFO))
     {
         throw synthese::server::RequestException ("Invalid map output type " + output);
     }
-    
+
     XMLNode envNode = XMLNode::parseString (request.getParameter (ENVIRONMENT_PARAMETER).c_str (),
 					    synthese::envlsxml::EnvironmentLS::ENVIRONMENT_TAG.c_str ());
 
@@ -124,58 +129,26 @@ MapRequestHandler::handleRequest (const synthese::server::Request& request,
     // Generate an id for the map file based on current time
     ptime timems = boost::date_time::microsec_clock<ptime>::local_time ();
 
-    std::string resultFilename = "map_" + to_iso_string (timems) + ".ps";
-    const boost::filesystem::path psFile (tempDir / resultFilename);
+    std::string filePrefix = "map_" + to_iso_string (timems);
 
-    // Create the postscript canvas for output
-    std::ofstream of (psFile.string ().c_str ());
-
-    // ---- Render postscript file ----
+    std::string resultFilename = "";
     RenderingConfig conf;
-    synthese::carto::PostscriptRenderer psRenderer (conf, of);
-    psRenderer.render (*map);
 
-    of.close ();
-
-    if ((output == REQUEST_OUTPUT_PNG) || (output == REQUEST_OUTPUT_HTML))
+    if (output == REQUEST_OUTPUT_PS) 
     {
-	// ---- Render PNG file ----
-	resultFilename = "map_" + to_iso_string (timems) + ".png";
-	const boost::filesystem::path pngFile (tempDir / resultFilename);
-
-	// Convert the ps file to png with ghostscript
-	std::stringstream gscmd;
-	gscmd << GHOSTSCRIPT_BIN << " -q -dSAFER -dBATCH -dNOPAUSE -sDEVICE=png16m -dTextAlphaBits=4 -dGraphicsAlphaBits=4 -g" 
-	      << map->getWidth () << "x" << map->getHeight () 
-	      << " -sOutputFile=" << pngFile.string () << " " << psFile.string ();
-	
-	Log::GetInstance ().debug (gscmd.str ());
-	
-	int ret = system (gscmd.str ().c_str ());
-	
-	if (ret != 0)
-	{
-	    throw synthese::util::Exception ("Error executing GhostScript (gs executable in path ?)");
-	}
-	
-	boost::filesystem::remove (psFile);
-
-	if (output == REQUEST_OUTPUT_HTML)
-	{
-	    std::string pngFilename (resultFilename);
-	    resultFilename = "map_" + to_iso_string (timems) + ".html";
-	    const boost::filesystem::path htmlFile (tempDir / resultFilename);
-
-	    std::ofstream ofhtml (htmlFile.string ().c_str ());
-	    synthese::carto::HtmlMapRenderer hmRenderer (conf, map->getUrlPattern (), pngFilename, ofhtml);
-	    hmRenderer.render (*map);
-	    ofhtml.close ();
-
-	} 
-	else 
-	{
-	}
-
+	resultFilename = renderPsFile (tempDir, filePrefix, *map, conf);
+    } 
+    else if (output == REQUEST_OUTPUT_PNG) 
+    {
+	resultFilename = renderPngFile (tempDir, filePrefix, *map, conf);
+    }
+    else if (output == REQUEST_OUTPUT_HTML) 
+    {
+	resultFilename = renderHtmlFile (tempDir, filePrefix, *map, conf);
+    }
+    else if (output == REQUEST_OUTPUT_MAPINFO) 
+    {
+	resultFilename = renderMapinfoFile (tempDir, filePrefix, *map, conf);
     }
 
 
@@ -229,6 +202,117 @@ MapRequestHandler::handleRequest (const synthese::server::Request& request,
 
 
 
+
+
+
+std::string 
+MapRequestHandler::renderPsFile (const boost::filesystem::path& tempDir, 
+				 const std::string filenamePrefix,
+				 Map& map,
+				 const RenderingConfig& conf) const
+{
+    std::string resultFilename = filenamePrefix + ".ps";
+    const boost::filesystem::path psFile (tempDir / resultFilename);
+
+    // Create the postscript canvas for output
+    std::ofstream of (psFile.string ().c_str ());
+
+    // ---- Render postscript file ----
+    synthese::carto::PostscriptRenderer psRenderer (conf, of);
+    psRenderer.render (map);
+
+    of.close ();
+
+    return resultFilename;
+}
+
+
+
+
+
+std::string 
+MapRequestHandler::renderPngFile (const boost::filesystem::path& tempDir, 
+				  const std::string filenamePrefix,
+				  Map& map,
+				  const RenderingConfig& conf) const
+{
+    renderPsFile (tempDir, filenamePrefix, map, conf);
+
+    std::string psFilename = filenamePrefix + ".ps";
+    const boost::filesystem::path psFile (tempDir / psFilename);
+
+    std::string resultFilename (filenamePrefix + ".png");
+    const boost::filesystem::path pngFile (tempDir / resultFilename);
+    
+    // Convert the ps file to png with ghostscript
+    std::stringstream gscmd;
+    gscmd << GHOSTSCRIPT_BIN << " -q -dSAFER -dBATCH -dNOPAUSE -sDEVICE=png16m -dTextAlphaBits=4 -dGraphicsAlphaBits=4 -g" 
+	  << map.getWidth () << "x" << map.getHeight () 
+	  << " -sOutputFile=" << pngFile.string () << " " << psFile.string ();
+    
+    Log::GetInstance ().debug (gscmd.str ());
+    
+    int ret = system (gscmd.str ().c_str ());
+    
+    boost::filesystem::remove (psFile);
+
+    if (ret != 0)
+    {
+	throw synthese::util::Exception ("Error executing GhostScript (gs executable in path ?)");
+    }
+
+    return resultFilename;
+}
+
+
+
+
+
+    
+std::string 
+MapRequestHandler::renderHtmlFile (const boost::filesystem::path& tempDir, 
+				   const std::string filenamePrefix,
+				   Map& map,
+				   const RenderingConfig& conf) const
+{
+    renderPngFile (tempDir, filenamePrefix, map, conf);
+
+    std::string pngFilename = filenamePrefix + ".png";
+
+    std::string resultFilename (filenamePrefix + ".html");
+    const boost::filesystem::path htmlFile (tempDir / resultFilename);
+    
+    std::ofstream ofhtml (htmlFile.string ().c_str ());
+    synthese::carto::HtmlMapRenderer hmRenderer (conf, map.getUrlPattern (), pngFilename, ofhtml);
+    hmRenderer.render (map);
+    ofhtml.close ();
+    
+    return resultFilename;
+}
+
+
+
+
+
+    
+std::string 
+MapRequestHandler::renderMapinfoFile (const boost::filesystem::path& tempDir, 
+				      const std::string filenamePrefix,
+				      Map& map,
+				      const RenderingConfig& conf) const
+{
+    std::string resultFilename (filenamePrefix + ".zip");
+    boost::filesystem::path zipOutput (tempDir / resultFilename);
+
+    synthese::carto::MapInfoRenderer mapinfoRenderer (conf, zipOutput);
+    mapinfoRenderer.render (map);
+
+    return resultFilename;
+}
+
+
+
+    
 
 
 
