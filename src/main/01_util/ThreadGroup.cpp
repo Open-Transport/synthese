ba84848cc21294e778118fb9a1b63be2ebcf4faa
no_author <no_author>
2006-06-22 01:36:28 +0000

    Minimalistic ThreadGroup implementation

---
+++ src/main/01_util/ThreadGroup.cpp
@@ -0,0 +1,78 @@
+#include "ThreadGroup.h"
+
+
+
+namespace synthese
+{
+namespace util
+{
+
+
+
+ThreadGroup::ThreadGroup () 
+{
+    
+}
+    
+
+
+ThreadGroup::~ThreadGroup () 
+{
+}
+    
+
+
+void 
+ThreadGroup::addThread (const Thread& thread)
+{
+    _threads.push_back (thread);
+}
+
+
+
+
+
+void 
+ThreadGroup::waitForAllReady () const
+{
+    waitAllInState (Thread::READY);
+}
+
+
+
+void 
+ThreadGroup::waitForAllStopped () const
+{
+    waitAllInState (Thread::STOPPED);
+}
+
+
+
+void 
+ThreadGroup::waitAllInState (const Thread::ThreadState& state) const
+{
+    while (1)
+    {
+	bool allInRightState = true;
+	for (std::vector<Thread>::const_iterator it = _threads.begin ();
+	     it != _threads.end (); ++it)
+	{
+	    if (it->getState () != state)
+	    {
+		allInRightState = false;
+		break;
+	    }
+	}
+	if (allInRightState) break;
+	Thread::Sleep (5);
+    }
+}
+
+
+
+
+
+
+
+}
+}
---
+++ src/main/01_util/ThreadGroup.h
@@ -0,0 +1,64 @@
+#ifndef SYNTHESE_UTIL_THREADGROUP_H
+#define SYNTHESE_UTIL_THREADGROUP_H
+
+
+
+#include "module.h"
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <boost/shared_ptr.hpp>
+
+
+#include "Thread.h"
+
+
+
+namespace synthese
+{
+namespace util
+{
+
+
+
+
+/** Group of threads.
+
+@ingroup m01
+*/
+class ThreadGroup
+{
+ public:
+
+
+ private:
+
+    std::vector<Thread> _threads;  //!< Threads in group.
+
+ protected:
+
+ public:
+
+    ThreadGroup ();
+    ~ThreadGroup ();
+
+    void addThread (const Thread& thread);
+
+    void waitForAllReady () const;
+    void waitForAllStopped () const;
+
+
+ private:
+
+    void waitAllInState (const Thread::ThreadState& state) const;
+    
+};
+
+
+
+
+}
+
+}
+#endif
