c6b9503ec469d009bfb704dc5b170209fb063401
no_author <no_author>
2006-07-19 17:30:52 +0000

    ServiceDateTableSync

---
+++ src/main/17_env_ls_sql/ServiceDateTableSync.cpp
@@ -0,0 +1,112 @@
+#include "ServiceDateTableSync.h"
+
+#include "01_util/Conversion.h"
+#include "01_util/UId.h"
+#include "02_db/SQLiteResult.h"
+#include "02_db/SQLiteThreadExec.h"
+#include "04_time/Date.h"
+
+#include "15_env/Service.h"
+
+#include <sqlite/sqlite3.h>
+
+#include "assert.h"
+
+
+
+using synthese::util::Conversion;
+using synthese::db::SQLiteResult;
+using synthese::time::Date;
+
+using synthese::env::Environment;
+using synthese::env::Service;
+
+namespace synthese
+{
+namespace envlssql
+{
+
+
+
+ServiceDateTableSync::ServiceDateTableSync (Environment::Registry& environments)
+: ComponentTableSync (SERVICEDATES_TABLE_NAME, environments)
+{
+    addTableColumn (SERVICEDATES_TABLE_COL_SERVICEID, "INTEGER", false);
+    addTableColumn (SERVICEDATES_TABLE_COL_DATE , "DATE", false);
+}
+
+
+
+ServiceDateTableSync::~ServiceDateTableSync ()
+{
+
+}
+
+
+
+
+void 
+ServiceDateTableSync::doAdd (const synthese::db::SQLiteResult& rows, int rowIndex,
+			     synthese::env::Environment& environment)
+{
+    updateServiceCalendar (rows, rowIndex, environment, true);
+}
+
+
+
+void 
+ServiceDateTableSync::doReplace (const synthese::db::SQLiteResult& rows, int rowIndex,
+			  synthese::env::Environment& environment)
+{
+    // Cannot happen (trigger).
+    assert (false);
+}
+
+
+
+void 
+ServiceDateTableSync::doRemove (const synthese::db::SQLiteResult& rows, int rowIndex,
+			 synthese::env::Environment& environment)
+{
+    updateServiceCalendar (rows, rowIndex, environment, false);
+}
+
+
+void 
+ServiceDateTableSync::updateServiceCalendar (const synthese::db::SQLiteResult& rows, int rowIndex,
+					     synthese::env::Environment& environment,
+					     bool marked) 
+{
+    // Get the corresponding calendar
+    uid serviceId = Conversion::ToLongLong (rows.getColumn (rowIndex, SERVICEDATES_TABLE_COL_SERVICEID));
+    int tableId = synthese::util::decodeTableId (serviceId);
+
+    Service* service = 0;
+    if (serviceId == ParseTableId (SCHEDULEDSERVICES_TABLE_NAME))
+    {
+	service = environment.getScheduledServices ().get (serviceId);
+    }
+    else if (serviceId == ParseTableId (CONTINUOUSSERVICES_TABLE_NAME ))
+    {
+	service = environment.getContinuousServices ().get (serviceId);
+    }
+    if (service == 0) return;
+
+    // Mark the date in service calendar
+    service->getCalendar ().mark (
+	Date::FromSQLDate (rows.getColumn (rowIndex, SERVICEDATES_TABLE_COL_DATE)), marked);
+    
+}
+    
+
+
+
+
+
+
+
+
+
+}
+
+}
---
+++ src/main/17_env_ls_sql/ServiceDateTableSync.h
@@ -0,0 +1,68 @@
+#ifndef SYNTHESE_ENVLSSQL_SERVICEDATETABLESYNC_H
+#define SYNTHESE_ENVLSSQL_SERVICEDATETABLESYNC_H
+
+
+#include <string>
+#include <iostream>
+
+#include "ComponentTableSync.h"
+
+
+
+namespace synthese
+{
+
+namespace env
+{
+
+}
+
+
+namespace envlssql
+{
+
+
+/** 
+
+ServiceDate SQLite table synchronizer.
+
+@ingroup m17
+*/
+
+class ServiceDateTableSync : public ComponentTableSync
+{
+ private:
+
+ public:
+
+    ServiceDateTableSync (synthese::env::Environment::Registry& environments);
+    ~ServiceDateTableSync ();
+
+ protected:
+
+    void doAdd (const synthese::db::SQLiteResult& rows, int rowIndex,
+		synthese::env::Environment& target);
+
+    void doReplace (const synthese::db::SQLiteResult& rows, int rowIndex,
+		    synthese::env::Environment& target);
+
+    void doRemove (const synthese::db::SQLiteResult& rows, int rowIndex,
+		   synthese::env::Environment& target);
+
+
+ private:
+
+    void updateServiceCalendar (const synthese::db::SQLiteResult& rows, int rowIndex,
+				synthese::env::Environment& environment,
+				bool marked);
+
+
+};
+
+
+
+
+}
+
+}
+#endif
