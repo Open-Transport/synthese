e9fa2bec42b1ba224c2e7c3d034e7c8282064d56
no_author <no_author>
2006-08-01 14:36:32 +0000

    Clean up

--- run_debug.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-./build/win32/Debug/main/70_server/70_server --datadir=c:/dev/synthese-data --loglevel=1 --port=3591 --threads=2 --httptempdir=c:/workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/webapps/static/images --httptempurl=http://localhost:8080/static/images
-
-#C:\workspace\.metadata\.plugins\org.eclipse.wst.server.core\tmp0\webapps\static
-#./build/win32/Debug/main/70_server/70_server --help
-
