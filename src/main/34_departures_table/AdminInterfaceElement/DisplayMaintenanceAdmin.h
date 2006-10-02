2f78ac62e476f1ceb864433ba2c3e8ea14e8feeb
no_author <no_author>
2006-10-02 02:30:43 +0000

    doc doc doc display maintenance

---
+++ src/main/34_departures_table/AdminInterfaceElement/DisplayMaintenanceAdmin.h
@@ -0,0 +1,65 @@
+
+#ifndef SYNTHESE_DISPLAY_MAINTENANCE_ADMIN_H
+#define SYNTHESE_DISPLAY_MAINTENANCE_ADMIN_H
+
+#include "11_interfaces/AdminInterfaceElement.h"
+
+namespace synthese
+{
+	namespace interfaces
+	{
+
+		/** Ecran de supervision d'un afficheur.
+			@ingroup m34
+
+			@image html cap_admin_display_maint.png
+			@image latex cap_admin_display_maint.png "Maquette de l'écran de supervision d'un afficheur" width=14cm
+
+			<i>Titre de la fenêtre</i> : SYNTHESE Admin - Afficheurs - [Lieu logique] - [Lieu physique] - [Complément de précision] - Maintenance
+
+			<i>Barre de navigation</i> :
+ 				- Lien vers @ref synthese::interface::HomeAdmin
+				- Lien vers @ref synthese::interface::DisplaySearchAdmin
+				- Nom du lieu logique auquel appartient l'afficheur, lien vers @ref synthese::interface::DisplayLocationsAdmin sur le lieu logique de l'afficheur
+				- Nom du lieu physique auquel appartient l'afficheur
+				- Complément de précision sur l'emplacement de l'afficheur (non affiché si le complément est vide)
+				- Texte <tt>Maintenance</tt>
+
+			<i>Zone de contenus</i> : 
+				-# <b>Formulaire de paramétrage</b> :
+					-# <tt>Nombre de contrôles matériels par jour</tt> : Liste déroulante définissant le nombre de contrôles matériels devant être faits chaque jour (valeurs possibles 1 à 48), afin de déclencher une alerte en cas d'absence de contrôle au delà d'une certaine durée
+					-# <tt>Afficheur déclaré en service</tt> : Choix OUI/NON permettant de déclarer l'afficheur en service ou non :
+						- OUI : l'afficheur est considéré comme théoriquement en service
+						- NON : l'afficheur est considéré comme hors service
+				-# Zone <b>Contrôle de cohérence des données</b> : le contrôle de cohérence de données est effectué en temps réel lors de l'affichage de la page.
+					-# Etiquette colorée : Indique le résultat du dernier contrôle suivi de la durée depuis laquelle le résultat est identique :
+						- <tt>Warning</tt> (orange) si le contenu de l'afficheur est vide car aucune ligne ne fréquente l'afficheur (si l'afficheur est vide durant la totalité du délai de validité de la base SYNTHESE)
+						- <tt>Error</tt> (rouge) si l'afficheur est positionné sur un emplacement qui n'existe plus (ex : Arrêt physique supprimé extérieurement)
+					-# <tt>Motif de l'alerte</tt> : Texte explicatif sur l'erreur relevée
+					-# <tt>Date du dernier contrôle positif</tt> : Indique la dernière date connue de contrôle de cohérence positif
+				-# Zone <b>Contrôle du matériel</b> : le contrôle du matériel consiste en la lecture du journal alimenté par ailleurs par les postes clients.
+					-# Etiquette colorée : Indique le résultat du dernier contrôle, suivi de la durée depuis laquelle le résultat est identique :
+						- <tt>Warning</tt> (orange) si aucun contrôle n'est présent dans le journal depuis une période comprise entre 150% et 500% de la durée présupposée entre deux contrôles (paramètre <tt>Nombre de contrôles par jour</tt>). Cette situation indique que le client n'a pas mis à jour le journal, sans pour autant pouvoir assurer que le terminal est bel est bien hors de fonctionnement.
+						- <tt>Warning</tt> (orange) si un contrôle est présent dans le journal, et relève une avarie n'ayant pas d'impact direct sur l'affichage, mais devant toutefois faire l'objet d'un traitement
+						- <tt>Error</tt> (rouge) si un contrôle est présent dans le journal, et relève une avarie ayant conduit le terminal à s'éteindre
+						- <tt>Error</tt> (rouge) si aucun contrôle n'est présent dans le journal depuis une période supérieure à 500% de la durée présupposée entre deux contrôles (paramètre <tt>Nombre de contrôles par jour</tt>).
+						- <tt>Error</tt> (rouge) si le terminal est déclaré hors service au niveau du présent écran.
+					-# <tt>Date du dernier contrôle positif</tt> : Indique la dernière date connue de contrôle matériel positif
+					-# <tt>Motif de l'alerte</tt> : Texte explicatif sur l'erreur relevée
+					
+			<i>Sécurité</i> :
+				- L'habilitation Lecture sur le module tableaux départs à la rubrique maintenance est nécessaire pour visualiser la page en consultation
+				- L'habilitation Ecriture sur le module tableaux départs à la rubrique maintenance est nécessaire pour modifier les paramètres présents sur la page
+
+			<i>Journal</i> : Les actions suivantes génèrent une entrée dans le journal de maintenance des afficheurs :
+				- Modification de paramètre
+				- Détection d'une erreur de cohérence de données lors de l'affichage de la page
+		*/
+		class DisplayMaintenanceAdmin : public AdminInterfaceElement
+		{
+
+		};
+	}
+}
+
+#endif
