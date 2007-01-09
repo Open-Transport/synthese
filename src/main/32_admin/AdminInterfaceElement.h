9583b576728b9e6538eaab69e7c440733b88ebee
Hugues Romain <hromain@rcsmobility.com>
2007-01-09 13:52:55 +0000

    Some GNU GPL insertions, just for fun...

--- src/main/32_admin/AdminInterfaceElement.h
+++ src/main/32_admin/AdminInterfaceElement.h
@@ -1,11 +1,31 @@
+
+/** AdminInterfaceElement class header.
+	@file AdminInterfaceElement.h
+
+	This file belongs to the SYNTHESE project (public transportation specialized software)
+	Copyright (C) 2002 Hugues Romain - RCS <contact@reseaux-conseil.com>
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
 #ifndef SYNTHESE_INTERFACES_ADMIN_INTERFACE_ELEMENT_H
 #define SYNTHESE_INTERFACES_ADMIN_INTERFACE_ELEMENT_H
 
 #include <string>
 
 #include "01_util/Factorable.h"
-
-#include "11_interfaces/DisplayableElement.h"
 
 namespace synthese
 {
@@ -23,7 +43,7 @@ namespace synthese
 				- un administrateur a accès à tous les composants d'administration.
 			
 		*/
-		class AdminInterfaceElement : public interfaces::DisplayableElement, public util::Factorable
+		class AdminInterfaceElement : public util::Factorable
 		{
 		public:
 			typedef enum { EVER_DISPLAYED, DISPLAYED_IF_CURRENT, NEVER_DISPLAYED } DisplayMode;
