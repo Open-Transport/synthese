721484ce08dea9a1100029dd5006532d815b65db
Hugues Romain <hromain@rcsmobility.com>
2007-03-14 23:56:27 +0000

    widows port of synthese didn't launch because of a *$¤^ù% of posix path in the source code of ServerConfig...... Thanx Marc. In the Windows version the default temp dir is a good choice : the declared temporary directory.

--- src/main/30_server/ServerConfig.cpp
+++ src/main/30_server/ServerConfig.cpp
@@ -1,4 +1,25 @@
 
+/** ServerConfig class implementation.
+	@file ServerConfig.cpp
+
+	This file belongs to the SYNTHESE project (public transportation specialized software)
+	Copyright (C) 2002 Hugues Romain - RCS <contact@reseaux-conseil.com>
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
 #include <boost/filesystem/operations.hpp>
 
 #include "01_util/Exception.h"
@@ -6,6 +27,14 @@
 
 #include "30_server/ServerConfigTableSync.h"
 #include "30_server/ServerConfig.h"
+
+#ifdef UNIX
+	#define DEFAULT_TEMP_DIR "/var/www/tmp"
+#endif
+#ifdef WIN32
+#define DEFAULT_TEMP_DIR getenv("TEMP"), boost::filesystem::native
+#endif
+
 
 using synthese::util::Log;
 
@@ -22,7 +51,7 @@ ServerConfig::ServerConfig ()
     , _nbThreads (5)
     , _dataDir (".")
     , _tempDir (".")
-    , _httpTempDir ("/var/www/tmp")
+    , _httpTempDir (DEFAULT_TEMP_DIR)
     , _httpTempUrl ("http://localhost/tmp")
 {
     
