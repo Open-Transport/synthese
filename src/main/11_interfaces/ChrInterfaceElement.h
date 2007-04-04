f38d30d90a6cd676c44a01c232ac156de2c4051a
Marc Jambert <mjambert@gmail.com>
2007-04-04 07:43:20 +0000

    ChrInterfaceElement

---
+++ src/main/11_interfaces/ChrInterfaceElement.h
@@ -0,0 +1,58 @@
+
+/** ChrInterfaceElement class header.
+	@file ChrInterfaceElement.h
+
+	This file belongs to the SYNTHESE project (public transportation specialized software)
+	Copyright (C) 2002 Hugues Romain - RCS <contact@reseaux-conseil.com>
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#ifndef SYNTHESE_ChrInterfaceElement_H__
+#define SYNTHESE_ChrInterfaceElement_H__
+
+#include "11_interfaces/ValueInterfaceElement.h"
+
+namespace synthese
+{
+	namespace interfaces
+	{
+
+		class ChrInterfaceElement : public interfaces::ValueInterfaceElement
+		{
+			ValueInterfaceElement* _asciiCode;
+
+		public:
+			/** Parameters parser.
+				The parser copies the ValueElementList as is.
+			*/
+			void storeParameters (ValueElementList& vel);
+			std::string getValue (const ParametersVector&, 
+                                              interfaces::VariablesMap& variables, 
+                                              const void* object = NULL, 
+                                              const server::Request* request = NULL ) const;
+                
+                /* std::string display (std::ostream& stream, 
+                                             const interfaces::ParametersVector& parameters, 
+                                             interfaces::VariablesMap& variables, 
+                                             const void* object = NULL, 
+                                             const server::Request* request = NULL) const; */
+                
+			~ChrInterfaceElement();
+		};
+	}
+}
+
+#endif // SYNTHESE_ChrInterfaceElement_H__
