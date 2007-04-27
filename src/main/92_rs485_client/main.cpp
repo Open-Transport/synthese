6a01574f90d04f34eecfc8375e9041301d694adf
build <build>
2007-04-27 13:45:59 +0000

    added test on fake COM

--- src/main/92_rs485_client/main.cpp
+++ src/main/92_rs485_client/main.cpp
@@ -74,6 +74,13 @@ int main(int argc, char* argv[])
     server = argv[1];
     int port = atoi (argv[2]);
     comm = argv[3];
+    bool useCOM (true);
+    if ( (comm.substr (0, 3) == "COM") ||
+	 (comm.substr (0, 3) == "com") )
+    {
+	useCOM = true;
+    }
+
 
     memset(codes, MAX_CLIENTS, sizeof(char*));
     for(nbclients=0; nbclients<argc-4; nbclients++) {
@@ -81,30 +88,32 @@ int main(int argc, char* argv[])
 	outdate[nbclients] = 0;
     }
 
-    hCom = CreateFile( comm,
-		       GENERIC_READ | GENERIC_WRITE,
-		       0,    // must be opened with exclusive-access
-		       NULL, // no security attributes
-		       OPEN_EXISTING, // must use OPEN_EXISTING
-		       0,    // not overlapped I/O
-		       NULL  // hTemplate must be NULL for comm devices
-	);
-    
-    if (hCom == INVALID_HANDLE_VALUE) 
+    if (useCOM)
     {
-	Log::GetInstance ().fatal ("Error while creating comm file " + Conversion::ToString (GetLastError()));
-	exit(1);
-    }
-    
-    // Build on the current configuration, and skip setting the size
-    // of the input and output buffers with SetupComm.
-    fSuccess = GetCommState(hCom, &dcb);
-    if (!fSuccess) 
-    {
-	// Handle the error.
-	Log::GetInstance ().fatal ("Error while getting comm state " + Conversion::ToString (GetLastError()));
-	exit(2);
-    }
+	hCom = CreateFile( comm,
+			   GENERIC_READ | GENERIC_WRITE,
+			   0,    // must be opened with exclusive-access
+			   NULL, // no security attributes
+			   OPEN_EXISTING, // must use OPEN_EXISTING
+			   0,    // not overlapped I/O
+			   NULL  // hTemplate must be NULL for comm devices
+	    );
+	
+	if (hCom == INVALID_HANDLE_VALUE) 
+	{
+	    Log::GetInstance ().fatal ("Error while creating comm file " + Conversion::ToString (GetLastError()));
+	    exit(1);
+	}
+	
+	// Build on the current configuration, and skip setting the size
+	// of the input and output buffers with SetupComm.
+	fSuccess = GetCommState(hCom, &dcb);
+	if (!fSuccess) 
+	{
+	    // Handle the error.
+	    Log::GetInstance ().fatal ("Error while getting comm state " + Conversion::ToString (GetLastError()));
+	    exit(2);
+	}
   
 	COMMTIMEOUTS lpct;
 	lpct.ReadIntervalTimeout = 200;
@@ -113,23 +122,24 @@ int main(int argc, char* argv[])
 	lpct.WriteTotalTimeoutMultiplier = 200;
 	lpct.WriteTotalTimeoutConstant = 0;
 
-    fSuccess = SetCommTimeouts (hCom, &lpct);
+	fSuccess = SetCommTimeouts (hCom, &lpct);
 
-    // Fill in DCB: 57,600 bps, 8 data bits, no parity, and 1 stop bit.
-    dcb.BaudRate = CBR_9600;      // set the baud rate
-    dcb.ByteSize = 8;             // data size, xmit, and rcv
-    dcb.Parity = NOPARITY;      // parity bit
-    dcb.StopBits = ONESTOPBIT;    // one stop bit
-    fSuccess = SetCommState(hCom, &dcb);
-    if (!fSuccess) 
-    {
-	// Handle the error.
-	Log::GetInstance ().fatal ("SetCommState failed with error " + Conversion::ToString (GetLastError()));
-	exit(3);
+	// Fill in DCB: 57,600 bps, 8 data bits, no parity, and 1 stop bit.
+	dcb.BaudRate = CBR_9600;      // set the baud rate
+	dcb.ByteSize = 8;             // data size, xmit, and rcv
+	dcb.Parity = NOPARITY;      // parity bit
+	dcb.StopBits = ONESTOPBIT;    // one stop bit
+	fSuccess = SetCommState(hCom, &dcb);
+	if (!fSuccess) 
+	{
+	    // Handle the error.
+	    Log::GetInstance ().fatal ("SetCommState failed with error " + Conversion::ToString (GetLastError()));
+	    exit(3);
+	}
+	
+	Log::GetInstance ().info ("Serial port " + std::string (comm) + " successfully reconfigured.");
     }
- 
-    Log::GetInstance ().info ("Serial port " + std::string (comm) + " successfully reconfigured.");
-
+	    
 
     char buf[MAX_QUERY_SIZE];
     DWORD readComm = 0;
@@ -155,7 +165,7 @@ int main(int argc, char* argv[])
                   //    fichier << "erreur reinit port com" << endl;
                   
                   Log::GetInstance ().info ("Connecting " + std::string (server) 
-                                                          + std::string (":") + Conversion::ToString (port));
+					    + std::string (":") + Conversion::ToString (port));
                   
                   int timeout = 2; // 2 seconds timeout
 		  {
@@ -186,82 +196,88 @@ int main(int argc, char* argv[])
 
 		  if (received.empty ())
 		  {
-			  Log::GetInstance ().warn ("Received empty command ! Maybe a server crash ?");
-              outdate[client] = hms->tm_min;
+		      Log::GetInstance ().warn ("Received empty command ! Maybe a server crash ?");
+		      outdate[client] = hms->tm_min;
 		  } 
 		  else
 		  {
-          Log::GetInstance ().info ("Received command : " + received);
-
-			  time(&now);
-			  hms = localtime(&now);
-	                          
+		      Log::GetInstance ().info ("Received command : " + received);
+		      
+		      time(&now);
+		      hms = localtime(&now);
+	        
+		      if (useCOM)
+		      {
 			  for (char* bufptr=buf ; *bufptr ; bufptr++)
 			  {
-				  fSuccess = TransmitCommChar (hCom, *bufptr);
+			      fSuccess = TransmitCommChar (hCom, *bufptr);
 			  }
-	              
-			  outdate[client] = hms->tm_min;
-	       
+		      }
+			  
+		      outdate[client] = hms->tm_min;
+		      
+		      if (useCOM)
+		      {
 			  // Read is updated with the number of bytes read
 			  fSuccess = ReadFile (hCom, buf, STATUS_MESSAGE_SIZE, &readComm, NULL); 
 			  if (!fSuccess)
 			  {
-					Log::GetInstance ().error ("Error while reading status ! Returned status message will be empty or incomplete."); 
+			      Log::GetInstance ().error ("Error while reading status ! Returned status message will be empty or incomplete."); 
 			  }
+		      }
 
-			  // Create status message to be sent back to server. 
-			  // It will be sent the next time that the client will ask for update.
-			  std::stringstream status;
-			  for (int i=0; i<STATUS_MESSAGE_SIZE; ++i)
+		      // Create status message to be sent back to server. 
+		      // It will be sent the next time that the client will ask for update.
+		      std::stringstream status;
+		      for (int i=0; i<STATUS_MESSAGE_SIZE; ++i)
+		      {
+			  status << std::hex << std::setw (2) << std::setfill ('0') << ((int) buf[i]);
+		      }
+		      
+		      {
+			  Log::GetInstance ().info ("Connecting " + std::string (server) 
+						    + std::string (":") + Conversion::ToString (port));
+			  TcpClientSocket clientSock (server, port, timeout);
+			  
+			  while (clientSock.isConnected () == false)
 			  {
-				  status << std::hex << std::setw (2) << std::setfill ('0') << ((int) buf[i]);
-			  }
-
-			  {
-				  Log::GetInstance ().info ("Connecting " + std::string (server) 
-														  + std::string (":") + Conversion::ToString (port));
-				  TcpClientSocket clientSock (server, port, timeout);
-			      
-				  while (clientSock.isConnected () == false)
-				  {
-							  clientSock.tryToConnect ();
-							  Thread::Sleep (500);
-				  }
-			      
-				  // The client is connected.
-				  Log::GetInstance ().info ("Connected.");
-			      
-				  // Create commodity stream:
-				  boost::iostreams::stream<TcpClientSocket> cliSocketStream;
-				  cliSocketStream.open (clientSock);
-			      
-				  cliSocketStream << "fonction=tds&tb=" << codes[client] 
-						  << "&status=" << status.str () << "&ipaddr=0.0.0.0" << std::endl;
-				  
-				  cliSocketStream.flush ();
-				  memset (buf, 0, sizeof (buf));
-				  cliSocketStream.getline (buf, sizeof (buf), (char) 0);
-				  cliSocketStream.close ();
-				  Log::GetInstance ().info ("Status : " + status.str ());
+			      clientSock.tryToConnect ();
+			      Thread::Sleep (500);
 			  }
 			  
+			  // The client is connected.
+			  Log::GetInstance ().info ("Connected.");
+			  
+			  // Create commodity stream:
+			  boost::iostreams::stream<TcpClientSocket> cliSocketStream;
+			  cliSocketStream.open (clientSock);
+			  
+			  cliSocketStream << "fonction=tds&tb=" << codes[client] 
+					  << "&status=" << status.str () << "&ipaddr=0.0.0.0" << std::endl;
+			  
+			  cliSocketStream.flush ();
+			  memset (buf, 0, sizeof (buf));
+			  cliSocketStream.getline (buf, sizeof (buf), (char) 0);
+			  cliSocketStream.close ();
+			  Log::GetInstance ().info ("Status : " + status.str ());
+		      }
+		      
 		  }
               } 
               catch (std::exception e) 
               {
-                      Log::GetInstance ().error ("Error while updating client " + Conversion::ToString (client) + e.what ());
+		  Log::GetInstance ().error ("Error while updating client " + Conversion::ToString (client) + e.what ());
               }
               catch (...) 
               {
-                      Log::GetInstance ().error ("Unexpected error !");
+		  Log::GetInstance ().error ("Unexpected error !");
               }
-    
-		}
-	    Thread::Sleep (100);
+	      
 	}
-	
-	Thread::Sleep (1000);
+	Thread::Sleep (100);
+    }
+    
+    Thread::Sleep (1000);
 }
 
 
