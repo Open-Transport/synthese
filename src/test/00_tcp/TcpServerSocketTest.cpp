478d49c8bf006dff091adeb1fa0365576e3fbd00
Marc Jambert <mjambert@gmail.com>
2007-05-22 08:42:54 +0000

    migrated to boost::tests (continued)

--- src/test/00_tcp/TcpServerSocketTest.cpp
+++ src/test/00_tcp/TcpServerSocketTest.cpp
@@ -1,4 +1,4 @@
-#include "TcpServerSocketTest.h"
+#include "01_util/Conversion.h"
 
 #include "00_tcp/TcpService.h"
 #include "00_tcp/TcpServerSocket.h"
@@ -13,138 +13,116 @@
 #include <boost/thread/thread.hpp>
 #include <boost/iostreams/stream.hpp>
 
+#include <boost/test/auto_unit_test.hpp>
 
-namespace synthese
+using namespace synthese::tcp;
+
+
+struct TcpClientThread
 {
-namespace tcp
-{
+    std::string _replyToSend;
 
-
-  void 
-  TcpServerSocketTest::setUp () 
-  {
-
-  }
-
-
-  void 
-  TcpServerSocketTest::tearDown () 
-  {
-
-  } 
-
-
-    
-    struct TcpClientThread
-    {
-	std::string _replyToSend;
-
-	TcpClientThread (const std::string& replyToSend) 
-	    : _replyToSend (replyToSend)
-	    {
-	    }
+    TcpClientThread (const std::string& replyToSend) 
+	: _replyToSend (replyToSend)
+	{
+	}
 	
-	void operator()()
+    void operator()()
+	{
+	    TcpClientSocket clientSock ("localhost", 8899);
+		
+	    while (clientSock.isConnected () == false)
 	    {
-		TcpClientSocket clientSock ("localhost", 8899);
-		
-		while (clientSock.isConnected () == false)
-		{
-		    clientSock.tryToConnect ();
-		    boost::thread::yield ();
-		}
-		CPPUNIT_ASSERT (clientSock.isConnected ());
-		
-		// The client is connected.
-
-		// Create commodity stream:
-		boost::iostreams::stream<TcpClientSocket> cliSocketStream;
-		cliSocketStream.open (clientSock);
-
-		// Wait for a message...
-		std::string message;
-		cliSocketStream >> message;
-
-		std::string expectedMessage ("MessageToClient");
-		CPPUNIT_ASSERT_EQUAL (expectedMessage, message);
-
-		// Send a reply
-		cliSocketStream << _replyToSend << std::endl;
-		
-		cliSocketStream.close ();
+		clientSock.tryToConnect ();
+		boost::thread::yield ();
 	    }
-    };
+	    BOOST_CHECK (clientSock.isConnected ());
+		
+	    // The client is connected.
+
+	    // Create commodity stream:
+	    boost::iostreams::stream<TcpClientSocket> cliSocketStream;
+	    cliSocketStream.open (clientSock);
+
+	    // Wait for a message...
+	    std::string message;
+	    cliSocketStream >> message;
+
+	    std::string expectedMessage ("MessageToClient");
+	    BOOST_CHECK_EQUAL (expectedMessage, message);
+
+	    // Send a reply
+	    cliSocketStream << _replyToSend << std::endl;
+		
+	    cliSocketStream.close ();
+	}
+};
     
 
 
+BOOST_AUTO_TEST_CASE (testSimpleConnection)
+{
+    TcpClientThread clientThread ("MessageToServerFromClient0");
+    boost::thread client (clientThread);
     
+    TcpService* service = TcpService::openService (8899, true, false);
+    
+    try 
+    {
+	// Wait for client connection...
+	TcpServerSocket* socket = service->acceptConnection ();
 
-  void 
-  TcpServerSocketTest::testSimpleConnection () 
-  {
-      
-      TcpClientThread clientThread ("MessageToServerFromClient0");
-      boost::thread client (clientThread);
-
-      TcpService* service = TcpService::openService (8899);
-
-      try 
-      {
-	  // Wait for client connection...
-	  TcpServerSocket& socket = service->acceptConnection ();
-
-	  CPPUNIT_ASSERT_EQUAL (1, service->getConnectionCount ());
-	  
-	  // Create commodity stream:
-	  boost::iostreams::stream<TcpServerSocket> srvSocketStream (socket);
-
-	  // Send a message to client:
-	  srvSocketStream << "MessageToClient" << std::endl;
-
-	  // Wait for a reply:
-	  std::string reply;
-	  srvSocketStream >> reply;
-
-	  std::string expectedReply ("MessageToServerFromClient0");
-	  CPPUNIT_ASSERT_EQUAL (expectedReply, reply);
-
-	  
-	  srvSocketStream.close ();
-
-	  service->closeConnection (socket);
-	  CPPUNIT_ASSERT_EQUAL (0, service->getConnectionCount ());
-      } 
-      catch (SocketException& se)
-      {
-	  CPPUNIT_ASSERT (false);
-      }
-      
-      client.join ();
-
-
-      TcpService::closeService (8899);
-  } 
+	BOOST_CHECK_EQUAL (1, service->getConnectionCount ());
+	
+	// Create commodity stream:
+	boost::iostreams::stream<TcpServerSocket> srvSocketStream (*socket);
+	
+	// Send a message to client:
+	srvSocketStream << "MessageToClient" << std::endl;
+	
+	// Wait for a reply:
+	std::string reply;
+	srvSocketStream >> reply;
+	
+	std::string expectedReply ("MessageToServerFromClient0");
+	BOOST_CHECK_EQUAL (expectedReply, reply);
+	
+	
+	srvSocketStream.close ();
+	
+	service->closeConnection (socket);
+	BOOST_CHECK_EQUAL (0, service->getConnectionCount ());
+    } 
+    catch (SocketException& se)
+    {
+	BOOST_CHECK (false);
+    }
+    
+    client.join ();
+    
+    
+    TcpService::closeService (8899);
+} 
 
 
 
-  void 
-  TcpServerSocketTest::testSimpleConnectionWhitespaceTransfer () 
-  {
-      
+BOOST_AUTO_TEST_CASE (testSimpleConnectionWhitespaceTransfer)
+{
+
       TcpClientThread clientThread ("Message to server from client 0");
       boost::thread client (clientThread);
 
-      TcpService* service = TcpService::openService (8899);
+      TcpService* service = TcpService::openService (8899, true, false);
 
       try 
       {
 	  // Wait for client connection...
-	  TcpServerSocket& socket = service->acceptConnection ();
+	  TcpServerSocket* socket = service->acceptConnection ();
 
-	  CPPUNIT_ASSERT_EQUAL (1, service->getConnectionCount ());
+	  BOOST_CHECK_EQUAL (1, service->getConnectionCount ());
 	  
 	  // Create commodity stream:
-	  boost::iostreams::stream<TcpServerSocket> srvSocketStream (socket);
+	  boost::iostreams::stream<TcpServerSocket> srvSocketStream (*socket);
 
 	  // Send a message to client:
 	  srvSocketStream << "MessageToClient" << std::endl;
@@ -155,16 +133,16 @@ namespace tcp
 	  std::string reply (buffer);
 
 	  std::string expectedReply ("Message to server from client 0");
-	  CPPUNIT_ASSERT_EQUAL (expectedReply, reply);
+	  BOOST_CHECK_EQUAL (expectedReply, reply);
 	  
 	  srvSocketStream.close ();
 
 	  service->closeConnection (socket);
-	  CPPUNIT_ASSERT_EQUAL (0, service->getConnectionCount ());
+	  BOOST_CHECK_EQUAL (0, service->getConnectionCount ());
       } 
       catch (SocketException& se)
       {
-	  CPPUNIT_ASSERT (false);
+	  BOOST_CHECK (false);
       }
       
       client.join ();
@@ -177,55 +155,54 @@ namespace tcp
 
 
 
-  void 
-  TcpServerSocketTest::testMultipleConnections () 
-  {
-      TcpService* service = TcpService::openService (8899);
+BOOST_AUTO_TEST_CASE (testMultipleConnections)
+{
+    TcpService* service = TcpService::openService (8899, true, false);
 
       TcpClientThread clientThread0 ("MessageToServerFromClient0");
       TcpClientThread clientThread1 ("MessageToServerFromClient1");
       TcpClientThread clientThread2 ("MessageToServerFromClient2");
 
       boost::thread client0 (clientThread0);
-      TcpServerSocket& socket0 = service->acceptConnection ();
-      CPPUNIT_ASSERT_EQUAL (1, service->getConnectionCount ());
+      TcpServerSocket* socket0 = service->acceptConnection ();
+      BOOST_CHECK_EQUAL (1, service->getConnectionCount ());
 
       boost::thread client1 (clientThread1);
-      TcpServerSocket& socket1 = service->acceptConnection ();
-      CPPUNIT_ASSERT_EQUAL (2, service->getConnectionCount ());
+      TcpServerSocket* socket1 = service->acceptConnection ();
+      BOOST_CHECK_EQUAL (2, service->getConnectionCount ());
 
       boost::thread client2 (clientThread2);
-      TcpServerSocket& socket2 = service->acceptConnection ();
-      CPPUNIT_ASSERT_EQUAL (3, service->getConnectionCount ());
+      TcpServerSocket* socket2 = service->acceptConnection ();
+      BOOST_CHECK_EQUAL (3, service->getConnectionCount ());
 
       {
-	  boost::iostreams::stream<TcpServerSocket> stream (socket1);
+	  boost::iostreams::stream<TcpServerSocket> stream (*socket1);
 	  stream << "MessageToClient" << std::endl;
 	  
 	  std::string reply;
 	  stream >> reply;
 	  std::string expectedReply ("MessageToServerFromClient1");
-	  CPPUNIT_ASSERT_EQUAL (expectedReply, reply);
+	  BOOST_CHECK_EQUAL (expectedReply, reply);
 	  client1.join ();
       }
       {
-	  boost::iostreams::stream<TcpServerSocket> stream (socket2);
+	  boost::iostreams::stream<TcpServerSocket> stream (*socket2);
 	  stream << "MessageToClient" << std::endl;
 	  
 	  std::string reply;
 	  stream >> reply;
 	  std::string expectedReply ("MessageToServerFromClient2");
-	  CPPUNIT_ASSERT_EQUAL (expectedReply, reply);
+	  BOOST_CHECK_EQUAL (expectedReply, reply);
 	  client2.join ();
       }
       {
-	  boost::iostreams::stream<TcpServerSocket> stream (socket0);
+	  boost::iostreams::stream<TcpServerSocket> stream (*socket0);
 	  stream << "MessageToClient" << std::endl;
 	  
 	  std::string reply;
 	  stream >> reply;
 	  std::string expectedReply ("MessageToServerFromClient0");
-	  CPPUNIT_ASSERT_EQUAL (expectedReply, reply);
+	  BOOST_CHECK_EQUAL (expectedReply, reply);
 	  client0.join ();
       }
 
@@ -233,18 +210,12 @@ namespace tcp
       service->closeConnection (socket1);
       service->closeConnection (socket2);
 
-      CPPUNIT_ASSERT_EQUAL (0, service->getConnectionCount ());
+      BOOST_CHECK_EQUAL (0, service->getConnectionCount ());
 
       TcpService::closeService (8899);
   } 
 
 
 
-
-
-
-
-}
-}
 
 
