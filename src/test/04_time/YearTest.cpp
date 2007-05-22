e3bd80478407c4474e9985b41472d059d2b725a9
Marc Jambert <mjambert@gmail.com>
2007-05-22 09:32:52 +0000

    migrated to boost::tests (continued)

--- src/test/04_time/YearTest.cpp
+++ src/test/04_time/YearTest.cpp
@@ -1,60 +1,36 @@
-#include "YearTest.h"
-
 #include "04_time/Year.h"
-
 #include <iostream>
 
+#include <boost/test/auto_unit_test.hpp>
 
-namespace synthese
+using namespace synthese::time;
+
+
+BOOST_AUTO_TEST_CASE (testLeapYears)
 {
-namespace time
-{
-
-
-
-  void 
-  YearTest::setUp () 
-  {
-
-  }
-
-
-  void 
-  YearTest::tearDown() 
-  {
-
-  } 
-
-
-  void
-  YearTest::testLeapYears ()
-  {
     {
-      Year y (2000); // divisible by 400
-      CPPUNIT_ASSERT (y.isLeapYear () == true);
-      CPPUNIT_ASSERT_EQUAL (366, y.getDaysCount ());
+	Year y (2000); // divisible by 400
+	BOOST_CHECK (y.isLeapYear () == true);
+	BOOST_CHECK_EQUAL (366, y.getDaysCount ());
     }
     {
-      Year y (1000); // not divisible by 400 but divisible by 100
-      CPPUNIT_ASSERT (y.isLeapYear () == false);
-      CPPUNIT_ASSERT_EQUAL (365, y.getDaysCount ());
+	Year y (1000); // not divisible by 400 but divisible by 100
+	BOOST_CHECK (y.isLeapYear () == false);
+	BOOST_CHECK_EQUAL (365, y.getDaysCount ());
     }
     {
-      Year y (2004); // not divisible by 400 nor 100, but divisible by 4
-      CPPUNIT_ASSERT (y.isLeapYear () == true);
-      CPPUNIT_ASSERT_EQUAL (366, y.getDaysCount ());
+	Year y (2004); // not divisible by 400 nor 100, but divisible by 4
+	BOOST_CHECK (y.isLeapYear () == true);
+	BOOST_CHECK_EQUAL (366, y.getDaysCount ());
     }
     {
-      Year y (2006); // not divisible by 400 nor 100, but divisible by 4
-      CPPUNIT_ASSERT (y.isLeapYear () == false);
-      CPPUNIT_ASSERT_EQUAL (365, y.getDaysCount ());
+	Year y (2006); // not divisible by 400 nor 100, but divisible by 4
+	BOOST_CHECK (y.isLeapYear () == false);
+	BOOST_CHECK_EQUAL (365, y.getDaysCount ());
     }
+    
+    
+ }
 
-
-  }
-
-
-}
-}
 
 
