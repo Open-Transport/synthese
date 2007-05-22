bf4ad9fae1ac621cc00bd577d2be8081220cd649
Marc Jambert <mjambert@gmail.com>
2007-05-22 10:15:07 +0000

    migrated to boost::tests

--- src/test/33_route_planner/RoutePlannerTest.cpp
+++ src/test/33_route_planner/RoutePlannerTest.cpp
@@ -1,91 +1,22 @@
-#define BOOST_AUTO_TEST_MAIN
+#include "33_route_planner/RoutePlanner.h"
 
 
 #include <boost/test/auto_unit_test.hpp>
-// #include <boost/test/included/unit_test_framework.hpp>
+
+using namespace synthese::routeplanner;
+using namespace synthese::util;
 
 
-int add( int i, int j ) { return i+j; }
-
-
-
-BOOST_AUTO_TEST_SUITE( suite_name );
-
-
-BOOST_AUTO_TEST_CASE( my_test )
+BOOST_AUTO_TEST_CASE (testFake)
 {
-    // seven ways to detect and report the same error:
-    BOOST_CHECK( add( 2,2 ) == 4 );        // #1 continues on error
-
-    BOOST_REQUIRE( add( 2,2 ) == 4 );      // #2 throws on error
-
-    if( add( 2,2 ) != 4 )
-      BOOST_ERROR( "Ouch..." );            // #3 continues on error
-
-    if( add( 2,2 ) != 4 )
-      BOOST_FAIL( "Ouch..." );             // #4 throws on error
-
-    if( add( 2,2 ) != 4 ) throw "Ouch..."; // #5 throws on error
-
-    BOOST_CHECK_MESSAGE( add( 2,2 ) == 4,  // #6 continues on error
-                         "add(..) result: " << add( 2,2 ) );
-
-    BOOST_CHECK_EQUAL( add( 2,2 ), 4 );	  // #7 continues on error
-}
-
-
-BOOST_AUTO_TEST_CASE( my_test2 )
-{
-    BOOST_CHECK( false );        // #1 continues on error
-
-}
-
-
-BOOST_AUTO_TEST_SUITE_END ();
-
-
 /*
-
-
-#include "15_env/Axis.h"
-#include "15_env/Calendar.h"
-#include "15_env/Line.h"
-
-#include "04_time/Hour.h"
-#include "04_time/Schedule.h"
-
-#include <iostream>
-
-
-namespace synthese
-{
-namespace routeplanner
-{
-
-
-
-
-
-
-  void 
-  RoutePlannerTest::setUp () 
-  {
-
-  }
-
-
-  void 
-  RoutePlannerTest::tearDown() 
-  {
-
-  } 
-
-
-
-
-
-}
-}
-
-
+     long long lli = 123456789012345LL;
+     std::string lls = Conversion::ToString (lli);
+      
+     BOOST_CHECK_EQUAL (std::string ("123456789012345"), lls);
+     BOOST_CHECK_EQUAL (lli, Conversion::ToLongLong ("123456789012345"));
 */
+
+}
+
+
