9653c5e73e00c25933d212e602384a3711d529b3
Hugues Romain <hromain@rcsmobility.com>
2010-03-25 13:14:46 +0000

    N°entrée RCS/projets : 7276

---
+++ src/14_geography/CityAliasTableSync.hpp
@@ -0,0 +1,85 @@
+
+//////////////////////////////////////////////////////////////////////////
+///	CityAliasTableSync class header.
+///	@file CityAliasTableSync.hpp
+///	@author Hugues
+///	@date 2010
+///
+///	This file belongs to the SYNTHESE project (public transportation specialized software)
+///	Copyright (C) 2002 Hugues Romain - RCS <contact@reseaux-conseil.com>
+///
+///	This program is free software; you can redistribute it and/or
+///	modify it under the terms of the GNU General Public License
+///	as published by the Free Software Foundation; either version 2
+///	of the License, or (at your option) any later version.
+///
+///	This program is distributed in the hope that it will be useful,
+///	but WITHOUT ANY WARRANTY; without even the implied warranty of
+///	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+///	GNU General Public License for more details.
+///
+///	You should have received a copy of the GNU General Public License
+///	along with this program; if not, write to the Free Software
+///	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+#ifndef SYNTHESE_CityAliasTableSync_hpp__
+#define SYNTHESE_CityAliasTableSync_hpp__
+
+#include "CityAlias.hpp"
+#include "SQLiteRegistryTableSyncTemplate.h"
+
+namespace synthese
+{
+	namespace geography
+	{
+		//////////////////////////////////////////////////////////////////////////
+		///	CityAlias table synchronizer.
+		///	@ingroup m32LS refLS
+		///	@author Hugues Romain
+		///	@date 2010
+		/// @since 3.1.16
+		class CityAliasTableSync:
+			public db::SQLiteRegistryTableSyncTemplate<CityAliasTableSync,CityAlias>
+		{
+		public:
+			//! @name Field names
+			//@{
+				static const std::string COL_NAME;
+				static const std::string COL_CODE;
+				static const std::string COL_ALIASED_CITY_ID;
+				static const std::string COL_VISIBLE;
+			//@}
+			
+
+			//! @name Services
+			//@{		
+				//////////////////////////////////////////////////////////////////////////
+				///	City Alias search.
+				///	@param env Environment to populate
+				/// @param aliasedCityFilter Aliased city filter
+				///	@param codeFilter optional filter on code field
+				///	@param first First  object to answer
+				///	@param number Number of  objects to answer (0 = all) The size of the vector is less or equal to number, then all users were returned despite of the number limit. If the size is greater than number (actually equal to number + 1) then there is others accounts to show. Test it to know if the situation needs a "click for more" button.
+				///	@param orderByName The result records must be ordered by their name
+				///	@param raisingOrder The result records must be sorted ascendantly
+				///	@param linkLevel Level of links to build when reading foreign keys
+				///	@return Found objects.
+				///	@author Hugues Romain
+				///	@date 2010
+				/// @since 3.1.16
+				static SearchResult Search(
+					util::Env& env,
+					boost::optional<util::RegistryKeyType> aliasedCityFilter = boost::optional<util::RegistryKeyType>(),
+					boost::optional<std::string> codeFilter = boost::optional<std::string>(),
+					std::size_t first = 0,
+					boost::optional<std::size_t> number = boost::optional<std::size_t>(),
+					bool orderByName = true,
+					bool raisingOrder = true,
+					util::LinkLevel linkLevel = util::UP_LINKS_LOAD_LEVEL
+				);
+			//@}
+		};
+	}
+}
+
+#endif // SYNTHESE_CityAliasTableSync_hpp__
