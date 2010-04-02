45b9df16fcaf0d369d2ffe2ca557a662338beae1
Hugues Romain <hromain@rcsmobility.com>
2010-04-02 10:05:57 +0000

    N°entrée RCS/projets : 7328

---
+++ src/35_pt/CheckLineCalendarFunction.cpp
@@ -0,0 +1,132 @@
+
+/** CheckLineCalendarFunction class implementation.
+	@file CheckLineCalendarFunction.cpp
+	@author Hugues Romain
+	@date 2010
+
+	This file belongs to the SYNTHESE project (public transportation specialized software)
+	Copyright (C) 2002 Hugues Romain - RCS <contact@reseaux-conseil.com>
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#include "RequestException.h"
+#include "Request.h"
+#include "CheckLineCalendarFunction.hpp"
+#include "CommercialLine.h"
+#include "CalendarTemplate.h"
+#include "Calendar.h"
+
+#include <boost/lexical_cast.hpp>
+
+using namespace std;
+using namespace boost;
+using namespace boost::gregorian;
+
+namespace synthese
+{
+	using namespace util;
+	using namespace server;
+	using namespace security;
+	using namespace env;
+	using namespace calendar;
+
+	template<> const string util::FactorableTemplate<Function,pt::CheckLineCalendarFunction>::FACTORY_KEY("CheckLineCalendar");
+	
+	namespace pt
+	{
+		const string CheckLineCalendarFunction::PARAMETER_DURATION("du");
+		
+		ParametersMap CheckLineCalendarFunction::_getParametersMap() const
+		{
+			ParametersMap map;
+			if(_line.get())
+			{
+				map.insert(Request::PARAMETER_OBJECT_ID, _line->getKey());
+				map.insert(PARAMETER_DURATION, _duration.days());
+			}
+			return map;
+		}
+
+		void CheckLineCalendarFunction::_setFromParametersMap(const ParametersMap& map)
+		{
+			RegistryKeyType id(map.get<RegistryKeyType>(Request::PARAMETER_OBJECT_ID));
+			if(Env::GetOfficialEnv().getRegistry<CommercialLine>().contains(id))
+			{
+				_line = Env::GetOfficialEnv().get<CommercialLine>(id);
+			}
+
+			_duration = days(map.getDefault<int>(PARAMETER_DURATION, 45));
+		}
+
+		void CheckLineCalendarFunction::run(
+			std::ostream& stream,
+			const Request& request
+		) const {
+
+			if(	!_line.get()
+			){
+				stream << "3\nNo such line.|days=-1\n";
+			} else {
+				
+				date now(day_clock::local_day());
+				date endDate(now + _duration);
+				Calendar period(now, endDate);
+
+				Calendar runDays(_line->getRunDays(period));
+				
+				if(	!_line->getCalendarTemplate()
+				){
+					stream << "3\nLine "+ _line->getShortName() +" is unmonitored.|days="+ lexical_cast<string>(runDays.size()) + "\n";
+				} else {
+
+					if(_line->respectsCalendarTemplate(_duration))
+					{
+						stream << "0\nOK Line "+ _line->getShortName() +" runs the "+ lexical_cast<string>(_duration.days()) +" next days of "+ _line->getCalendarTemplate()->getText() +"|days="+ lexical_cast<string>(runDays.size()) + "\n";
+					}
+					else
+					{
+						stream << "2\nKO Line "+ _line->getShortName() +" does not run then "+ lexical_cast<string>(_duration.days()) +" days of "+ _line->getCalendarTemplate()->getText() +"|days="+ lexical_cast<string>(runDays.size()) + "\n";
+					}
+				}
+			}
+
+		}
+		
+		
+		
+		bool CheckLineCalendarFunction::isAuthorized(
+			const Session* session
+		) const {
+			return true;
+		}
+
+
+
+		std::string CheckLineCalendarFunction::getOutputMimeType() const
+		{
+			return "text/plain";
+		}
+
+
+
+		CheckLineCalendarFunction::CheckLineCalendarFunction():
+			FactorableTemplate<server::Function,CheckLineCalendarFunction>(),
+			_duration(days(45))
+		{
+
+		}
+	}
+}
---
+++ src/35_pt/CommercialLineCalendarTemplateUpdateAction.cpp
@@ -0,0 +1,116 @@
+
+//////////////////////////////////////////////////////////////////////////
+/// CommercialLineCalendarTemplateUpdateAction class implementation.
+/// @file CommercialLineCalendarTemplateUpdateAction.cpp
+/// @author Hugues Romain
+/// @date 2010
+///
+///	This file belongs to the SYNTHESE project (public transportation specialized software)
+///	Copyright (C) 2002 Hugues Romain - RCS <contact@reseaux-conseil.com>
+///
+///	This program is free software; you can redistribute it and/or
+///	modify it under the terms of the GNU General Public License
+///	as published by the Free Software Foundation; either version 2
+///	of the License, or (at your option) any later version.
+///
+///	This program is distributed in the hope that it will be useful,
+///	but WITHOUT ANY WARRANTY; without even the implied warranty of
+///	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+///	GNU General Public License for more details.
+///
+///	You should have received a copy of the GNU General Public License
+///	along with this program; if not, write to the Free Software
+///	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+#include "ActionException.h"
+#include "ParametersMap.h"
+#include "CommercialLineCalendarTemplateUpdateAction.hpp"
+#include "TransportNetworkRight.h"
+#include "Request.h"
+#include "CommercialLineTableSync.h"
+#include "CalendarTemplateTableSync.h"
+
+using namespace std;
+using namespace boost;
+
+namespace synthese
+{
+	using namespace server;
+	using namespace security;
+	using namespace util;
+	using namespace env;
+	using namespace calendar;
+	
+	namespace util
+	{
+		template<> const string FactorableTemplate<Action, pt::CommercialLineCalendarTemplateUpdateAction>::FACTORY_KEY("CommercialLineCalendarTemplateUpdateAction");
+	}
+
+	namespace pt
+	{
+		const string CommercialLineCalendarTemplateUpdateAction::PARAMETER_LINE_ID = Action_PARAMETER_PREFIX + "id";
+		const string CommercialLineCalendarTemplateUpdateAction::PARAMETER_CALENDAR_TEMPLATE_ID = Action_PARAMETER_PREFIX + "ci";
+		
+		
+		
+		ParametersMap CommercialLineCalendarTemplateUpdateAction::getParametersMap() const
+		{
+			ParametersMap map;
+			if(_line.get())
+			{
+				map.insert(PARAMETER_LINE_ID, _line->getKey());
+				if(_calendarTemplate.get())
+				{
+					map.insert(PARAMETER_CALENDAR_TEMPLATE_ID, _calendarTemplate->getKey());
+				}
+			}
+			return map;
+		}
+		
+		
+		
+		void CommercialLineCalendarTemplateUpdateAction::_setFromParametersMap(const ParametersMap& map)
+		{
+			try
+			{
+				_line = CommercialLineTableSync::GetEditable(map.get<RegistryKeyType>(PARAMETER_LINE_ID), *_env);
+			}
+			catch(ObjectNotFoundException<CommercialLine>& e)
+			{
+				throw ActionException("No such line");
+			}
+
+			try
+			{
+				_calendarTemplate = CalendarTemplateTableSync::GetEditable(map.get<RegistryKeyType>(PARAMETER_CALENDAR_TEMPLATE_ID), *_env);
+			}
+			catch(ObjectNotFoundException<CommercialLine>& e)
+			{
+				throw ActionException("No such calendar template");
+			}
+		}
+		
+		
+		
+		void CommercialLineCalendarTemplateUpdateAction::run(
+			Request& request
+		){
+			//stringstream text;
+			//::appendToLogIfChange(text, "Parameter ", _object->getAttribute(), _newValue);
+			
+			_line->setCalendarTemplate(_calendarTemplate.get());
+
+			CommercialLineTableSync::Save(_line.get());
+
+			//::AddUpdateEntry(*_object, text.str(), request.getUser().get());
+		}
+		
+		
+		
+		bool CommercialLineCalendarTemplateUpdateAction::isAuthorized(
+			const Session* session
+		) const {
+			return session && session->hasProfile() && session->getUser()->getProfile()->isAuthorized<TransportNetworkRight>(WRITE);
+		}
+	}
+}
