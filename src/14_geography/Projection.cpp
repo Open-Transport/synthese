9713b4f48754fc1b5254c56a0ef61dd7780760e4
Hugues Romain <hromain@rcsmobility.com>
2010-05-31 19:49:20 +0000

    N°entrée RCS/projets : 7608

--- src/14_geography/Projection.cpp
+++ src/14_geography/Projection.cpp
@@ -1,3 +1,26 @@
+
+/** Projection tools implementation.
+	@file Projection.cpp
+
+	This file belongs to the SYNTHESE project (public transportation specialized software)
+	Copyright (C) 2002 Hugues Romain - RCS <contact@reseaux-conseil.com>
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+
 #include "Projection.h"
 
 #include "Point2D.h"
@@ -67,6 +90,20 @@ namespace synthese
 			while (std::abs(phi_i - phi_im1) >= epsilon);
 
 			return phi_i;
+		}
+
+
+
+		Point2D ALG0003(double lambda, double phi, double e, double Xs, double Ys, double c, double n, double lambdac)
+		{
+			double L = ALG0001(phi, e);
+
+			Point2D result(
+				Xs + c * exp(-n * L) * sin(n * (lambda - lambdac)),
+				Ys - c * exp(-n * L) * cos(n * (lambda - lambdac))
+			);
+
+			return result;		
 		}
 
 
@@ -284,6 +321,153 @@ namespace synthese
 			LatLonHe coords = ALG0012(coords1.x, coords1.y, coords1.z, a, e, epsilon);
 		    
 			return GeoPoint (ToDegrees (coords.phi), ToDegrees (coords.lambda), coords.he);
+		}
+
+
+
+		GeoPoint WGS84FromCH1903( const geometry::Point2D& point )
+		{
+			// Converts militar to civil and  to unit = 1000km
+			// Axiliary values (% Bern)
+			double y_aux = (point.getX() - 600000)/1000000;
+			double x_aux = (point.getY() - 200000)/1000000;
+
+			// Process lat
+			double lat = 16.9023892
+				+  3.238272 * x_aux
+				-  0.270978 * y_aux * y_aux
+				-  0.002528 * x_aux * x_aux
+				-  0.0447   * y_aux * y_aux * x_aux
+				-  0.0140   * x_aux * x_aux * x_aux;
+
+			// Unit 10000" to 1 " and converts seconds to degrees (dec)
+			lat = lat * 100/36;
+
+			// Process long
+			double lng = 2.6779094
+				+ 4.728982 * y_aux
+				+ 0.791484 * y_aux * x_aux
+				+ 0.1306   * y_aux * x_aux * x_aux
+				- 0.0436   * y_aux * y_aux * y_aux;
+
+			// Unit 10000" to 1 " and converts seconds to degrees (dec)
+			lng = lng * 100/36;
+
+			return GeoPoint (lat, lng, 0);
+		}
+
+
+
+		geometry::Point2D CH1903FromWGS84( GeoPoint point )
+		{
+/*			// Converts degrees dec to sex
+			lat = DecToSexAngle(lat);
+			lng = DecToSexAngle(lng);
+
+			// Converts degrees to seconds (sex)
+			lat = SexAngleToSeconds(lat);
+			lng = SexAngleToSeconds(lng);
+
+			// Axiliary values (% Bern)
+			double lat_aux = (lat - 169028.66)/10000;
+			double lng_aux = (lng - 26782.5)/10000;
+
+			// Process Y
+			double y = 600072.37 
+				+ 211455.93 * lng_aux 
+				-  10938.51 * lng_aux * lat_aux
+				-      0.36 * lng_aux * Math.Pow(lat_aux, 2)
+				-     44.54 * Math.Pow(lng_aux, 3);
+
+			return y;
+
+			// Converts degrees dec to sex
+			lat = DecToSexAngle(lat);
+			lng = DecToSexAngle(lng);
+
+			// Converts degrees to seconds (sex)
+			lat = SexAngleToSeconds(lat);
+			lng = SexAngleToSeconds(lng);
+
+			// Axiliary values (% Bern)
+			double lat_aux = (lat - 169028.66)/10000;
+			double lng_aux = (lng - 26782.5)/10000;
+
+			// Process X
+			double x = 200147.07
+				+ 308807.95 * lat_aux 
+				+   3745.25 * Math.Pow(lng_aux, 2)
+				+     76.63 * Math.Pow(lat_aux, 2)
+				-    194.56 * Math.Pow(lng_aux, 2) * lat_aux
+				+    119.79 * Math.Pow(lat_aux, 3);
+
+			return x;
+*/
+			return Point2D();
+		}
+
+
+
+		const geometry::Point2D LambertFromWGS84( const GeoPoint& point, const LambertOrigin orig /*= LAMBERT_IIe */ )
+		{
+			double epsilon (0.00000000001);
+			double n, c, Xs, Ys, lambdac, e, he, a, Tx, Ty, Tz, D, Rx, Ry, Rz;
+			switch (orig)
+			{
+			case LAMBERT_II :
+				n = 0.7289686274;
+				c = 11745793.39;
+				Xs = 600000;
+				Ys = 6199695.768;
+				lambdac = 0.04079234433; // Greenwich
+				e = 0.08248325676; 
+				he = 100;
+				a = 6378249.2;
+				Tx = -168;
+				Ty = -60;
+				Tz = +320;
+				D  = 0;
+				Rx = Ry = Rz = 0;
+				break;
+			case LAMBERT_IIe :
+				n = 0.7289686274;
+				c = 11745793.39;
+				Xs = 600000;
+				Ys = 8199695.768;
+				lambdac = 0.04079234433; // Greenwich
+				e = 0.08248325676; 
+				he = 100;
+				a = 6378249.2;
+				Tx = -168;
+				Ty = -60;
+				Tz = +320;
+				D  = 0;
+				Rx = Ry = Rz = 0;
+				break;
+			case LAMBERT_93 :
+				n = 0.7256077650;
+				c = 11745255.426;
+				Xs = 700000;
+				Ys = 12655612.050;
+				lambdac = 0.04079234433; // Greenwich	
+				e = 0.08248325676; 
+				he = 100;
+				a = 6378249.2;
+				Tx = -168;
+				Ty = -60;
+				Tz = +320;
+				D  = 0;
+				Rx = Ry = Rz = 0;
+				break;
+			}
+
+			Point2D result = ALG0003(
+				ToRadians(point.getLongitude()),
+				ToRadians(point.getLatitude()),
+				e, Xs, Ys, c, n, lambdac
+			);
+
+			return result;
 		}
 
 	}
--- src/14_geography/Projection.h
+++ src/14_geography/Projection.h
@@ -1,3 +1,25 @@
+
+/** Projection tools header.
+	@file Projection.h
+
+	This file belongs to the SYNTHESE project (public transportation specialized software)
+	Copyright (C) 2002 Hugues Romain - RCS <contact@reseaux-conseil.com>
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
 #ifndef SYNTHESE_GEOGRAPHY_PROJECTION_H
 #define SYNTHESE_GEOGRAPHY_PROJECTION_H
 
@@ -33,10 +55,36 @@ namespace synthese
 		///
 		/// http://www.ign.fr/telechargement/MPro/geodesie/CIRCE/transfo.pdf
 		/// http://www.ign.fr/telechargement/MPro/geodesie/CIRCE/NTG_80.pdf
-		/// http://www.ign.fr/telechargement/MPro/geodesie/CIRCE/NTG_71.pdf
+		/// <a href="include/14_geography/NTG_71.pdf">Algorithm documentation</a>
 		GeoPoint WGS84FromLambert(
 			const geometry::Point2D& point,
 			const LambertOrigin orig = LAMBERT_IIe
+		);
+
+		const geometry::Point2D LambertFromWGS84(
+			const GeoPoint& point,
+			const LambertOrigin orig = LAMBERT_IIe
+		);
+
+
+		//////////////////////////////////////////////////////////////////////////
+		/// WGS84 point generation from Swiss MN03 projection (CH-1903)
+		/// @return WGS84 converted point
+		/// @ingroup m32
+		/// @date 2010
+		/// @since 3.1.18
+		/// @author Hugues Romain
+		//////////////////////////////////////////////////////////////////////////
+		/// http://www.swisstopo.admin.ch/internet/swisstopo/en/home/products/software/products/skripts.html
+		/// <a href="include/14_geography/ch1903wgs84en.pdf">Algorithm documentation (EN)</a>
+		/// <a href="include/14_geography/ch1903wgs84fr.pdf">Algorithm documentation (FR)</a>
+		GeoPoint WGS84FromCH1903(
+			const geometry::Point2D& point
+		);
+
+
+		geometry::Point2D CH1903FromWGS84(
+			GeoPoint point
 		);
 	}
 }
