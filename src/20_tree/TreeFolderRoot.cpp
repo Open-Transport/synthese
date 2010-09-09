165b6286393ffea5a8db8da1dadfa3ec67c4b6c1
Hugues Romain <hromain@rcsmobility.com>
2010-09-09 05:40:59 +0000

    The commit message was empty

---
+++ src/20_tree/TreeFolderRoot.cpp
@@ -0,0 +1,12 @@
+
+#include "TreeFolderRoot.hpp"
+
+using namespace std;
+
+namespace synthese
+{
+	namespace util
+	{
+		template<> const string Registry<tree::TreeFolderRoot>::KEY("TreeFolderRoot");
+	}
+}
