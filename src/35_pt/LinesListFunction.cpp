c9ccb7b1cdd3c7c23a0847ec894dbd483e04c3e7
Xavier Raffin <xavier.raffin@tisseo.fr>
2010-09-22 11:18:19 +0000

    Change LinesListFunction from SQLite search to RAM search.
    Add a ordering mecanism which works like that:
    
                            //Sortering is made on numerical order
    			//   (except for line number which doesn't begin by a number)
    			//   then alphabetic order.
    			//
    			//So, linesMap is like that:
    			//
    			// [1]  -> ligne 1 : XXX - XXX
    			// [2]  -> ligne 2 : XXX - XXX
    			// [2S] -> ligne 2S: XXX - XXX
    			// [A]  -> ligne A : XXX - XXX
    			// [A1] -> ligne A1: XXX - XXX

--- src/35_pt/LinesListFunction.cpp
+++ src/35_pt/LinesListFunction.cpp
@@ -26,15 +26,13 @@
 #include "RequestException.h"
 #include "LinesListFunction.h"
 #include "TransportNetwork.h"
-#include "TransportNetworkTableSync.h"
 #include "CommercialLine.h"
-#include "CommercialLineTableSync.h"
 #include "LineMarkerInterfacePage.h"
 #include "Webpage.h"
 #include "RollingStock.h"
 #include "Path.h"
 #include "JourneyPattern.hpp"
-
+#include "TransportNetworkTableSync.h"
 #include <boost/foreach.hpp>
 #include <boost/lexical_cast.hpp>
 
@@ -88,20 +86,128 @@ namespace synthese
 			_outputFormat = (*idOf);
 		}
 
+		//Class used for trim:
+		class sortableNumber
+		{
+		private:
+			string _value;
+			string _begin;// = _value except for 12s it is 12
+			string _end;  // =  ""    except for 12s it is  s
+			long int _numericalValue;// = 12 for 12 and 12s, = -1 for A
+
+			typedef enum {
+				isAnInteger,   //example : 12
+				beginIsInteger,   //example : 12s
+				beginIsNotInteger //example : A, or T1
+			} numberType;
+			numberType _numberType;
+
+		public:
+			sortableNumber(string str)
+			{
+				_numericalValue = -1;
+				_value = str;
+
+				char * buffer = strdup(_value.c_str());
+				char * pEnd;
+
+				errno = 0;
+
+				_numericalValue = strtol (buffer, &pEnd, 10);
+				if (errno != 0 || pEnd == buffer)//Is A form
+				{
+					_numberType = beginIsNotInteger;
+					_begin = _value;
+					_end = "";
+				}
+				else
+				{
+					if(*pEnd != 0 )//Is 12s form so pEnd is "s";
+					{
+						_numberType = beginIsInteger;
+						_end = string(pEnd);
+						*pEnd = '\0';
+						_begin = string(buffer);
+					}
+					else//Is 12 form
+					{
+						_numberType = isAnInteger;
+						_begin = _value;
+						_end = "";
+					}
+				}
+				cout << "str = " << str;
+				cout << "_numericalValue = " << _numericalValue;
+				cout << "_numberType = " << _numberType;
+				cout << "_begin = " << _begin;
+				cout << "_end = " << _end;
+				cout << "_value = " << _value << endl;
+			}
+
+			bool operator<(sortableNumber const &otherNumber) const
+			{
+				if((_numberType != beginIsInteger)
+						&& (otherNumber._numberType != beginIsInteger))//No number have form "12S"
+				{
+					if((_numberType == beginIsNotInteger)
+							|| (otherNumber._numberType == beginIsNotInteger))//At least one number have form "A"
+					{
+						return _value < otherNumber._value;
+					}
+					else//The two numbers have form 12
+					{
+						return _numericalValue < otherNumber._numericalValue;
+					}
+				}
+				else if(_begin != otherNumber._begin)//At least one number have form 12S, and the other one have a different begin (eg. 13)
+				{
+					if((_numberType == isAnInteger)
+							|| (otherNumber._numberType == isAnInteger))//The second number have 13 form
+					{
+						return _numericalValue < otherNumber._numericalValue;
+					}
+					else//The second number have A form
+					{
+						return _value < otherNumber._value;
+					}
+				}
+				else // Case 12/12s or 12s/12k : sort based on end
+				{
+					return _end < otherNumber._end;
+				}
+			}
+		};
 
 		void LinesListFunction::run( std::ostream& stream, const Request& request ) const
 		{
-			CommercialLineTableSync::SearchResult lines(
-					CommercialLineTableSync::Search(*_env,
-							                        _network->getKey(),
-							                        boost::optional<std::string>(),
-							                        boost::optional<std::string>(),
-							                        0,
-							                        boost::optional<std::size_t>(),
-							                        true,//OrderByNetwork
-							                        true,//OrderByName
-							                        true)//is Ascendent ordering
-			);
+			//Sortering is made on numerical order
+			//   (except for line number which doesn't begin by a number)
+			//   then alphabetic order.
+			//
+			//So, linesMap is like that:
+			//
+			// [1]  -> ligne 1 : XXX - XXX
+			// [2]  -> ligne 2 : XXX - XXX
+			// [2S] -> ligne 2S: XXX - XXX
+			// [A]  -> ligne A : XXX - XXX
+			// [A1] -> ligne A1: XXX - XXX
+			//
+
+			typedef map<sortableNumber,shared_ptr<const CommercialLine> > linesMapType;
+			linesMapType linesMap;
+
+			//Get CommercialLine Global Registry
+			typedef const pair<const RegistryKeyType, shared_ptr<CommercialLine> > myType;
+			BOOST_FOREACH(myType&  myLine,Env::GetOfficialEnv().getRegistry<CommercialLine>())
+			{
+				/*string shortname = myLine.second->getShortName();
+				sortableNumber toti = sortableNumber(shortname);
+				shared_ptr<CommercialLine> toto = myLine.second;
+				linesMap[toti]=toto;*/
+				//Insert respecting order described up there
+				linesMap[sortableNumber(myLine.second->getShortName())] = myLine.second;
+			}
+
 			if((!_page.get())&&(_outputFormat =="xml"))
 			{
 				// XML header
@@ -111,8 +217,9 @@ namespace synthese
 					;
 			}
 			size_t rank(0);
-			BOOST_FOREACH(shared_ptr<const CommercialLine> line, lines)
+			BOOST_FOREACH(linesMapType::value_type it, linesMap)
 			{
+				shared_ptr<const CommercialLine> line = it.second;
 				if(_page.get())
 				{
 					LineMarkerInterfacePage::Display(
