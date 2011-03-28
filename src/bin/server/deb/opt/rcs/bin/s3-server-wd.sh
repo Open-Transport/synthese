cb43c25b1b3636b7b8bdcf5e6fd733b9c80b3983
Sylvain Pasche <spasche.rcs@gmail.com>
2011-03-28 09:37:07 +0000

    Update synthese launch script and configuration file for new database command line argument. Translate some text into english.

--- src/bin/server/deb/opt/rcs/bin/s3-server-wd.sh
+++ src/bin/server/deb/opt/rcs/bin/s3-server-wd.sh
@@ -2,18 +2,25 @@
 
 NAME=s3-server
 
-source /etc/opt/rcs/$NAME.conf
+. /etc/opt/rcs/$NAME.conf
 
 DAEMON=/opt/rcs/bin/$NAME
 
 test -x $DAEMON || exit 0
 while true
 do
-	($DAEMON --pidfile $S3_SERVER_PID_FILE --logfile $S3_SERVER_LOG_FILE --db $S3_SERVER_DB_FILE --param tmp_dir=$S3_SERVER_TMP_DIR --param port=$S3_SERVER_PORT --param log_level=$S3_SERVER_LOG_LEVEL) || (echo "Failed!" && exit 1)
+	($DAEMON \
+		--pidfile $S3_SERVER_PID_FILE \
+		--logfile $S3_SERVER_LOG_FILE \
+		--dbconn $S3_SERVER_DB_CONNECTION \
+		--param tmp_dir=$S3_SERVER_TMP_DIR \
+		--param port=$S3_SERVER_PORT \
+		--param log_level=$S3_SERVER_LOG_LEVEL
+	) || (echo "Failed!" && exit 1)
 	rm -f $S3_SERVER_PID_FILE
 
-	mail $S3_EMAIL_SEGFAULT -s "Crash SYNTHESE3 sur `hostname`" <<ENDOFMAIL
-SYNTHESE3 a rebooté sur `hostname`, demarrage automatique en cours.
+	mail $S3_EMAIL_SEGFAULT -s "SYNTHESE3 crash on `hostname`" <<ENDOFMAIL
+SYNTHESE3 was rebooted on `hostname`, automatic restart is in progress.
 
 `tail -n 100 /var/log/rcs/s3-server.log`
 
