f08d491bde5596829fd2215c472a38cc58877b19
Sylvain Pasche <sylvain.pasche@gmail.com>
2011-03-28 09:14:43 +0000

    New ConcurrentQueue.hpp header.

---
+++ src/01_util/ConcurrentQueue.hpp
@@ -0,0 +1,83 @@
+
+/** ConcurrentQueue class header.
+	@file ConcurrentQueue.hpp
+	@author Anthony Williams
+
+	This file belongs to the SYNTHESE project (public transportation specialized software)
+	Copyright (C) 2002 Hugues Romain - RCS <contact@reseaux-conseil.com>
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#ifndef SYNTHESE_util_ConcurrentQueue_h__
+#define SYNTHESE_util_ConcurrentQueue_h__
+
+// This class was copied from
+// http://www.justsoftwaresolutions.co.uk/threading/implementing-a-thread-safe-queue-using-condition-variables.html
+// By Anthony Williams
+
+#include <boost/thread/condition_variable.hpp>
+#include <boost/thread/mutex.hpp>
+#include <queue>
+
+template<typename Data>
+class ConcurrentQueue
+{
+private:
+	std::queue<Data> the_queue;
+	mutable boost::mutex the_mutex;
+	boost::condition_variable the_condition_variable;
+public:
+	void push(Data const& data)
+	{
+		boost::mutex::scoped_lock lock(the_mutex);
+		the_queue.push(data);
+		lock.unlock();
+		the_condition_variable.notify_one();
+	}
+
+	bool empty() const
+	{
+		boost::mutex::scoped_lock lock(the_mutex);
+		return the_queue.empty();
+	}
+
+	bool try_pop(Data& popped_value)
+	{
+		boost::mutex::scoped_lock lock(the_mutex);
+		if(the_queue.empty())
+		{
+			return false;
+		}
+		
+		popped_value=the_queue.front();
+		the_queue.pop();
+		return true;
+	}
+
+	void wait_and_pop(Data& popped_value)
+	{
+		boost::mutex::scoped_lock lock(the_mutex);
+		while(the_queue.empty())
+		{
+			the_condition_variable.wait(lock);
+		}
+		
+		popped_value=the_queue.front();
+		the_queue.pop();
+	}
+};
+
+#endif // SYNTHESE_util_ConcurrentQueue_h__
