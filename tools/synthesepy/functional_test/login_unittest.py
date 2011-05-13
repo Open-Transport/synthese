dbde92fff1db58ff548a8869245119c505c09477
Sylvain Pasche <sylvain.pasche@gmail.com>
2011-05-13 18:57:27 +0000

    Add license text to Python scripts in tools.

--- tools/synthesepy/functional_test/login_unittest.py
+++ tools/synthesepy/functional_test/login_unittest.py
@@ -1,3 +1,25 @@
+#    Login unit tests.
+#    @file login_unittest.py
+#    @author Sylvain Pasche
+#
+#    This file belongs to the SYNTHESE project (public transportation specialized software)
+#    Copyright (C) 2002 Hugues Romain - RCS <contact@reseaux-conseil.com>
+#
+#    This program is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU General Public License
+#    as published by the Free Software Foundation; either version 2
+#    of the License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
 import sys
 
 if sys.version_info >= (2, 7):
