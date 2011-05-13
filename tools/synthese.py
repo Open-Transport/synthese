cfdddf645987327366e7e277047519bd76592bcf
Sylvain Pasche <spasche.rcs@gmail.com>
2011-05-13 18:52:22 +0000

    Improve the bootstram mechanism: the Python environment is now stored in the user's home directory (unless it is running from the installed location). Command line processing moved to a cli.py script.

--- tools/synthese.py
+++ tools/synthese.py
@@ -1,112 +1,192 @@
-import argparse
-import logging
-import time
+#!/usr/bin/env python
 
-import synthesepy
-import synthesepy.daemon
-import synthesepy.db_backends
-import synthesepy.env
-import synthesepy.sqlite_to_mysql
-import synthesepy.test.main
-import synthesepy.utils
+import logging
+import os
+from os.path import join
+import shutil
+import site
+import subprocess
+import sys
 
 log = logging.getLogger(__name__)
 
-
-def rundaemon(args, env):
-    daemon = synthesepy.daemon.Daemon(env)
-    daemon.start()
-    # TODO: show URL in info message.
-    log.info('Daemon running, press ctrl-c to stop')
-    try:
-        while True:
-            time.sleep(10)
-    except KeyboardInterrupt:
-        print '^C'
-
-    log.info('Stopping daemon')
-    daemon.stop()
+SOURCE_PATH = "@PROJECT_SOURCE_DIR@"
 
 
-def stopdaemon(args, env):
-    # TODO: should use the HTTP method to stop the daemon once it works.
-    synthesepy.utils.kill_listening_processes(env.port)
+class Config(object):
+    pyenv_path = None
+    tools_path = None
+    env_type = None
+    env_path = None
 
 
-def runtests(args, env):
-    tester = synthesepy.test.main.Tester(env, args)
-    tester.run_tests(args.suites)
+class Bootstrap(object):
+    def __init__(self):
+        bootstrap_script = os.environ.get('SYNTHESE_BOOTSTRAP_SCRIPT', __file__)
+        bootstrap_script = os.path.abspath(bootstrap_script)
 
+        self.bootstrap_script_dir = os.path.dirname(bootstrap_script)
 
-def initdb(args, env):
-    backend = synthesepy.db_backends.create_backend(env, args.conn_string)
-    backend.init_db()
+        if os.path.isdir(
+            os.path.join(
+                self.bootstrap_script_dir, os.pardir,
+                'share', 'synthese', 'tools'
+            )):
+            log.debug('Running script from installed location')
+            self.config = self._get_installed_config()
+            return
 
+        self.config = self._get_source_config()
 
-def sqlite_to_mysql(args, env):
-    synthesepy.sqlite_to_mysql.convert(env, args.sourceconn, args.targetconn)
+    def _get_user_storage_path(self):
+        userbase = site.getuserbase() if site.getuserbase else site.USER_BASE
+        # TODO: should we add a version too?
+        storage_path = os.path.join(userbase, 'synthese')
+        if not os.path.isdir(storage_path):
+            os.makedirs(storage_path)
+        return storage_path
+
+    def _get_installed_config(self):
+        config = Config()
+        config.env_type = 'installed'
+        config.env_path = os.path.normpath(
+            os.path.join(self.bootstrap_script_dir, os.pardir)
+        )
+        config.pyenv_path = join(
+            config.env_path, 'share', 'synthese', 'env'
+        )
+        config.tools_path = join(
+            config.env_path, 'share', 'synthese', 'tools'
+        )
+        return config
+
+    def _is_scons_objdir(self, path):
+        # Scons build directories are in a build/{debug,release} directories
+
+        if os.path.basename(path) not in ('debug', 'release'):
+            return False
+
+        parent_dir = os.path.normpath(os.path.join(path, os.pardir))
+        if os.path.basename(parent_dir) != 'build':
+            return False
+
+        return True
+
+    def _is_cmake_objdir(self, path):
+        return os.path.isfile(os.path.join(path, 'CMakeCache.txt'))
+
+    def _get_source_config(self):
+        config = Config()
+        config.pyenv_path = join(self._get_user_storage_path(), 'env')
+
+        if SOURCE_PATH.startswith('@'):
+            # This assumes the script is run from the source directory
+            source_path = os.path.normpath(
+                join(self.bootstrap_script_dir, os.pardir)
+            )
+        else:
+            source_path = SOURCE_PATH
+
+        config.tools_path = join(source_path, 'tools')
+        if not os.path.isdir(config.tools_path):
+            raise Exception(
+                'Can\'t find tools path at %s. Executing '
+                'synthese.py from wrong location?' % config.tools_path
+            )
+
+        if not SOURCE_PATH.startswith('@'):
+            config.env_path = self.bootstrap_script_dir
+            # Try to guess env
+            if self._is_scons_objdir(config.env_path):
+                config.env_type = 'scons'
+            elif self._is_cmake_objdir(config.env_path):
+                config.env_type = 'cmake'
+
+        log.debug('Source config: %s', config.__dict__)
+        return config
+
+    def _get_env_executable(self, executable):
+        if sys.platform == 'win32':
+            return join(self.config.pyenv_path, 'Scripts', executable + '.exe')
+
+        return join(self.config.pyenv_path, 'bin', executable)
+
+    def _do_create_env(self):
+        c = self.config
+
+        virtualenv_script = join(c.tools_path, 'virtualenv.py')
+        log.info('Installing Python environment...')
+        subprocess.check_call([
+            sys.executable, virtualenv_script, '--distribute', c.pyenv_path
+        ])
+
+        log.info('Installing dependencies...')
+        requirements_files = [
+            join(c.tools_path, 'requirements.txt'),
+            join(c.tools_path, 'requirements_%s.txt' % sys.platform),
+        ]
+
+        download_cache = join(self._get_user_storage_path(), 'pip_download_cache')
+
+        for requirements_file in requirements_files:
+            if not os.path.isfile(requirements_file):
+                continue
+            subprocess.check_call([
+                self._get_env_executable('pip'),
+                'install',
+                '--download-cache=%s' % download_cache,
+                '-r',
+                requirements_file
+            ])
+
+        # Installing MySQLdb on Windows with pip fails.
+        if sys.platform == 'win32':
+            try:
+                import MySQLdb
+            except ImportError:
+                raise Exception(
+                    'MySQLdb library not found. Reinstall and try again.\n'
+                    'For Python 2.7, you can try http://www.codegood.com/archives/129'
+                )
+
+    def maybe_create_pyenv(self):
+        if os.path.isdir(self.config.pyenv_path):
+            return
+
+        try:
+            self._do_create_env()
+        except Exception, e:
+            shutil.rmetree(self.config.pyenv_path)
+            raise e
+
+    def run_synthese(self):
+        c = self.config
+
+        if sys.platform == 'win32':
+            python = join(c.pyenv_path, 'Scripts', 'python.exe')
+        else:
+            python = join(c.pyenv_path, 'bin', 'python')
+
+        if c.env_type:
+            os.environ['SYNTHESE_ENV_TYPE'] = c.env_type
+        if c.env_path:
+            os.environ['SYNTHESE_ENV_PATH'] = c.env_path
+
+        pythonpath = os.environ.get('PYTHONPATH', '')
+        if pythonpath:
+            pythonpath += os.pathsep
+        os.environ['PYTHONPATH'] = pythonpath + c.tools_path
+
+        subprocess.call(
+            [self._get_env_executable('python'), '-m', 'synthesepy.cli'] + sys.argv[1:],
+            env=os.environ
+        )
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Synthese management tool')
-    parser.add_argument('-t', '--env-type', choices=['scons', 'cmake', 'installed'])
-    parser.add_argument('-b', '--env-path', help='Env path')
-    parser.add_argument('-m', '--mode', choices=['release', 'debug'])
-    parser.add_argument('-p', '--port', type=int, default=8080)
-    parser.add_argument('-d', '--dbconn', default='sqlite://', dest='conn_string')
-    parser.add_argument('-v', '--verbose', action='store_true', default=False)
-    parser.add_argument('-s', '--stdout', help='Log daemon output to stdout',
-                        action='store_true', default=False, dest='log_stdout')
-    parser.add_argument('--static-dir', help='Directory containing static files '
-                        'served by the HTTP proxy')
-    parser.add_argument('-e', '--extra-params',
-                        help='Daemon extra parameters, using format '
-                             '"param0=value0 param1=value1"')
+    logging.basicConfig(level=logging.DEBUG if '-v' in sys.argv else logging.INFO)
 
-    subparsers = parser.add_subparsers(help='sub-command help')
-
-    parser_rundaemon = subparsers.add_parser('rundaemon')
-    parser_rundaemon.set_defaults(func=rundaemon)
-
-    parser_stopdaemon = subparsers.add_parser('stopdaemon')
-    parser_stopdaemon.set_defaults(func=stopdaemon)
-
-    parser_runtests = subparsers.add_parser('runtests')
-    # for python suite
-    parser_runtests.add_argument('--dbconns', nargs='+', dest='conn_strings',
-                                 default=[])
-    parser_runtests.add_argument(
-        '--no-init',
-        help='Don\'t start/stop the daemon or initialize the db. '
-             'Can be used to reuse an already running daemon',
-        action='store_true', default=False
-    )
-
-    parser_runtests.add_argument(
-        'suites', nargs='*',
-        help='List of test suites to run. Choices: style, python, cpp'
-    )
-    parser_runtests.set_defaults(func=runtests)
-
-    parser_initdb = subparsers.add_parser('initdb')
-    parser_initdb.set_defaults(func=initdb)
-
-    parser_sqlite_to_mysql = subparsers.add_parser('sqlite_to_mysql')
-    parser_sqlite_to_mysql.set_defaults(func=sqlite_to_mysql)
-    parser_sqlite_to_mysql.add_argument('--sourceconn')
-    parser_sqlite_to_mysql.add_argument('--targetconn')
-
-    args = parser.parse_args()
-
-    logging.basicConfig(level=(logging.DEBUG if args.verbose else logging.INFO))
-
-    env = synthesepy.env.create_env(args.env_type, args.env_path, args.mode)
-    env.port = args.port
-    env.wsgi_proxy_port = env.port + 1
-    env.conn_string = args.conn_string
-    env.log_stdout = args.log_stdout
-    env.static_dir = args.static_dir
-    env.extra_params = args.extra_params
-
-    log.debug('Args: %s', args)
-    args.func(args, env)
+    bootstrap = Bootstrap()
+    bootstrap.maybe_create_pyenv()
+    if '--create-pyenv-only' not in sys.argv:
+        bootstrap.run_synthese()
---
+++ tools/synthesepy/cli.py
@@ -0,0 +1,115 @@
+import argparse
+import logging
+import time
+
+import synthesepy
+import synthesepy.daemon
+import synthesepy.db_backends
+import synthesepy.env
+import synthesepy.sqlite_to_mysql
+import synthesepy.test.main
+import synthesepy.utils
+
+log = logging.getLogger(__name__)
+
+
+def rundaemon(args, env):
+    daemon = synthesepy.daemon.Daemon(env)
+    daemon.start()
+    # TODO: show URL in info message.
+    log.info('Daemon running, press ctrl-c to stop')
+    try:
+        while True:
+            time.sleep(10)
+    except KeyboardInterrupt:
+        print '^C'
+
+    log.info('Stopping daemon')
+    daemon.stop()
+
+
+def stopdaemon(args, env):
+    # TODO: should use the HTTP method to stop the daemon once it works.
+    synthesepy.utils.kill_listening_processes(env.port)
+
+
+def runtests(args, env):
+    tester = synthesepy.test.main.Tester(env, args)
+    tester.run_tests(args.suites)
+
+
+def initdb(args, env):
+    backend = synthesepy.db_backends.create_backend(env, args.conn_string)
+    backend.init_db()
+
+
+def sqlite_to_mysql(args, env):
+    synthesepy.sqlite_to_mysql.convert(env, args.sourceconn, args.targetconn)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Synthese management tool')
+    parser.add_argument('-t', '--env-type', choices=['scons', 'cmake', 'installed'])
+    parser.add_argument('-b', '--env-path', help='Env path')
+    parser.add_argument('-m', '--mode', choices=['release', 'debug'])
+    parser.add_argument('-p', '--port', type=int, default=8080)
+    parser.add_argument('-d', '--dbconn', default='sqlite://', dest='conn_string')
+    parser.add_argument('-v', '--verbose', action='store_true', default=False)
+    parser.add_argument('-s', '--stdout', help='Log daemon output to stdout',
+                        action='store_true', default=False, dest='log_stdout')
+    parser.add_argument('--static-dir', help='Directory containing static files '
+                        'served by the HTTP proxy')
+    parser.add_argument('-e', '--extra-params',
+                        help='Daemon extra parameters, using format '
+                             '"param0=value0 param1=value1"')
+
+    subparsers = parser.add_subparsers(help='sub-command help')
+
+    parser_rundaemon = subparsers.add_parser('rundaemon')
+    parser_rundaemon.set_defaults(func=rundaemon)
+
+    parser_stopdaemon = subparsers.add_parser('stopdaemon')
+    parser_stopdaemon.set_defaults(func=stopdaemon)
+
+    parser_runtests = subparsers.add_parser('runtests')
+    # for python suite
+    parser_runtests.add_argument('--dbconns', nargs='+', dest='conn_strings',
+                                 default=[])
+    parser_runtests.add_argument(
+        '--no-init',
+        help='Don\'t start/stop the daemon or initialize the db. '
+             'Can be used to reuse an already running daemon',
+        action='store_true', default=False
+    )
+
+    parser_runtests.add_argument(
+        'suites', nargs='*',
+        help='List of test suites to run. Choices: style, python, cpp'
+    )
+    parser_runtests.set_defaults(func=runtests)
+
+    parser_initdb = subparsers.add_parser('initdb')
+    parser_initdb.set_defaults(func=initdb)
+
+    parser_sqlite_to_mysql = subparsers.add_parser('sqlite_to_mysql')
+    parser_sqlite_to_mysql.set_defaults(func=sqlite_to_mysql)
+    parser_sqlite_to_mysql.add_argument('--sourceconn')
+    parser_sqlite_to_mysql.add_argument('--targetconn')
+
+    args = parser.parse_args()
+
+    logging.basicConfig(level=(logging.DEBUG if args.verbose else logging.INFO))
+
+    env = synthesepy.env.create_env(args.env_type, args.env_path, args.mode)
+    env.port = args.port
+    env.wsgi_proxy_port = env.port + 1
+    env.conn_string = args.conn_string
+    env.log_stdout = args.log_stdout
+    env.static_dir = args.static_dir
+    env.extra_params = args.extra_params
+
+    log.debug('Args: %s', args)
+    args.func(args, env)
+
+if __name__ == '__main__':
+    main()
