ef2b91b170fcf17a18451548f4e381febc657626
Sylvain Pasche <sylvain.pasche@gmail.com>
2011-06-05 22:19:18 +0000

    revert previous commit. -v is given explicitly only for tests

--- tools/jenkins_build.py
+++ tools/jenkins_build.py
@@ -40,12 +40,10 @@ default_config = {
 config = {}
 
 def run_synthesepy(command, tool, mode, global_args, command_args, environ,
-                   verbose=False, ignore_failure=False):
+                   ignore_failure=False):
 
     thisdir = os.path.abspath(os.path.dirname(__file__))
     synthesepy_path = os.path.join(thisdir, "synthese.py")
-    if verbose:
-        global_args.append("-v")
     args = ([sys.executable, synthesepy_path, "-t", tool, "-m", mode] +
         global_args + [command] + command_args)
     log.debug("Running command: %s", args)
@@ -80,11 +78,7 @@ def build(options):
     environ["SYNTHESE_MYSQL_DB"] = c["mysql_db"]
 
     run_synthesepy(
-        "build", options.tool, options.mode,
-        [],
-        ["--with-mysql"],
-        environ,
-        options.verbose)
+        "build", options.tool, options.mode, [], ["--with-mysql"], environ)
 
     dbconns = [
         "mysql://debug=1,{mysql_params},db={mysql_db}".format(**c), "sqlite://"]
@@ -92,9 +86,7 @@ def build(options):
         "runtests", options.tool, options.mode,
         ["-v", "-s", "-p", c["port"]],
         ["--dbconns"] + dbconns,
-        environ,
-        options.verbose,
-        options.test_failure_not_fatal)
+        environ, options.test_failure_not_fatal)
 
 
 if __name__ == "__main__":
