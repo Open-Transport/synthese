6a2eff48d453f97c4376086d88577159586aba00
Sylvain Pasche <spasche.rcs@gmail.com>
2011-06-16 14:25:05 +0000

    If SYNTHESE_MYSQL_RESTART_COMMAND or SYNTHESE_MYSQL_RESTART_MANUAL env variables are not defined, abort the test without a failure

--- test/10_db/MySQLReconnectTest.cpp
+++ test/10_db/MySQLReconnectTest.cpp
@@ -98,11 +98,9 @@ BOOST_AUTO_TEST_CASE(MySQLReconnect)
 	}
 	else
 	{
-		BOOST_CHECK_MESSAGE(
-			false,
-			"Neither SYNTHESE_MYSQL_RESTART_COMMAND or SYNTHESE_MYSQL_RESTART_MANUAL "
-			"environment variable defined. Can't run test"
-		);
+		cout << 
+			"WARNING: Neither SYNTHESE_MYSQL_RESTART_COMMAND or SYNTHESE_MYSQL_RESTART_MANUAL "
+			"environment variable defined. Can't run test" << endl;
 		return;
 	}
 
