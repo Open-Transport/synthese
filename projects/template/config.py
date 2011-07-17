f90a38c1d8596f398cf860aa9dadc34bf9eda068
Sylvain Pasche <sylvain.pasche@gmail.com>
2011-07-17 22:42:34 +0000

    Add a new projects directory for storing the template project and other projects in the future

---
+++ projects/template/config.py
@@ -0,0 +1,3 @@
+# Sample project configuration file
+
+#conn_string = 'mysql://host=localhost,user=synthese,passwd=synthese,db=synthese'
---
+++ projects/template/sites/admin/config.py
@@ -0,0 +1,3 @@
+id = 7036874417766401
+# For compatibility with already deployed projects.
+base_path = 'synthese3/'
