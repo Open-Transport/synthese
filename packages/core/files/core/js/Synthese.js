8c2d2a06f6117119656cc5d307106da6f80febc8
Sylvain Pasche <sylvain.pasche@gmail.com>
2011-07-18 21:38:58 +0000

    Some refactoring of the stop selector page

--- packages/core/files/core/js/Synthese.js
+++ packages/core/files/core/js/Synthese.js
@@ -12,6 +12,28 @@ try {
 var Synthese = {
   URL: "/synthese",
 
+  init: function(siteId) {
+    function parseQueryString(str) {
+      var paramArray = str.split("&");
+      var regex = /^([^=]+)=(.*)$/;
+      var params = {};
+      for (var i = 0, sz = paramArray.length; i < sz; i++) {
+        var match = regex.exec(paramArray[i]);
+        if (!match)
+          continue;
+        params[decodeURIComponent(match[1])] = decodeURIComponent(match[2]);
+      }
+      return params;
+    }
+    
+    this.queryStringOptions = parseQueryString(location.search.slice(1));
+    this.siteId = siteId || this.queryStringOptions.siteId;
+    if (!this.siteId) {
+      console.warn("Missing siteId parameter");
+      alert("Missing siteId parameter");
+    }
+  },
+  
   callService: function(serviceName, args, dataType) {
     console.log("Synthese.callService", args);
     args["SERVICE"] = serviceName;
