b25be60731e7af7a8f06d6402797a3894de70151
Sylvain Pasche <spasche.rcs@gmail.com>
2011-07-18 14:51:27 +0000

    Stop selector interface, extension of CityBrowser. Some API refactoring

---
+++ packages/map/files/map/js/StopSelector.js
@@ -0,0 +1,47 @@
+var StopSelector = Backbone.View.extend({
+
+  // TODO: i18n
+  template: $.template(null, [
+    '<h1>',
+    '  <a href="#" class="close">fermer</a>',
+    '  Sélectionnez un arrêt',
+    '</h1>',
+    '<div class="cityBrowser"></div>'
+  ].join('\n')),
+
+  events: {
+    "click .close": "close",
+  },
+
+  initialize: function(options) {
+
+    this.cityBrowserView =  new CityBrowser({
+      mapOptions: options.mapOptions,
+      noMapAutoRender: true,
+    });
+
+    var self = this;
+    this.cityBrowserView.syntheseMap.bind("all", function(eventName, arg) {
+      self.trigger(eventName, arg);
+    });
+
+    this.render();
+  },
+
+  render: function() {
+    $(this.el).empty().addClass("stopSelector");
+    $.tmpl(this.template, {}).appendTo(this.el);
+
+    this.$(".cityBrowser").replaceWith(this.cityBrowserView.el);
+    this.cityBrowserView.renderMap();
+  },
+
+  close: function() {
+    $(this.el).hide();
+  },
+
+  show: function() {
+    $(this.el).show();
+  },
+
+});
