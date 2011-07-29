bf6674a685aa84b961afc3a80e010cf7bdc8f664
Sylvain Pasche <sylvain.pasche@gmail.com>
2011-07-29 16:19:05 +0000

    Add a configuration option for the min number of chars before firing autocompletion.

--- packages/map/files/map/js/legacy/app.setup.js
+++
@@ -1,8 +0,0 @@
-window.app.setup = {
-    // valid API key for http://www.tisseo-urbain.fr/:
-    // (no more useful here but kept as a reference)
-    //key: 'ABQIAAAAzH3Bfejt36YChlg6FVbDAhTgeFP99OVUaA-CxgcZRxphs5kgmxRhJ5Uxs52aRTa9qVNILPhbHIy0NA',
-    
-    // zoom level for start and stop maps (up to 17 = closer)
-    zoomLevelForPoint: 15
-};
