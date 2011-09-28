9fbcb2f5bbe13c970eaaeb6f56e6d86a60adf691
Sylvain Pasche <sylvain.pasche@gmail.com>
2011-09-28 16:17:20 +0000

    Dummy statement to prevent an import error.

--- packages/core/install.sql
+++ packages/core/install.sql
@@ -4,3 +4,6 @@
 
 -- Note: 17732927827738624 = 63 << 48 | 1 << 32 (63 = table id, 1 = node id)
 -- REPLACE INTO t063_web_pages(id,site_id,up_id,rank,title,start_time,end_time,mime_type,do_not_use_template,content1) VALUES(17732927827738624 | 2000, 7036874417766401, 0, 0, 'Maxid allocation page',NULL,NULL,'text/html',1,'Dummy page to allocate reserved ids');
+
+-- XXX: executescript on linux fails if there's no statements to run in this file.
+select 1;
