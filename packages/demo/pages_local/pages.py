95faadd72774804a7111b2ee5a6e90199086b447
Sylvain Pasche <sylvain.pasche@gmail.com>
2011-10-07 19:21:45 +0000

    Add web pages to some packages (sometimes converted from install.sql).

---
+++ packages/demo/pages_local/pages.py
@@ -0,0 +1,13 @@
+pages = [{
+    'id': pid(0),
+    'rank': 0,
+    'title': 'Template_',
+    'content1': 'file:template.html',
+}, {
+    'id': pid(1),
+    'rank': 0,
+    'up_id': pid(0),
+    'title': 'Home',
+    'smart_url_path': '/',
+    'content1': 'file:home.html',
+}]
