91e90cba8d791343259525e3e4e87867c8152451
Sylvain Pasche <sylvain.pasche@gmail.com>
2011-10-07 19:20:49 +0000

    Add web pages to some packages (sometimes converted from install.sql).

--- packages/demo/config.py
+++ packages/demo/config.py
@@ -1,1 +1,2 @@
 dependencies = ('routePlanner', 'testData',)
+base_page_index = 5
--- packages/demo/install.sql
+++ packages/demo/install.sql
@@ -1,11 +1,2 @@
-
-REPLACE INTO "t025_sites" VALUES(7036874417766411,'Demo',NULL,NULL,1,0,10,14,'02:30:00|02:30:00|Journée,02:30:00|12:00:00|Matin,11:00:00|15:00:00|Midi,14:00:00|19:00:00|Après midi,18:00:00|02:30:00|Soir',0,'/synthese','17732927827738728');
-
--- Site template
-REPLACE INTO "t063_web_pages" VALUES(17732927827738728,7036874417766411,0,0,'Template','<h1><@title@></h1>
-
-Site Template
-
-<@content@>','2011-07-12 13:04:40',NULL,'','','','',0,0,'','',0,1);
--- Home page
-REPLACE INTO "t063_web_pages" VALUES(17732927827738729,7036874417766411,17732927827738728,0,'home','<p>Home Page</p>','2011-07-12 13:11:02',NULL,'','','','',0,0,'','',0,0);
+/* TODO: rename cient_url to client_url once complex schema updates are in place. */
+REPLACE INTO t025_sites(id,name,start_date,end_date,cient_url,default_page_template_id) VALUES(7036874417766411, 'Demo', NULL, NULL, '/synthese',17732927936790528);
