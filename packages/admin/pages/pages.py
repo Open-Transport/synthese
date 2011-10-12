44f10284680251639d18e1078c361e1b73e5c959
Sylvain Pasche <spasche.rcs@gmail.com>
2011-10-12 10:50:40 +0000

    Put compatibility pages under the existing admin site.

--- packages/admin/pages/pages.py
+++ packages/admin/pages/pages.py
@@ -1,12 +1,19 @@
-# TODO: use the id from pid() commented below instead,
+# TODO: use the id from pid() commented below instead and remove the site_id
 # once the admin URL has been migrated.
 BACKCOMPAT_ID_MAIN = 177329235327713281
 BACKCOMPAT_ID_POS_ELEM = 177329235327713283
 BACKCOMPAT_ID_TREE_ELEM = 177329235327713282
+BACKCOMPAT_ADMIN_SITE_ID = 7036874417766401
 
 pages = [{
+    # TODO: make all other pages children of this one.
+    'id': pid(0),
+    'title': 'admin',
+    'content1': '',
+}, {
     #'id': pid(0),
     'id': BACKCOMPAT_ID_MAIN,
+    'site_id': BACKCOMPAT_ADMIN_SITE_ID,
     'rank': 0,
     'smart_url_path': ':admin_main',
     'content1': 'file:admin_main.html',
@@ -16,6 +23,7 @@ pages = [{
     #'up_id': pid(0),
     'id': BACKCOMPAT_ID_POS_ELEM,
     'up_id': BACKCOMPAT_ID_MAIN,
+    'site_id': BACKCOMPAT_ADMIN_SITE_ID,
     'rank': 0,
     'smart_url_path': ':admin_pos_elem',
     'content1': 'file:admin_pos_elem.html',
@@ -25,6 +33,7 @@ pages = [{
     #'up_id': pid(0),
     'id': BACKCOMPAT_ID_TREE_ELEM,
     'up_id': BACKCOMPAT_ID_MAIN,
+    'site_id': BACKCOMPAT_ADMIN_SITE_ID,
     'rank': 1,
     'smart_url_path': ':admin_tree_elem',
     'content1': 'file:admin_tree_elem.html',
