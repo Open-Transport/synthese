066f3e83dd4b4d1bcdf0aeac69169741ba2ec6dc
Sylvain Pasche <sylvain.pasche@gmail.com>
2011-10-12 10:42:03 +0000

    route planner pages config.

---
+++ packages/routePlanner/pages/pages.py
@@ -0,0 +1,230 @@
+pages = [{
+    'id': pid(0),
+    'rank': 0,
+    'title': 'routePlanner',
+    'content1': 'TODO',
+}, {
+    'id': pid(1), # 17732927894847489
+    'rank': 0,
+    'up_id': pid(0),
+    'title': 'JSON Journey',
+    'content1': 'file:json_journey/index.html',
+}, {
+    'id': pid(2), # 17732927894847490
+    'rank': 0,
+    'up_id': pid(1),
+    'title': 'Used Line',
+    'content1': 'file:json_journey/Used_Line.html',
+}, {
+    'id': pid(3), # 17732927894847491
+    'rank': 0,
+    'up_id': pid(1),
+    'title': 'Stop',
+    'content1': 'file:json_journey/Stop.html',
+}, {
+    'id': pid(4), # 17732927894847492
+    'rank': 0,
+    'up_id': pid(1),
+    'title': 'Service',
+    'content1': 'file:json_journey/Service.html',
+},  {
+    'id': pid(5), # 17732927894847493
+    'rank': 0,
+    'up_id': pid(1),
+    'title': 'Junction',
+    'content1': 'file:json_journey/Junction.html',
+}]
+
+
+# TODO: integrate these other pages:
+"""
+  "17732927827738629": {
+    "id": 17732927827738629, 
+    "path": [
+      "by_title", 
+      "Templates_dir", 
+      "Recherche__dir", 
+      "Rangee_horaire.html"
+    ], 
+    "smart_url_path": "", 
+    "title": "Rang\u00e9e horaire", 
+    "up_id": 17732927827738628
+  }, 
+  "17732927827738630": {
+    "id": 17732927827738630, 
+    "path": [
+      "by_title", 
+      "Templates_dir", 
+      "Recherche__dir", 
+      "Cellule_lignes_empruntees.html"
+    ], 
+    "smart_url_path": "", 
+    "title": "Cellule lignes emprunt\u00e9es", 
+    "up_id": 17732927827738628
+  }, 
+  "17732927827738631": {
+    "id": 17732927827738631, 
+    "path": [
+      "by_title", 
+      "Templates_dir", 
+      "Recherche__dir", 
+      "Cellule_duree.html"
+    ], 
+    "smart_url_path": "", 
+    "title": "Cellule dur\u00e9e", 
+    "up_id": 17732927827738628
+  }, 
+  "17732927827738632": {
+    "id": 17732927827738632, 
+    "path": [
+      "by_title", 
+      "Templates_dir", 
+      "Recherche__dir", 
+      "Cellule_reservation.html"
+    ], 
+    "smart_url_path": "", 
+    "title": "Cellule r\u00e9servation", 
+    "up_id": 17732927827738628
+  }, 
+  "17732927827738633": {
+    "id": 17732927827738633, 
+    "path": [
+      "by_title", 
+      "Templates_dir", 
+      "Recherche__dir", 
+      "Feuille_de_route.html"
+    ], 
+    "smart_url_path": "", 
+    "title": "Feuille de route", 
+    "up_id": 17732927827738628
+  }, 
+  "17732927827738634": {
+    "id": 17732927827738634, 
+    "path": [
+      "by_title", 
+      "Templates_dir", 
+      "Recherche__dir", 
+      "Trajet_carto_JSON.html"
+    ], 
+    "smart_url_path": "", 
+    "title": "Trajet carto JSON", 
+    "up_id": 17732927827738628
+  }, 
+  "17732927827738635": {
+    "id": 17732927827738635, 
+    "path": [
+      "by_title", 
+      "Templates_dir", 
+      "Recherche__dir", 
+      "Rangee_hor_dir", 
+      "Cellule_horaire.html"
+    ], 
+    "smart_url_path": "", 
+    "title": "Cellule horaire", 
+    "up_id": 17732927827738629
+  }, 
+  "17732927827738636": {
+    "id": 17732927827738636, 
+    "path": [
+      "by_title", 
+      "Templates_dir", 
+      "Recherche__dir", 
+      "Cellule_li_dir", 
+      "Ligne_empruntee.html"
+    ], 
+    "smart_url_path": "", 
+    "title": "Ligne emprunt\u00e9e", 
+    "up_id": 17732927827738630
+  }, 
+  "17732927827738637": {
+    "id": 17732927827738637, 
+    "path": [
+      "by_title", 
+      "Templates_dir", 
+      "Recherche__dir", 
+      "Feuille_de_dir", 
+      "Cellule_arret.html"
+    ], 
+    "smart_url_path": "", 
+    "title": "Cellule arr\u00eat", 
+    "up_id": 17732927827738633
+  }, 
+  "17732927827738638": {
+    "id": 17732927827738638, 
+    "path": [
+      "by_title", 
+      "Templates_dir", 
+      "Recherche__dir", 
+      "Feuille_de_dir", 
+      "Cellule_service.html"
+    ], 
+    "smart_url_path": "", 
+    "title": "Cellule service", 
+    "up_id": 17732927827738633
+  }, 
+  "17732927827738639": {
+    "id": 17732927827738639, 
+    "path": [
+      "by_title", 
+      "Templates_dir", 
+      "Recherche__dir", 
+      "Feuille_de_dir", 
+      "Cellule_jonction.html"
+    ], 
+    "smart_url_path": "", 
+    "title": "Cellule jonction", 
+    "up_id": 17732927827738633
+  }, 
+  "17732927827738640": {
+    "id": 17732927827738640, 
+    "path": [
+      "by_title", 
+      "Templates_dir", 
+      "Recherche__dir", 
+      "Trajet_car_dir", 
+      "Ligne_empruntee.html"
+    ], 
+    "smart_url_path": "", 
+    "title": "Ligne emprunt\u00e9e", 
+    "up_id": 17732927827738634
+  }, 
+  "17732927827738641": {
+    "id": 17732927827738641, 
+    "path": [
+      "by_title", 
+      "Templates_dir", 
+      "Recherche__dir", 
+      "Trajet_car_dir", 
+      "Arret.html"
+    ], 
+    "smart_url_path": "", 
+    "title": "Arr\u00eat", 
+    "up_id": 17732927827738634
+  }, 
+  "17732927827738642": {
+    "id": 17732927827738642, 
+    "path": [
+      "by_title", 
+      "Templates_dir", 
+      "Recherche__dir", 
+      "Trajet_car_dir", 
+      "Service.html"
+    ], 
+    "smart_url_path": "", 
+    "title": "Service", 
+    "up_id": 17732927827738634
+  }, 
+  "17732927827738643": {
+    "id": 17732927827738643, 
+    "path": [
+      "by_title", 
+      "Templates_dir", 
+      "Recherche__dir", 
+      "Trajet_car_dir", 
+      "Jonction.html"
+    ], 
+    "smart_url_path": "", 
+    "title": "Jonction", 
+    "up_id": 17732927827738634
+  }, 
+"""
