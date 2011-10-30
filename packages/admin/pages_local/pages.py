8ea868abeb1a352068fc12d1ccd8456ef9c101b7
Sylvain Pasche <sylvain.pasche@gmail.com>
2011-10-30 23:52:45 +0000

    local pages for admin branding.

---
+++ packages/admin/pages_local/pages.py
@@ -0,0 +1,19 @@
+pages = [{
+    'id': pid(0),
+    'title': 'admin_local',
+    'content1': '',
+}, {
+    'id': pid(1), # 17732927869681665
+    'up_id': pid(0),
+    'rank': 0,
+    'smart_url_path': ':admin_brand_topmenu',
+    'content1': 'file:admin_brand_topmenu.html',
+    'do_not_use_template': True,
+}, {
+    'id': pid(2), # 17732927869681666
+    'up_id': pid(0),
+    'rank': 1,
+    'smart_url_path': ':admin_brand_css',
+    'content1': 'file:admin_brand.css',
+    'do_not_use_template': True,
+}]
