7a0b384ffa5ed739e47019d3f12b0003dee5d765
Sylvain Pasche <sylvain.pasche@gmail.com>
2011-11-13 22:02:27 +0000

    Put all pages below a 'packages' directory. Apache configuration with compression and other goodies in the core package. Change where admin pages are stored.

---
+++ packages/core/pages_local/pages.py
@@ -0,0 +1,19 @@
+pages = [{
+    'id': pid(0),
+    'rank': 0,
+    'title': 'packages',
+    'content1': '',
+}, {
+    'id': pid(1),
+    'rank': 0,
+    'up_id': pid(0),
+    'title': 'core_local',
+    'content1': '',
+}, {
+    'id': pid(2),
+    'rank': 0,
+    'up_id': pid(1),
+    'title': 'Sample page',
+    'smart_url_path': '/sample.html',
+    'content1': 'file:sample.html',
+}]
--- packages/demo/install.sql
+++ packages/demo/install.sql
@@ -1,2 +1,3 @@
 /* TODO: rename cient_url to client_url once complex schema updates are in place. */
-REPLACE INTO t025_sites(id,name,start_date,end_date,cient_url,default_page_template_id) VALUES(7036874417766411, 'Demo', NULL, NULL, '/synthese',17732927936790528);
+/* default template id: 17732927929122816 has page = 0, local_site_id = 11 */
+REPLACE INTO t025_sites(id,name,start_date,end_date,cient_url,default_page_template_id) VALUES(7036874417766411, 'Demo', NULL, NULL, '/synthese',17732927929122816);
