d29dea7d3eec89d77ca62923bdd49446ea373b77
Sylvain Pasche <sylvain.pasche@gmail.com>
2011-11-22 10:59:41 +0000

    Version mechanism to prevent using old cached scripts

--- packages/core/files/core/js/init.js
+++ packages/core/files/core/js/init.js
@@ -1,5 +1,17 @@
+(function() {
 if (!window.requireJSConfig)
   window.requireJSConfig = {};
+
+function getVersion() {
+  var scripts = document.getElementsByTagName("script");
+  for (var i = 0; i < scripts.length; i++) {
+    var script = scripts[i];
+    var src = scripts[i].getAttribute("src");
+    if (!src || src.search(/\/init.js$/) == -1)
+      continue;
+    return script.getAttribute("data-version");
+  }
+}
 
 requireJSConfig.paths = {
   "text": "core/vendor/requirejs/text",
@@ -7,9 +19,15 @@ requireJSConfig.paths = {
 };
 requireJSConfig.baseUrl = requireJSConfig.baseUrl || "/";
 
+var version = getVersion();
+if (version)
+  requireJSConfig.urlArgs = "v=" + version;
+
 var locStorage = window.localStorage || {};
 if (locStorage.debug) {
   requireJSConfig.urlArgs = "bust=" + (new Date()).getTime();
 }
 
 require.config(requireJSConfig);
+
+})();
