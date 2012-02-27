1876aa3a9f7e98ed9116fa188b4c18459b990a20
Sylvain Pasche <sylvain.pasche@gmail.com>
2012-02-27 10:07:29 +0000

    config for udf proxy options

--- tools/synthesepy/external_tools.py
+++ tools/synthesepy/external_tools.py
@@ -131,14 +131,15 @@ class UDFProxySupervisor(Supervisor):
     def get_command(self):
         config = self.project.config
         return ('python {script} -ns -p {udf_proxy_port} --log-path={log_path} '
-            '-t http://localhost:{synthese_port} start'.format(
+            '-t http://localhost:{synthese_port} {udf_proxy_options} start'.format(
             script=join(
                 self.project.env.source_path, 'utils',
                 'udf_proxy', 'udf_proxy.py'),
             udf_proxy_port=config.udf_proxy_port,
             log_path=join(self.project.config.project_path,
                 'logs', 'udf_proxy.log'),
-            synthese_port=config.port))
+            synthese_port=config.port,
+            udf_proxy_options=config.udf_proxy_options))
 
 
 class WSGI(ExternalTool):
