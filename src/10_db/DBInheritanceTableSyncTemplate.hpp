cc5a3cabc492319904c19b828a3039a17f4dc4ae
Hugues Romain <hromain@rcsmobility.com>
2012-03-09 07:07:03 +0000

    Linux build 806

--- src/10_db/DBInheritanceTableSyncTemplate.hpp
+++ src/10_db/DBInheritanceTableSyncTemplate.hpp
@@ -55,9 +55,9 @@ namespace synthese
 			static FieldsList GetFieldsList()
 			{
 				FieldsList l;
-				for(size_t i(0); !_FIELDS[i].empty(); ++i)
+				for(size_t i(0); !DBTableSyncTemplate<K>::_FIELDS[i].empty(); ++i)
 				{
-					l.push_back(_FIELDS[i]);
+					l.push_back(DBTableSyncTemplate<K>::_FIELDS[i]);
 				}
 				return l;
 			}
