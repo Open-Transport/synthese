4abc4d162cd9316df8ca2236374105d458730eee
Sylvain Pasche <sylvain.pasche@gmail.com>
2012-03-21 12:04:03 +0000

    udf proxy profiling.

---
+++ utils/udf_proxy/profiling_stats.py
@@ -0,0 +1,11 @@
+import pstats
+
+def generate_stats(stat_name):
+    stream = open('{0}.prof_result'.format(stat_name), 'w')
+    p = pstats.Stats('{0}.prof'.format(stat_name), stream=stream)
+    #kind = 'cumulative'
+    kind = 'time'
+    p.sort_stats(kind).print_stats() 
+
+for stat_name in ('httpd', 'dispatcher'):
+    generate_stats(stat_name)
