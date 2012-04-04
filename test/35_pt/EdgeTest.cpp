0b4677e7c69c475826512110ce6d35abcc459c0b
Hugues Romain <hromain@rcsmobility.com>
2012-04-04 08:43:58 +0000

    fixed unit tests

--- test/35_pt/EdgeTest.cpp
+++ test/35_pt/EdgeTest.cpp
@@ -49,23 +49,23 @@ BOOST_AUTO_TEST_CASE (testEdgeOrderedLinking)
 
 	JourneyPattern l;
 
-	StopArea p1(UNKNOWN_VALUE, true);
-	StopArea p2(UNKNOWN_VALUE, false);
-	StopArea p3(UNKNOWN_VALUE, false);
-	StopArea p4(UNKNOWN_VALUE, false);
-	StopArea p5(UNKNOWN_VALUE, true);
-	StopArea p6(UNKNOWN_VALUE, true);
-	StopArea p7(UNKNOWN_VALUE, false);
-	StopArea p8(UNKNOWN_VALUE, false);
+	StopArea p1(0, true);
+	StopArea p2(0, false);
+	StopArea p3(0, false);
+	StopArea p4(0, false);
+	StopArea p5(0, true);
+	StopArea p6(0, true);
+	StopArea p7(0, false);
+	StopArea p8(0, false);
 
-	StopPoint s1(UNKNOWN_VALUE, "s1", &p1);
-	StopPoint s2(UNKNOWN_VALUE, "s1", &p2);
-	StopPoint s3(UNKNOWN_VALUE, "s1", &p3);
-	StopPoint s4(UNKNOWN_VALUE, "s1", &p4);
-	StopPoint s5(UNKNOWN_VALUE, "s1", &p5);
-	StopPoint s6(UNKNOWN_VALUE, "s1", &p6);
-	StopPoint s7(UNKNOWN_VALUE, "s1", &p7);
-	StopPoint s8(UNKNOWN_VALUE, "s1", &p8);
+	StopPoint s1(0, "s1", &p1);
+	StopPoint s2(0, "s1", &p2);
+	StopPoint s3(0, "s1", &p3);
+	StopPoint s4(0, "s1", &p4);
+	StopPoint s5(0, "s1", &p5);
+	StopPoint s6(0, "s1", &p6);
+	StopPoint s7(0, "s1", &p7);
+	StopPoint s8(0, "s1", &p8);
 
 	DesignatedLinePhysicalStop l1D(0, &l, 0, true, false,0,&s1);
 	DesignatedLinePhysicalStop l2D(0, &l, 1, true, false,0,&s2);
@@ -311,23 +311,23 @@ BOOST_AUTO_TEST_CASE (testEdgeRandomLinking)
 
 	JourneyPattern l;
 
-	StopArea p1(UNKNOWN_VALUE, true);
-	StopArea p2(UNKNOWN_VALUE, false);
-	StopArea p3(UNKNOWN_VALUE, false);
-	StopArea p4(UNKNOWN_VALUE, false);
-	StopArea p5(UNKNOWN_VALUE, true);
-	StopArea p6(UNKNOWN_VALUE, true);
-	StopArea p7(UNKNOWN_VALUE, false);
-	StopArea p8(UNKNOWN_VALUE, false);
+	StopArea p1(0, true);
+	StopArea p2(0, false);
+	StopArea p3(0, false);
+	StopArea p4(0, false);
+	StopArea p5(0, true);
+	StopArea p6(0, true);
+	StopArea p7(0, false);
+	StopArea p8(0, false);
 
-	StopPoint s1(UNKNOWN_VALUE, "s1", &p1);
-	StopPoint s2(UNKNOWN_VALUE, "s1", &p2);
-	StopPoint s3(UNKNOWN_VALUE, "s1", &p3);
-	StopPoint s4(UNKNOWN_VALUE, "s1", &p4);
-	StopPoint s5(UNKNOWN_VALUE, "s1", &p5);
-	StopPoint s6(UNKNOWN_VALUE, "s1", &p6);
-	StopPoint s7(UNKNOWN_VALUE, "s1", &p7);
-	StopPoint s8(UNKNOWN_VALUE, "s1", &p8);
+	StopPoint s1(0, "s1", &p1);
+	StopPoint s2(0, "s1", &p2);
+	StopPoint s3(0, "s1", &p3);
+	StopPoint s4(0, "s1", &p4);
+	StopPoint s5(0, "s1", &p5);
+	StopPoint s6(0, "s1", &p6);
+	StopPoint s7(0, "s1", &p7);
+	StopPoint s8(0, "s1", &p8);
 
 	DesignatedLinePhysicalStop l1D(0, &l, 0, true, false,0,&s1);
 	DesignatedLinePhysicalStop l2D(0, &l, 1, true, false,0,&s2);
@@ -528,14 +528,14 @@ BOOST_AUTO_TEST_CASE (testDRTAreaLinking)
 {
 	JourneyPattern l;
 
-	StopArea p1(UNKNOWN_VALUE, true);
-	StopArea p2(UNKNOWN_VALUE, false);
-	StopArea p3(UNKNOWN_VALUE, false);
-	StopArea p4(UNKNOWN_VALUE, false);
-	StopArea p5(UNKNOWN_VALUE, true);
-	StopArea p6(UNKNOWN_VALUE, true);
-	StopArea p7(UNKNOWN_VALUE, false);
-	StopArea p8(UNKNOWN_VALUE, false);
+	StopArea p1(0, true);
+	StopArea p2(0, false);
+	StopArea p3(0, false);
+	StopArea p4(0, false);
+	StopArea p5(0, true);
+	StopArea p6(0, true);
+	StopArea p7(0, false);
+	StopArea p8(0, false);
 
 	StopPoint s1(1, "s1", &p1);
 	p1.addPhysicalStop(s1);
