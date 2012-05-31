7b8011cf02ef55f1671e31d8e429a420b9309916
Sylvain Pasche <sylvain.pasche@gmail.com>
2012-05-31 16:52:34 +0000

    Remove s3-admin (now moved to a package).

--- s3-admin/res/win32/install_interface_data.bat
+++
@@ -1,5 +0,0 @@
-@REM This script have to be run in the directory of the SQLite database file
-@echo off
-echo Installation of Common/Admin interface data...
-echo .exit | sqlite3 -init ..\..\..\..\..\..\..\data\interface\common\s3-admin\deb\opt\rcs\common-datas3-interface-admin\install.sql config.db3
-pause
