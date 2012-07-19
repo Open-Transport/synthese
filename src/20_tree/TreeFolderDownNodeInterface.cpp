40a0b53f5a71992bcad7574c30fe4fcf0f128b17
Hugues Romain <hromain@rcsmobility.com>
2012-07-19 22:38:21 +0000

    Feature : directory structure for commercial lines

---
+++ src/20_tree/TreeFolderDownNodeInterface.cpp
@@ -0,0 +1,185 @@
+
+/** TreeFolderDownNodeInterface class implementation.
+	@file TreeFolderDownNodeInterface.cpp
+
+	This file belongs to the SYNTHESE project (public transportation specialized software)
+	Copyright (C) 2002 Hugues Romain - RCSmobility <contact@rcsmobility.com>
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#include "TreeFolderDownNodeInterface.hpp"
+
+#include "DBDirectTableSync.hpp"
+#include "DBModule.h"
+#include "TreeFolder.hpp"
+#include "TreeFolderRoot.hpp"
+
+using namespace boost;
+
+namespace synthese
+{
+	using namespace db;
+	using namespace tree;
+	using namespace util;
+
+
+
+	FIELD_DEFINITION_OF_TYPE(Parent, "parent_id", SQL_INTEGER)
+
+
+
+	void ObjectField<Parent, Parent::Type>::GetLinkedObjectsIds(
+		LinkedObjectsIds& list,
+		const Record& record
+	){
+		RegistryKeyType parent_id(record.getDefault<RegistryKeyType>(FIELD.name, 0));
+		if(parent_id > 0)
+		{
+			list.push_back(parent_id);
+		}
+	}
+
+
+
+	void ObjectField<Parent, Parent::Type>::LoadFromRecord(
+		Parent::Type fieldObject,
+		ObjectBase& object,
+		const Record& record,
+		const Env& env
+	){
+		assert(dynamic_cast<TreeFolderDownNodeInterface*>(&object));
+		TreeFolderDownNodeInterface& node(dynamic_cast<TreeFolderDownNodeInterface&>(object));
+
+		if(record.isDefined(FIELD.name))
+		{
+			// Name
+			try
+			{
+				RegistryKeyType id(record.get<RegistryKeyType>(FIELD.name));
+
+				boost::shared_ptr<DBTableSync> ts(DBModule::GetTableSync(decodeTableId(id)));
+				if(!dynamic_cast<DBDirectTableSync*>(ts.get()))
+				{
+					throw ObjectNotFoundException<TreeFolderUpNode>(id, "forbidden class");
+				}
+
+				const RegistryBase& registry(dynamic_cast<DBDirectTableSync&>(*ts).getRegistry(env));
+				shared_ptr<Registrable> parent(registry.getEditableObject(id));
+
+				if(!dynamic_cast<TreeFolderUpNode*>(parent.get()))
+				{
+					throw ObjectNotFoundException<TreeFolderUpNode>(id, "forbidden class");
+				}
+				node._setParent(*dynamic_cast<TreeFolderUpNode*>(parent.get()));
+				fieldObject = dynamic_cast<TreeFolderUpNode*>(parent.get());
+			}
+			catch(ObjectNotFoundException<Registrable>&)
+			{
+				Log::GetInstance().warn(
+					"Data corrupted on object parent"
+				);
+			}
+			catch(ObjectNotFoundException<TreeFolderUpNode>&)
+			{
+				Log::GetInstance().warn(
+					"Data corrupted on object parent"
+				);
+			}
+		}
+	}
+
+
+
+	void ObjectField<Parent, Parent::Type>::SaveToParametersMap(
+		tree::TreeFolderUpNode* fieldObject,
+		const ObjectBase& object,
+		util::ParametersMap& map,
+		const std::string& prefix
+	){
+		assert(dynamic_cast<const TreeFolderDownNodeInterface*>(&object));
+		const TreeFolderDownNodeInterface& node(dynamic_cast<const TreeFolderDownNodeInterface&>(object));
+
+		if(node._getParent() && dynamic_cast<Registrable*>(node._getParent()))
+		{
+			map.insert(prefix + FIELD.name, dynamic_cast<Registrable*>(node._getParent())->getKey());
+		}
+		else if(fieldObject && dynamic_cast<const Registrable*>(fieldObject))
+		{
+			map.insert(prefix + FIELD.name, dynamic_cast<const Registrable*>(fieldObject)->getKey());
+		}
+	}
+
+
+
+	void ObjectField<Parent, Parent::Type>::SaveToParametersMap(
+		tree::TreeFolderUpNode* fieldObject,
+		util::ParametersMap& map,
+		const std::string& prefix
+	){
+		if(fieldObject && dynamic_cast<const Registrable*>(fieldObject))
+		{
+			map.insert(prefix + FIELD.name, dynamic_cast<const Registrable*>(fieldObject)->getKey());
+		}
+	}
+
+
+	namespace tree
+	{
+		TreeFolder* TreeFolderDownNodeInterface::getParentFolder() const
+		{
+			if(dynamic_cast<TreeFolder*>(_parent))
+			{
+				return dynamic_cast<TreeFolder*>(_parent);
+			}
+			return NULL;
+		}
+
+
+
+		void TreeFolderDownNodeInterface::_setParent(TreeFolderUpNode& value)
+		{
+			if(_parent)
+			{
+				_parent->removeChild(*this);
+			}
+			_parent = &value;
+			_parent->addChild(*this);
+			if(dynamic_cast<TreeFolderDownNodeInterface*>(_parent))
+			{
+				_root = dynamic_cast<TreeFolderDownNodeInterface*>(_parent)->_getRoot();
+			}
+			else if(dynamic_cast<TreeFolderRoot*>(_parent))
+			{
+				_root = dynamic_cast<TreeFolderRoot*>(_parent);
+			}
+			else
+			{
+				assert(false);
+			}
+		}
+
+
+
+		void TreeFolderDownNodeInterface::setNullParent()
+		{
+			if(_parent)
+			{
+				_parent->removeChild(*this);
+			}
+			_parent = NULL;
+			_root = NULL;
+		}
+}	}
---
+++ src/20_tree/TreeFolderUpNode.cpp
@@ -0,0 +1,94 @@
+
+/** TreeFolderUpNode class implementation.
+	@file TreeFolderUpNode.cpp
+
+	This file belongs to the SYNTHESE project (public transportation specialized software)
+	Copyright (C) 2002 Hugues Romain - RCSmobility <contact@rcsmobility.com>
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#include "TreeFolderUpNode.hpp"
+
+#include "TreeFolder.hpp"
+
+using namespace std;
+
+namespace synthese
+{
+	namespace tree
+	{
+		TreeFolderUpNode::SubFolders TreeFolderUpNode::getSubFolders() const
+		{
+			return getChildren<TreeFolder>();
+		}
+
+
+
+		TreeFolderUpNode::Labels TreeFolderUpNode::getSubFoldersLabels(
+			std::string prefix /*= std::string()*/,
+			boost::optional<util::RegistryKeyType> forbiddenId /*= boost::optional<util::RegistryKeyType>() */
+		) const {
+			Labels labels;
+			getSubFoldersLabels(labels, prefix, forbiddenId);
+			return labels;
+		}
+
+
+
+		void TreeFolderUpNode::getSubFoldersLabels(
+			Labels& labels,
+			std::string prefix /*= std::string()*/,
+			boost::optional<util::RegistryKeyType> forbiddenId /*= boost::optional<util::RegistryKeyType>() */
+		) const	{
+
+			// Avoid forbidden id
+			if (forbiddenId && getKey() == *forbiddenId)
+			{
+				return;
+			}
+
+			// Adds the current item to the labels list
+			if(dynamic_cast<const TreeFolder*>(this))
+			{
+				labels.push_back(make_pair(getKey(), prefix + dynamic_cast<const TreeFolder*>(this)->getName()));
+			}
+			else
+			{
+				labels.push_back(make_pair(getKey(), prefix + "(racine)"));
+			}
+
+			// Adds the current folder name to the prefix
+			string fullPrefix(prefix);
+			if(dynamic_cast<const TreeFolder*>(this))
+			{
+				fullPrefix += dynamic_cast<const TreeFolder*>(this)->getName() + "/";
+			}
+
+			// Recursion
+			BOOST_FOREACH(TreeFolder* folder, getSubFolders())
+			{
+				folder->getSubFoldersLabels(labels, fullPrefix, forbiddenId);
+			}
+		}
+
+
+
+		bool TreeFolderUpNode::empty() const
+		{
+			return _children.empty();
+		}
+}	}
+
