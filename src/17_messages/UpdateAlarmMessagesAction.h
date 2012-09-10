c2e8776759ef378ee34ad8e289b5582df71695f4
Hugues Romain <hromain@rcsmobility.com>
2012-09-10 08:26:14 +0000

    Fix last build failure

--- src/17_messages/UpdateAlarmMessagesAction.h
+++ src/17_messages/UpdateAlarmMessagesAction.h
@@ -79,14 +79,14 @@ namespace synthese
 			*/
 			void _setFromParametersMap(
 				const util::ParametersMap& map
-			) throw(server::ActionException);
+			);
 
 		public:
 			/** Action to run, defined by each subclass.
 			*/
 			void run(
 				server::Request& request
-			) throw(server::ActionException);
+			);
 
 
 
@@ -97,7 +97,7 @@ namespace synthese
 			///	@date 2008
 			void setAlarmId(
 				util::RegistryKeyType id
-			) throw(server::ActionException);
+			);
 
 			virtual bool isAuthorized(const server::Session* session) const;
 
