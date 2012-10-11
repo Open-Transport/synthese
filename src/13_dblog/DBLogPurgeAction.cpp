0cf75b8350cd15be7012ef76b091ebab848b3bcc
Gael Sauvanet <gsauvanet@rcsmobility.com>
2012-10-11 15:39:04 +0000

    fix documentation

--- src/13_dblog/DBLogPurgeAction.cpp
+++ src/13_dblog/DBLogPurgeAction.cpp
@@ -1,6 +1,6 @@
 
-/** CreateDisplayScreenAction class implementation.
-	@file CreateDisplayScreenAction.cpp
+/** DBLogPurgeAction class implementation.
+	@file DBLogPurgeAction.cpp
 
 	This file belongs to the SYNTHESE project (public transportation specialized software)
 	Copyright (C) 2002 Hugues Romain - RCSmobility <contact@rcsmobility.com>
