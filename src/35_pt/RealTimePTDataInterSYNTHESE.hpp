6904470dd64294839033b8a67a80e7570b9a6d5c
Hugues Romain <hromain@rcsmobility.com>
2012-11-13 09:08:35 +0000

    Feature : real time data inter synthese

---
+++ src/35_pt/RealTimePTDataInterSYNTHESE.hpp
@@ -0,0 +1,86 @@
+
+/** RealTimePTDataInterSYNTHESE class header.
+	@file RealTimePTDataInterSYNTHESE.hpp
+
+	This file belongs to the SYNTHESE project (public transportation specialized software)
+	Copyright (C) 2002 Hugues Romain - RCSmobility <contact@rcsmobility.com>
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#ifndef SYNTHESE_pt_RealTimePTDataInterSYNTHESE_hpp__
+#define SYNTHESE_pt_RealTimePTDataInterSYNTHESE_hpp__
+
+#include "FactorableTemplate.h"
+#include "InterSYNTHESESyncTypeFactory.hpp"
+
+namespace synthese
+{
+	namespace pt
+	{
+		class ScheduledService;
+
+		//////////////////////////////////////////////////////////////////////////
+		//// RealTimePTDataInterSYNTHESE class.
+		///	@ingroup m35
+		///
+		///	Messages schema :
+		///		- service id
+		///		- loop of :
+		///			- stop rank
+		///			- arrival time
+		///			- departure time
+		///			- stop id (0 if NULL)
+		/// the data fields are separated by , 
+		class RealTimePTDataInterSYNTHESE:
+			public util::FactorableTemplate<inter_synthese::InterSYNTHESESyncTypeFactory, RealTimePTDataInterSYNTHESE>
+		{
+			static const std::string FIELD_SEPARATOR;
+
+		public:
+			RealTimePTDataInterSYNTHESE();
+
+			virtual bool mustBeEnqueued(
+				const std::string& configPerimeter,
+				const std::string& messagePerimeter
+			) const;
+
+			virtual void initSync(
+			) const;
+
+			virtual bool sync(
+				const std::string& parameter
+			) const;
+
+			virtual void closeSync(
+			) const;
+
+			virtual void initQueue(
+				const inter_synthese::InterSYNTHESESlave& slave,
+				const std::string& perimeter
+			) const;
+
+			typedef std::vector<bool> RanksToSync;
+
+			static std::string GetContent(
+				const ScheduledService& service,
+				const RanksToSync& ranksToSync
+			);
+		};
+	}
+}
+
+#endif // SYNTHESE_pt_RealTimePTDataInterSYNTHESE_hpp__
+
