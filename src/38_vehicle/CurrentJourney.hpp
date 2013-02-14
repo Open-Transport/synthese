826faf6084c7c7dbc7549dd89cfa21320a0065fe
Bruno Coudoin <bcoudoin@rcsmobility.com>
2013-02-14 10:36:14 +0000

    added NCE retrieval of next departure time at terminus.

--- src/38_vehicle/CurrentJourney.hpp
+++ src/38_vehicle/CurrentJourney.hpp
@@ -54,12 +54,14 @@ namespace synthese
 			static const std::string ATTR_LINE_NUMBER;
 			static const std::string TAG_NEXT_STOP;
 			static const std::string TAG_COMMERCIAL_LINE;
+			static const std::string TAG_TERMINUS_DEPARTURE_TIME;
 
 			NextStops _nextStops;
 			pt::CommercialLine* _line;
 			std::string _lineNumber;
 			bool _stopRequested;
-
+			boost::posix_time::ptime _terminusDepartureTime;
+			
 		public:
 			CurrentJourney();
 
@@ -67,12 +69,20 @@ namespace synthese
 			void setLine(pt::CommercialLine* value){ _line = value; }
 			void setLineNumber(const std::string& value){ _lineNumber = value; }
 			void setStopRequested(bool value){ _stopRequested = value; }
+			void setTerminusDepartureTime(boost::posix_time::ptime value)
+			{
+				_terminusDepartureTime = value;
+			}
 
 			const NextStops& getNextStops() const { return _nextStops; }
 			NextStops& getNextStops(){ return _nextStops; }
 			pt::CommercialLine* getLine() const { return _line; }
 			const std::string& getLineNumber() const { return _lineNumber; }
 			bool getStopRequested() const { return _stopRequested; }
+			boost::posix_time::ptime getTerminusDepartureTime()
+			{
+				return _terminusDepartureTime;
+			}
 
 			void toParametersMap(
 				util::ParametersMap& pm
