21f8ef450c4e52268fcc8817db2b68160cc01a52
Hugues Romain <hromain@rcsmobility.com>
2013-02-25 21:34:23 +0000

    Feature : make port configurable

--- utils/synthese_watchdog/watchdog.sh
+++ utils/synthese_watchdog/watchdog.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
+PORT=$1
 sleep 240
 while true
 do 
 	supervisor_status=`supervisorctl status synthese | grep RUNNING`
 	if [ ! -z "$supervisor_status" ]
 	then
-		curl -o /dev/null --silent -m 5  http://localhost/
+		curl -o /dev/null --silent -m 5  http://localhost:$PORT/
 		rc=$?
 		if [ $rc -ne 0 ]
 		then
 			date >> /var/log/synthese.log
-			echo " SYNTHESE did not answer to port 80 and will be restarted by the watchdog" >> /var/log/synthese.log
+			echo " SYNTHESE did not answer to port $PORT and will be restarted by the watchdog" >> /var/log/synthese.log
 			supervisorctl restart synthese
 			sleep 240
 		fi
