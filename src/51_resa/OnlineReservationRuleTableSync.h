428a8f8c5d55f4bd38f5aa2aed08441273fff0e3
Camille Hue <chue@rcsmobility.com>
2013-02-26 15:57:46 +0000

    #26767 Use of CMS module for TAD reservation email
    In t047_online_reservation_rule, if CMS template id's and interface id are defined, CMS module will be used. If only interface id is defined, interface will be used.

--- src/51_resa/OnlineReservationRuleTableSync.h
+++ src/51_resa/OnlineReservationRuleTableSync.h
@@ -62,6 +62,9 @@ namespace synthese
 			static const std::string COL_SENDER_EMAIL;
 			static const std::string COL_SENDER_NAME;
 			static const std::string COL_EMAIL_INTERFACE_ID;
+			static const std::string COL_CONFIRMATION_EMAIL_CMS_ID;
+			static const std::string COL_CANCELLATION_EMAIL_CMS_ID;
+			static const std::string COL_PASSWORD_EMAIL_CMS_ID;
 
 
 
