4c61ab0b491fe62d0616d89e346e66d3fff9c25e
Bruno Coudoin <bcoudoin@rcsmobility.com>
2013-03-04 08:27:37 +0000

    minor typo in a doxygen comment.

--- src/61_data_exchange/OGTFileFormat.hpp
+++ src/61_data_exchange/OGTFileFormat.hpp
@@ -1,6 +1,6 @@
 
 /** OGTFileFormat class header.
-	@file IneoFileFormat.hpp
+	@file OGTFileFormat.hpp
 
 	This file belongs to the SYNTHESE project (public transportation specialized software)
 	Copyright (C) 2002 Hugues Romain - RCSmobility <contact@rcsmobility.com>
