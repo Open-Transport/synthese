e2c4d42fd3577a697a6417ed940f41e024bb8334
Bruno Coudoin <bcoudoin@rcsmobility.com>
2013-05-02 15:48:12 +0000

    Replaced the global_variable_update action by a service
    The service has the same name, take the same parameters,
    does to same thing except that it can also be used to
    return the list of parameters and their current values.
    
    The action 'global_variable_update' is marked as
    deprecated.
    
    This can be used to create an admin page that allows
    the user to display and change these variables.

--- src/15_server/GlobalVariableUpdateService.cpp
+++ src/15_server/GlobalVariableUpdateService.cpp
@@ -1,7 +1,7 @@
 
 //////////////////////////////////////////////////////////////////////////
-/// GlobalVariableUpdateAction class implementation.
-/// @file GlobalVariableUpdateAction.cpp
+/// GlobalVariableUpdateService class implementation.
+/// @file GlobalVariableUpdateService.cpp
 /// @author Hugues Romain
 /// @date 2012
 ///
@@ -22,7 +22,7 @@
 ///	along with this program; if not, write to the Free Software
 ///	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-#include "GlobalVariableUpdateAction.hpp"
+#include "GlobalVariableUpdateService.hpp"
 
 #include "ActionException.h"
 #include "Conversion.h"
@@ -46,40 +46,51 @@ namespace synthese
 	using namespace util;
 	
 	template<>
-	const string FactorableTemplate<Action, server::GlobalVariableUpdateAction>::FACTORY_KEY = "global_variable_update";
+	const string FactorableTemplate<Function, server::GlobalVariableUpdateService>::FACTORY_KEY = "global_variable_update";
 
 	namespace server
 	{
-		const string GlobalVariableUpdateAction::PARAMETER_VARIABLE = Action_PARAMETER_PREFIX + "_variable";
-		const string GlobalVariableUpdateAction::PARAMETER_VALUE = Action_PARAMETER_PREFIX + "_value";
-		const string GlobalVariableUpdateAction::PARAMETER_PERSISTENT = Action_PARAMETER_PREFIX + "_persistent";
+		const string GlobalVariableUpdateService::PARAMETER_VARIABLE("variable");
+		const string GlobalVariableUpdateService::PARAMETER_VALUE("value");
+		const string GlobalVariableUpdateService::PARAMETER_PERSISTENT("persistent");
+		const string GlobalVariableUpdateService::TAG_VARIABLES = "variables";
+		const string GlobalVariableUpdateService::TAG_KEY = "key";
+		const string GlobalVariableUpdateService::TAG_VALUE = "value";
 
 
-
-		GlobalVariableUpdateAction::GlobalVariableUpdateAction(
-		):	FactorableTemplate<server::Action, GlobalVariableUpdateAction>(),
-			_persistent(false)
-		{}
-
-		server::FunctionAPI GlobalVariableUpdateAction::getAPI() const
+		server::FunctionAPI GlobalVariableUpdateService::getAPI() const
 		{
 			FunctionAPI api(
 				"15_server",
-				"Set SYNTHESE global parameters",
-				"Deprecated, use the service with the same name instead."
+				"Query or set SYNTHESE global parameters",
+				"If a variable and a value are provided in the request, then "
+				"this variable is set to this value. Instead the list of \n"
+				"all the SYNTHESE variables are returned as a parameters map.\n"
+				"This example returns the list of all the variables:\n"
+				"  <?global_variable_update&\n"
+				"    template=<{variables&template=<@key@>=<@value@><br>}>\n"
+				"  ?>"
 			);
 			api.openParamGroup("Variable set");
-			api.addParams(GlobalVariableUpdateAction::PARAMETER_VARIABLE,
+			api.addParams(GlobalVariableUpdateService::PARAMETER_VARIABLE,
 						  "The name of the variable", true);
-			api.addParams(GlobalVariableUpdateAction::PARAMETER_VALUE,
+			api.addParams(GlobalVariableUpdateService::PARAMETER_VALUE,
 						  "The value to set to this variable", true);
-			api.addParams(GlobalVariableUpdateAction::PARAMETER_PERSISTENT,
+			api.addParams(GlobalVariableUpdateService::PARAMETER_PERSISTENT,
 						  "Set to true to make this value persistant", false);
+			api.openParamGroup("Get the variable list");
+			api.addParams("", "No parameters required in this mode", false);
 			return api;
 		}
 
+		GlobalVariableUpdateService::GlobalVariableUpdateService(
+		):	FactorableTemplate<server::Function, GlobalVariableUpdateService>(),
+			_persistent(false)
+		{}
+
 		
-		ParametersMap GlobalVariableUpdateAction::getParametersMap() const
+		
+		ParametersMap GlobalVariableUpdateService::_getParametersMap() const
 		{
 			ParametersMap map;
 			return map;
@@ -87,18 +98,22 @@ namespace synthese
 		
 		
 		
-		void GlobalVariableUpdateAction::_setFromParametersMap(const ParametersMap& map)
+		void GlobalVariableUpdateService::_setFromParametersMap(const ParametersMap& map)
 		{
-			_variable = map.get<string>(PARAMETER_VARIABLE);
+			_variable = map.getDefault<string>(PARAMETER_VARIABLE);
 			_value = map.getDefault<string>(PARAMETER_VALUE);
 			_persistent = map.getDefault<bool>(PARAMETER_PERSISTENT, false);
 		}
 		
 		
 		
-		void GlobalVariableUpdateAction::run(
-			Request& request
-		){
+		ParametersMap GlobalVariableUpdateService::run(
+			ostream &stream,
+			const Request &request
+		) const
+		{
+			ParametersMap map;
+
 			if(_persistent)
 			{ // Persistent mode
 				DBTransaction transaction;
@@ -118,17 +133,37 @@ namespace synthese
 
 				transaction.run();
 			}
-			{ // Necessary in the two modes because the database triggers are not available in t999 table (to be fixed)
+			if(!_variable.empty())
+			{
+				// Necessary in the two modes because the database triggers are not available in t999 table (to be fixed)
 				ModuleClass::SetParameter(_variable, _value);
 			}
+			else
+			{
+				// No variable given, return the list of all the variables
+				BOOST_FOREACH(const ModuleClass::Parameters::value_type& item, ModuleClass::GetParameters())
+				{
+					// Insert a submap for each screen
+					boost::shared_ptr<ParametersMap> variableMap(new ParametersMap);
+					variableMap->insert(TAG_KEY, item.first);
+					variableMap->insert(TAG_VALUE, item.second);
+					map.insert(TAG_VARIABLES, variableMap);
+				}
+			}
+			return map;
 		}
 		
 		
 		
-		bool GlobalVariableUpdateAction::isAuthorized(
+		bool GlobalVariableUpdateService::isAuthorized(
 			const Session* session
 		) const {
 			return session && session->hasProfile() && session->getUser()->getProfile()->isAuthorized<GlobalRight>(DELETE_RIGHT);
+		}
+
+		std::string GlobalVariableUpdateService::getOutputMimeType() const
+		{
+			return "text/html";
 		}
 }	}
 
