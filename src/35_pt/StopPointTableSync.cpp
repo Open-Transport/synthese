06000077e07fcf88a3184a3a428bd2329bac3d82
Camille Hue <chue@rcsmobility.com>
2013-05-15 08:02:36 +0000

    Protect AfterDelete of StopPointTableSync (was breaking when trying to delete a stop point which was not already inserted inthe database). Issue : #29328

--- src/35_pt/StopPointTableSync.cpp
+++ src/35_pt/StopPointTableSync.cpp
@@ -306,16 +306,21 @@ namespace synthese
 			db::DBTransaction& transaction
 		){
 			Env env;
-			shared_ptr<const StopPoint> stopPoint(StopPointTableSync::Get(id, env));
-			StopArea::TransferDelaysMap tdMap(stopPoint->getConnectionPlace()->getTransferDelays());
-			BOOST_FOREACH(const StopArea::TransferDelaysMap::value_type& td, tdMap)
+			try
 			{
-				if(td.first.first == id || td.first.second == id)
+				shared_ptr<const StopPoint> stopPoint(StopPointTableSync::Get(id, env));
+				StopArea::TransferDelaysMap tdMap(stopPoint->getConnectionPlace()->getTransferDelays());
+				BOOST_FOREACH(const StopArea::TransferDelaysMap::value_type& td, tdMap)
 				{
-					const_cast<StopArea*>(stopPoint->getConnectionPlace())->removeTransferDelay(td.first.first, td.first.second);
+					if(td.first.first == id || td.first.second == id)
+					{
+						const_cast<StopArea*>(stopPoint->getConnectionPlace())->removeTransferDelay(td.first.first, td.first.second);
+					}
 				}
+				StopAreaTableSync::Save(const_cast<StopArea*>(stopPoint->getConnectionPlace()), transaction);
 			}
-			StopAreaTableSync::Save(const_cast<StopArea*>(stopPoint->getConnectionPlace()), transaction);
+			catch (...)
+			{}
 		}
 
 
