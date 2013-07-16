464c5384a2e39f6877f264b887f867e5b96ef1fb
Camille Hue <chue@rcsmobility.com>
2013-07-16 11:58:29 +0000

    Bugfix : add the coordinate system to PlacesListService to fix a crash case when used with address

--- src/53_pt_journey_planner/PTJourneyPlannerService.cpp
+++ src/53_pt_journey_planner/PTJourneyPlannerService.cpp
@@ -477,6 +477,7 @@ namespace synthese
 				{
 					PlacesListService placesListService;
 					placesListService.setNumber(1);
+					placesListService.setCoordinatesSystem(_coordinatesSystem);
 
 					placesListService.setClassFilter(map.getDefault<string>(PARAMETER_DEPARTURE_CLASS_FILTER));
 					placesListService.setText(map.get<string>(PARAMETER_DEPARTURE_PLACE_TEXT));
@@ -528,6 +529,7 @@ namespace synthese
 				{
 					PlacesListService placesListService;
 					placesListService.setNumber(1);
+					placesListService.setCoordinatesSystem(_coordinatesSystem);
 
 					// Arrival
 					placesListService.setClassFilter(map.getDefault<string>(PARAMETER_ARRIVAL_CLASS_FILTER));
