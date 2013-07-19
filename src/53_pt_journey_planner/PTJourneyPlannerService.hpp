6dd8bb1d10e8836ffc8bd0a87c9874068f6bcda6
Camille Hue <chue@rcsmobility.com>
2013-07-19 09:23:21 +0000

    Feature : add Max Approach Distance parameter un journey_planner service
    Parameter is named "mad" as in rp service.
    Issue : #31304

--- src/53_pt_journey_planner/PTJourneyPlannerService.hpp
+++ src/53_pt_journey_planner/PTJourneyPlannerService.hpp
@@ -109,6 +109,7 @@ namespace synthese
 			static const std::string PARAMETER_CONFIG_ID;
 			static const std::string PARAMETER_MAX_SOLUTIONS_NUMBER;
 			static const std::string PARAMETER_APPROACH_SPEED;
+			static const std::string PARAMETER_MAX_APPROACH_DISTANCE;
 			static const std::string PARAMETER_MAX_DEPTH;
 			static const std::string PARAMETER_DEPARTURE_CITY_TEXT;
 			static const std::string PARAMETER_ARRIVAL_CITY_TEXT;
