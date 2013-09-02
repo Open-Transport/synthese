d8a7e8338a9bd10fa59095f5d1d58ecad0963f5d
jdestraz <jdestraz>
2013-09-02 15:29:42 +0000

    Follows submit 9326: fix broken build (Linux specific) wrong function return type

--- src/61_data_exchange/vix/VIX-timeutil.cpp
+++ src/61_data_exchange/vix/VIX-timeutil.cpp
@@ -21,25 +21,29 @@ TimeUtil::~TimeUtil()
 // WARNING:  boost get time may be way to slow for our usage.
 // better to using OS specific calls.
 #ifdef _WIN32
-unsigned long long TimeUtil::GetTickCount(){
-	return ::GetTickCount64();
-}
 
-void TimeUtil::sleep(unsigned long ms){
-	::Sleep(ms);
-}
+	unsigned long long TimeUtil::GetTickCount(){
+		return ::GetTickCount64();
+	}
+
+	void TimeUtil::sleep(unsigned long ms){
+		::Sleep(ms);
+	}
+
 #else
-unsigned long TimeUtil::GetTickCount(){
-      
-  struct timeval tv;
-  if(gettimeofday(&tv, 0) != 0)
-    return 0;
 
-  unsigned long long  value = (tv.tv_sec * 1000) + (tv.tv_usec / 1000);
-  
-  return value;
-}
-void sleep(unsigned long ms){
-	usleep(ms*1000);
-}
+	unsigned long long TimeUtil::GetTickCount(){
+	      
+	  struct timeval tv;
+	  if(gettimeofday(&tv, 0) != 0)
+		return 0;
+
+	  unsigned long long  value = (tv.tv_sec * 1000) + (tv.tv_usec / 1000);
+	  
+	  return value;
+	}
+
+	void sleep(unsigned long ms){
+		usleep(ms*1000);
+	}
 #endif
