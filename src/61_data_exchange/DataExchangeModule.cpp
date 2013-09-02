5484af354cd01c52ed66fe8e8426b6ebd1642929
Jean Destraz <jdestraz@rcsmobility.com>
2013-09-02 10:55:02 +0000

    PCCar first version.

--- src/61_data_exchange/DataExchangeModule.cpp
+++ src/61_data_exchange/DataExchangeModule.cpp
@@ -26,6 +26,8 @@
 #include "MGScreenConnection.hpp"
 #include "ServerModule.h"
 #include "StopButtonFilePoller.hpp"
+#include "ValidatorVIXv6000Poller.hpp"
+#include "GpsPoller.hpp"
 #include "VDVClient.hpp"
 #include "VDVClientSubscription.hpp"
 #include "VDVServer.hpp"
@@ -109,6 +111,12 @@ namespace synthese
 
 			// Stop Button File Poller
 			ServerModule::AddThread(&StopButtonFilePoller::RunThread, "Stop Button File Poller");
+
+			// Validator VIX V6000 Poller
+			//ServerModule::AddThread(&ValidatorVIXv6000Poller::RunThread, "Validator VIX V6000 Poller");
+
+			// GPS Poller
+			//ServerModule::AddThread(&GpsPoller::RunThread, "GPS Poller");
 		}
 
 		template<> void ModuleClassTemplate<DataExchangeModule>::End()
