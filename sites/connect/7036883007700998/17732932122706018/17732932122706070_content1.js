8427d9171253e220af6802e8b39592bc13eaa954
Camille Hue <chue@rcsmobility.com>
2013-09-03 14:48:29 +0000

    Feature : Use of Devices in Connect

--- sites/connect/7036883007700998/17732932122706018/17732932122706070_content1.js
+++ sites/connect/7036883007700998/17732932122706018/17732932122706070_content1.js
@@ -7,3 +7,13 @@ function connection_add_click()
 $(function(){
   $('#connection_add_link').click(connection_add_click);
 });
+
+function device_add_click()
+{
+  $('#new_connection').modal('show');
+  return false;
+}
+
+$(function(){
+  $('#device_add_link').click(device_add_click);
+});
