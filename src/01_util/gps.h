74f073868e021748dd3dc180ab583cc5a126c02c
Jean Destraz <jdestraz@rcsmobility.com>
2013-09-05 15:56:03 +0000

    Renamed new Poller to DevicePoller. moved gps to synthese namespace. removed std::coud and co to synthese log.

--- src/01_util/gps.h
+++ src/01_util/gps.h
@@ -3,30 +3,33 @@
 
 #include <boost/asio.hpp>
 
-class gps
+namespace synthese
 {
-public:
-  gps();
-  ~gps();
-  
-  bool tests();
-  
-  void initSocket(std::string hostname, int port);
-  bool enableTalk();
-  bool updateFromGps();
-  void getLatLong(double &longitude, double &latitude);
-  void releaseSocket();
-  
-private:
-  
-  bool ExtractLongitudeAndLatitudeJson(std::string &str, double &longitude, double &latitude);
-  bool readGSPJsonAnswers(boost::asio::streambuf &ss, double &longitude, double &latitude);
+	class gps
+	{
+	public:
+	  gps();
+	  ~gps();
+	  
+	  bool tests();
+	  
+	  void initSocket(std::string hostname, int port);
+	  bool enableTalk();
+	  bool updateFromGps();
+	  void getLatLong(double &longitude, double &latitude);
+	  void releaseSocket();
+	  
+	private:
+	  
+	  bool ExtractLongitudeAndLatitudeJson(std::string &str, double &longitude, double &latitude);
+	  bool readGSPJsonAnswers(boost::asio::streambuf &ss, double &longitude, double &latitude);
 
-  double m_longitude;
-  double m_latitude;
-  bool m_bEnabled;
-  boost::asio::io_service m_ios;  
-  boost::asio::ip::tcp::socket m_socket;
-};
+	  double m_longitude;
+	  double m_latitude;
+	  bool m_bEnabled;
+	  boost::asio::io_service m_ios;  
+	  boost::asio::ip::tcp::socket m_socket;
+	};
+}
 
 #endif // GPS_H
--- src/61_data_exchange/GpsDevicePoller.hpp
+++ src/61_data_exchange/GpsDevicePoller.hpp
@@ -1,6 +1,6 @@
 
-/** ValidatorVIXv6000Poller class header.
-	@file ValidatorVIXv6000Poller.hpp
+/** GpsDevicePoller class header.
+	@file GpsDevicePoller.hpp
 
 	This file belongs to the SYNTHESE project (public transportation specialized software)
 	Copyright (C) 2002 Hugues Romain - RCSmobility <contact@rcsmobility.com>
@@ -20,31 +20,35 @@
 	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-#ifndef SYNTHESE_pt_ValidatorVIXv6000Poller_hpp__
-#define SYNTHESE_pt_ValidatorVIXv6000Poller_hpp__
+#ifndef SYNTHESE_pt_GpsDevicePoller_hpp__
+#define SYNTHESE_pt_GpsDevicePoller_hpp__
 
 #include <boost/shared_ptr.hpp>
 #include <string>
 
 namespace synthese
 {
-	namespace impex
-	{
-		class DataSource;
-	}
-
 	namespace data_exchange
 	{
-		/** ValidatorVIXv6000Poller class.
+		/** GpsDevicePoller class.
 			@ingroup m61
 		*/
-		class ValidatorVIXv6000Poller
+		class GpsDevicePoller
 		{
+		#define GPS_POLLER_SOCKET_PORT 2947
+
 		private:
-			static boost::shared_ptr<ValidatorVIXv6000Poller> _theConnection;
+			static boost::shared_ptr<GpsDevicePoller> _theConnection;
+			mutable double _longitude;
+			mutable double _latitude;
+			mutable bool _bGpsOk;
 
 		public:
-			ValidatorVIXv6000Poller();
+			GpsDevicePoller();
+
+			const double getLongitude() const { return _longitude; }
+			const double getLatitude() const { return _latitude; }
+			const double getGpsStatus() const { return _bGpsOk; }
 
 			static void RunThread();
 
@@ -55,5 +59,5 @@ namespace synthese
 		};
 }	}
 
-#endif // SYNTHESE_pt_ValidatorVIXv6000Poller_hpp__
+#endif // SYNTHESE_pt_GpsDevicePoller_hpp__
 
