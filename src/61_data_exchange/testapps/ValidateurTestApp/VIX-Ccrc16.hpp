a7a70ab836f83565457801818c1d5825ee7fb9f8
Jean Destraz <jdestraz@rcsmobility.com>
2013-11-13 11:15:09 +0000

    raw test/debug application for PCCar GPS and Validator

---
+++ src/61_data_exchange/testapps/ValidateurTestApp/VIX-Ccrc16.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace synthese
+{
+
+	class Ccrc16
+	{
+		static unsigned short crctable[256];
+
+	public:
+		Ccrc16(void);
+		~Ccrc16(void);
+
+		void crc_calc( unsigned char ch , unsigned short * crc );
+		unsigned short calculCrc16_ForTransparent_Mode( unsigned char * buffer, int len, int init );
+
+		bool testCRC();
+	};
+}
