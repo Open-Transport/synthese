3c1c745fedd581ae60ee4994224b4d7dacbb0261
Camille Hue <chue@rcsmobility.com>
2013-11-20 09:12:08 +0000

    FeatFeature : Add param to set a time limit to not update services.
    This param is called "hours_to_keep" and its value by default is 20 (hours).
    If a theorical time of a service is more than now + hours_to_keep, then the RT data is replaced by theorical.
    
    
    Issue : #30367

--- src/61_data_exchange/IneoBDSIFileFormat.hpp
+++ src/61_data_exchange/IneoBDSIFileFormat.hpp
@@ -85,12 +85,14 @@ namespace synthese
 				static const std::string PARAMETER_PLANNED_DATASOURCE_ID;
 				static const std::string PARAMETER_HYSTERESIS;
 				static const std::string PARAMETER_DELAY_BUS_STOP;
+				static const std::string PARAMETER_HOURS_TO_KEEP;
 		
 			private:
 				boost::shared_ptr<const impex::DataSource> _plannedDataSource;
 				boost::shared_ptr<const impex::DataSource> _messagesRecipientsDataSource;
 				boost::posix_time::time_duration _hysteresis;
 				boost::posix_time::time_duration _delay_bus_stop;
+				boost::posix_time::time_duration _time_to_keep;
 	
 				mutable std::set<util::RegistryKeyType> _scenariosToRemove;
 				mutable std::set<util::RegistryKeyType> _alarmObjectLinksToRemove;
