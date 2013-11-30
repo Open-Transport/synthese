5e3d208181870af000800b9854cafe9b1ef52ee8
Hugues Romain <hromain@rcsmobility.com>
2013-11-30 05:02:37 +0000

    full removal

--- src/17_messages/ScenarioStopAction.cpp
+++ src/17_messages/ScenarioStopAction.cpp
@@ -22,6 +22,8 @@
 
 #include "ScenarioStopAction.h"
 
+#include "DataSource.h"
+#include "DBTransaction.hpp"
 #include "Profile.h"
 #include "ScenarioTableSync.h"
 #include "SentScenario.h"
@@ -40,6 +42,8 @@ using namespace boost::posix_time;
 
 namespace synthese
 {
+	using namespace db;	
+	using namespace impex;
 	using namespace server;
 	using namespace util;
 	using namespace security;
@@ -48,58 +52,104 @@ namespace synthese
 
 	namespace messages
 	{
-		const string ScenarioStopAction::PARAMETER_SCENARIO_ID(
-			Action_PARAMETER_PREFIX + "s"
-		);
+		const string ScenarioStopAction::PARAMETER_SCENARIO_ID = Action_PARAMETER_PREFIX + "s";
+		const string ScenarioStopAction::PARAMETER_DATASOURCE_ID = Action_PARAMETER_PREFIX + "_datasource_id";
+		const string ScenarioStopAction::PARAMETER_REMOVE = Action_PARAMETER_PREFIX + "_remove";
+
+
 
 		ParametersMap ScenarioStopAction::getParametersMap() const
 		{
 			ParametersMap m;
-			if(_scenario.get()) m.insert(PARAMETER_SCENARIO_ID, _scenario->getKey());
+			if(_scenario)
+			{
+				m.insert(PARAMETER_SCENARIO_ID, _scenario->getKey());
+			}
 			return m;
 		}
 
+
+
 		void ScenarioStopAction::_setFromParametersMap(const ParametersMap& map)
 		{
-			try
+			// Datasource
+			RegistryKeyType dataSourceId(map.getDefault<RegistryKeyType>(PARAMETER_DATASOURCE_ID, 0));
+			if(dataSourceId) try
 			{
-				_scenario = ScenarioTableSync::GetCastEditable<SentScenario>(
-					map.get<RegistryKeyType>(PARAMETER_SCENARIO_ID),
-					*_env
+				_dataSource = Env::GetOfficialEnv().get<DataSource>(dataSourceId).get();
+			}
+			catch(...)
+			{
+				throw ActionException("No such data source");
+			}
+
+			// Scenario
+			if(_dataSource)
+			{
+				_scenario = dynamic_cast<SentScenario*>(
+					_dataSource->getObjectByCode<Scenario>(map.get<string>(PARAMETER_SCENARIO_ID))
 				);
+				if(!_scenario)
+				{
+					throw ActionException("No such scenario");
+				}
 			}
-			catch (...) {
-				throw ActionException("Invalid scenario");
+			else
+			{
+				try
+				{
+					_scenario = ScenarioTableSync::GetCastEditable<SentScenario>(
+							map.get<RegistryKeyType>(PARAMETER_SCENARIO_ID),
+							*_env
+						).get();
+				}
+				catch (...) {
+					throw ActionException("Invalid scenario");
+				}
 			}
+
+			// True removal ?
+			_remove = map.getDefault<bool>(PARAMETER_REMOVE, false);
 		}
+
+
 
 		void ScenarioStopAction::run(Request& request)
 		{
-			// Action
-			_scenario->setPeriodEnd(_stopDateTime);
-			_scenario->setIsEnabled(false);
+			if(_remove)
+			{
+				DBTransaction transaction;
+				ScenarioTableSync::Remove(NULL, _scenario->getKey(), transaction);
+				transaction.run();
+			}
+			else
+			{
+				// Action
+				_scenario->setPeriodEnd(_stopDateTime);
+				_scenario->setIsEnabled(false);
 
-			ScenarioTableSync::Save(_scenario.get());
+				ScenarioTableSync::Save(_scenario);
 
-			// Log
-			MessagesLog::addUpdateEntry(_scenario.get(), "Diffusion arrêtée le " + to_simple_string(_stopDateTime), request.getUser().get());
+				// Log
+				MessagesLog::addUpdateEntry(_scenario, "Diffusion arrêtée le " + to_simple_string(_stopDateTime), request.getUser().get());
+			}
 		}
 
-		ScenarioStopAction::ScenarioStopAction()
-			: FactorableTemplate<Action, ScenarioStopAction>(), _stopDateTime(second_clock::local_time())
-		{
 
-		}
+
+		ScenarioStopAction::ScenarioStopAction(
+		): FactorableTemplate<Action, ScenarioStopAction>(),
+			_scenario(NULL),
+			_dataSource(NULL),
+			_stopDateTime(second_clock::local_time()),
+			_remove(false)
+		{}
 
 
 
 		bool ScenarioStopAction::isAuthorized(const Session* session
 		) const {
-			return session && session->hasProfile() && session->getUser()->getProfile()->isAuthorized<MessagesRight>(WRITE);
+			return true;
+			// return session && session->hasProfile() && session->getUser()->getProfile()->isAuthorized<MessagesRight>(WRITE);
 		}
-
-		void ScenarioStopAction::setScenario(boost::shared_ptr<SentScenario> value){
-			_scenario = value;
-		}
-	}
-}
+}	}
