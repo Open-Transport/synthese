2fcf8040451182a23197cee65bf3bf3956118a06
Bruno Coudoin <bcoudoin@rcsmobility.com>
2013-12-16 16:38:17 +0000

    screen rotation

--- utils/SyntheseKiosk/buildroot/synthese-kiosk/run_synthese_kiosk.sh
+++ utils/SyntheseKiosk/buildroot/synthese-kiosk/run_synthese_kiosk.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 rm -rf /tmp/kiosk_config
-cp -r /usr/share/synthese_kiosk/kiosk_config_template /tmp/kiosk_config
+mkdir /tmp/kiosk_config
+cp -r /etc/synthese_kiosk/config.json /tmp/kiosk_config
+xrandr -d :0 --output LVDS1 --rotate left
 python /usr/bin/synthese-kiosk -c /tmp/kiosk_config
