921f807b31ecc2520d7f7f9c87b68c50a732ec22
Bruno Coudoin <bcoudoin@rcsmobility.com>
2013-12-16 16:47:56 +0000

    Updated synthese-kiosk package.

--- utils/SyntheseKiosk/buildroot/synthese-kiosk/synthese-kiosk.mk
+++ utils/SyntheseKiosk/buildroot/synthese-kiosk/synthese-kiosk.mk
@@ -9,21 +9,16 @@ SYNTHESE_KIOSK_SITE = https://extranet.rcsmobility.com/svn/synthese3/trunk/utils
 SYNTHESE_KIOSK_SITE_METHOD = svn
 SYNTHESE_KIOSK_DEPENDENCIES = python
 
-define HOST_SYNTHESE_KIOSK_INSTALL_CMDS
-	(cd $(@D); PYTHONPATH="$(HOST_DIR)/usr/lib/python$(PYTHON_VERSION_MAJOR)/site-packages"\
-	$(HOST_DIR)/usr/bin/python setup.py install --prefix=$(HOST_DIR)/usr)
+define SYNTHESE_KIOSK_BUILD_CMDS
+	(cd $(@D); $(HOST_DIR)/usr/bin/python setup.py build --executable=/usr/bin/python)
 endef
 
 define SYNTHESE_KIOSK_INSTALL_TARGET_CMDS
-	(cd $(@D); PYTHONPATH="$(TARGET_DIR)/usr/lib/python$(PYTHON_VERSION_MAJOR)/site-packages"\
-	$(HOST_DIR)/usr/bin/python setup.py install --prefix=$(TARGET_DIR)/usr)
-	rm -rf $(TARGET_DIR)/usr/share/synthese_kiosk
-	mkdir $(TARGET_DIR)/usr/share/synthese_kiosk
-	cp -r package/synthese-kiosk/kiosk_config_template $(TARGET_DIR)/usr/share/synthese_kiosk
+	(cd $(@D); $(HOST_DIR)/usr/bin/python setup.py install --prefix=$(TARGET_DIR)/usr)
+	rm -rf $(TARGET_DIR)/etc/synthese_kiosk
+	mkdir $(TARGET_DIR)/etc/synthese_kiosk
 	cp package/synthese-kiosk/run_synthese_kiosk.sh $(TARGET_DIR)/usr/bin
 	chmod +x $(TARGET_DIR)/usr/bin/run_synthese_kiosk.sh
 endef
 
-$(eval $(call GENTARGETS))
-$(eval $(call GENTARGETS,host))
-
+$(eval $(generic-package))
