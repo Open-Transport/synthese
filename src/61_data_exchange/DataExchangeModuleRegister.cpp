f6719be6c3391266b15f4fd9393e1dcfe45f4539
Bruno Coudoin <bcoudoin@rcsmobility.com>
2014-02-07 15:31:46 +0000

    Added support for CMS export to directory
    This is an initial version implemented as an import.
    The metadata saving is not implemented.

--- src/61_data_exchange/DataExchangeModuleRegister.cpp
+++ src/61_data_exchange/DataExchangeModuleRegister.cpp
@@ -5,6 +5,7 @@
 
 #include "DataExchangeModule.hpp"
 
+#include "CMSExport.hpp"
 #include "CMSImport.hpp"
 #include "GPSdFileFormat.hpp"
 #include "GTFSFileFormat.hpp"
@@ -101,6 +102,7 @@ void synthese::data_exchange::moduleRegister()
 	synthese::data_exchange::OSMFileFormat::integrate();
 	synthese::data_exchange::RoadShapeFileFormat::integrate();
 	synthese::data_exchange::IGNstreetsFileFormat::integrate();
+	synthese::data_exchange::CMSExport::integrate();
 	synthese::data_exchange::CMSImport::integrate();
 
 	synthese::data_exchange::VixV6000FileFormat::integrate();
