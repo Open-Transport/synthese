53bf920785f0cad80009d04036f36eaeea19b44e
Olivier Vernhet <overnhet@rcsmobility.com>
2014-04-29 09:09:14 +0000

    Feature : Add the t119_vehicle_calls table
    This table represents, within the OnBoard context, the requests of a bus driver toward the regulation so that it calls him.
    The attributes are : vehicle_id, call_time, priority, closure_time
    Issue : #40907

---
+++ src/38_vehicle/VehicleCall.cpp
@@ -0,0 +1,84 @@
+﻿
+/** VehicleCall class implementation.
+	@file VehicleCall.cpp
+
+	This file belongs to the SYNTHESE project (public transportation specialized software)
+	Copyright (C) 2002 Hugues Romain - RCSmobility <contact@rcsmobility.com>
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#include "VehicleCall.hpp"
+
+#include "ImportableTableSync.hpp"
+
+using namespace boost;
+using namespace std;
+using namespace boost::posix_time;
+
+namespace synthese
+{
+	using namespace vehicle;
+	using namespace util;
+
+	CLASS_DEFINITION(VehicleCall, "t119_vehicle_calls", 119)
+	FIELD_DEFINITION_OF_TYPE(Vehicle, "vehicle_id", SQL_INTEGER)
+	FIELD_DEFINITION_OF_TYPE(CallTime, "call_time", SQL_DATETIME)
+	FIELD_DEFINITION_OF_TYPE(Priority, "priority", SQL_BOOLEAN)
+	FIELD_DEFINITION_OF_TYPE(ClosureTime, "closure_time", SQL_DATETIME)
+
+
+	namespace vehicle
+	{
+		VehicleCall::VehicleCall(
+			RegistryKeyType id
+		):	Registrable(id),
+			Object<VehicleCall, VehicleCallSchema>(
+				Schema(
+					FIELD_VALUE_CONSTRUCTOR(Key, id),
+					FIELD_DEFAULT_CONSTRUCTOR(Vehicle),
+					FIELD_DEFAULT_CONSTRUCTOR(CallTime),
+					FIELD_DEFAULT_CONSTRUCTOR(Priority),
+					FIELD_DEFAULT_CONSTRUCTOR(ClosureTime)
+			)	)
+		{}
+
+
+
+		void VehicleCall::addAdditionalParameters(
+			util::ParametersMap& map,
+			std::string prefix /*= std::string() */
+		) const	{
+		}
+
+
+
+		void VehicleCall::link( util::Env& env, bool withAlgorithmOptimizations /*= false*/ )
+		{
+		}
+
+
+
+		void VehicleCall::unlink()
+		{
+		}
+
+
+
+		VehicleCall::~VehicleCall()
+		{
+		unlink();
+		}
+}	}
---
+++ src/38_vehicle/VehicleCall.hpp
@@ -0,0 +1,86 @@
+﻿
+/** VehicleCall class header.
+	@file VehicleCall.hpp
+
+	This file belongs to the SYNTHESE project (public transportation specialized software)
+	Copyright (C) 2002 Hugues Romain - RCSmobility <contact@rcsmobility.com>
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#ifndef SYNTHESE_vehicle_VehicleCall_hpp__
+#define SYNTHESE_vehicle_VehicleCall_hpp__
+
+#include "ImportableTemplate.hpp"
+#include "PtimeField.hpp"
+#include "Registrable.h"
+#include "Registry.h"
+#include "Vehicle.hpp"
+
+#include "FrameworkTypes.hpp"
+
+#include <set>
+#include <vector>
+
+namespace synthese
+{
+	namespace vehicle
+	{
+		class VehicleCall;
+
+		FIELD_PTIME(CallTime)
+		FIELD_BOOL(Priority)
+		FIELD_PTIME(ClosureTime)
+
+		typedef boost::fusion::map<
+			FIELD(Key),
+			FIELD(Vehicle),
+			FIELD(CallTime),
+			FIELD(Priority),
+			FIELD(ClosureTime)
+		> VehicleCallSchema;
+
+		/** VehicleCall class.
+		@ingroup m38
+		*/
+		class VehicleCall:
+			public Object<VehicleCall, VehicleCallSchema>,
+			public impex::ImportableTemplate<VehicleCall>
+		{
+		private:
+
+
+		public:
+			VehicleCall(util::RegistryKeyType id=0);
+			~VehicleCall();
+
+			//! @name Updaters
+			//@{
+			//@}
+
+			//! @name Services
+			//@{
+				virtual void link(util::Env& env, bool withAlgorithmOptimizations = false);
+				virtual void unlink();
+
+				virtual void addAdditionalParameters(
+				util::ParametersMap& map,
+				std::string prefix = std::string()
+				) const;
+			//@}
+		};
+}	}
+
+#endif // SYNTHESE_vehicle_VehicleCall_hpp__
