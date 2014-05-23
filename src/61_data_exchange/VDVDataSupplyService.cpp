273597ed834317fb492009171678e1dce8aa8012
Camille Hue <chue@rcsmobility.com>
2014-05-23 14:49:53 +0000

    Feature : Add betreiber attribute (name of the network)

--- src/61_data_exchange/VDVDataSupplyService.cpp
+++ src/61_data_exchange/VDVDataSupplyService.cpp
@@ -372,6 +372,9 @@ namespace synthese
 								"<AufAZB>false</AufAZB>" <<
 								"<FahrtStatus>" << (isRealTime ? "Ist" : "Soll") << "</FahrtStatus>"
 							;
+							Log::GetInstance().debug("VDVDataSupply : 1. Ecriture du networkname");
+							result << "<Betreiber>" << networkName << "</Betreiber>";
+							Log::GetInstance().debug("VDVDataSupply : 2. Ecriture du networkname");
 							if(!plannedArrivalDateTime.is_not_a_date_time())
 							{
 								result << "<AnkunftszeitAZBPlan>";
