fdf2647178d15159f7ad121f0eb62ee89420d228
Erwan Turck <erwan.turck@tisseo.fr>
2014-06-19 13:18:13 +0000

    Crash fix on tdg with use_sae_direct_connection.
    Check for null pointer on destination.

--- src/54_departure_boards/DisplayScreenContentFunction.cpp
+++ src/54_departure_boards/DisplayScreenContentFunction.cpp
@@ -1261,12 +1261,11 @@ namespace synthese
 								realTimeService.destination = ocStops.find(result->getInfo("oc_arrivee"))->second->getConnectionPlace();
 
 							const StopArea* area = realTimeService.stop->getConnectionPlace();
+							if(_SAELine.find(result->getInfo("ligne")) == _SAELine.end() || !realTimeService.destination)
+								continue;
 
-								// Ignore when destination and stop are on the same stop area
-								if(area->getKey() == realTimeService.destination->getKey())
-									continue;
-
-							if(_SAELine.find(result->getInfo("ligne")) == _SAELine.end())
+							// Ignore when destination and stop are on the same stop area
+							if(area->getKey() == realTimeService.destination->getKey())
 								continue;
 
 							BOOST_FOREACH(SAELine::mapped_type::value_type idLine, _SAELine.find(result->getInfo("ligne"))->second)
@@ -1826,11 +1825,11 @@ namespace synthese
 							if(ocStops.find(result->getInfo("oc_arrivee"))->second)
 								realTimeService.destination = ocStops.find(result->getInfo("oc_arrivee"))->second->getConnectionPlace();
 
-								// Ignore when destination and stop are on the same stop area
-								if(realTimeService.stop->getConnectionPlace()->getKey() == realTimeService.destination->getKey())
-									continue;
+							if(_SAELine.find(result->getInfo("ligne")) == _SAELine.end() || !realTimeService.destination)
+								continue;
 
-							if(_SAELine.find(result->getInfo("ligne")) == _SAELine.end())
+							// Ignore when destination and stop are on the same stop area
+							if(realTimeService.stop->getConnectionPlace()->getKey() == realTimeService.destination->getKey())
 								continue;
 
 							BOOST_FOREACH(SAELine::mapped_type::value_type idLine, _SAELine.find(result->getInfo("ligne"))->second)
