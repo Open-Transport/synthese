5ee9cbc032e0db5dc6a7441e46d44066ee817294
Thomas Puigt <tpuigt@capgemini.com>
2014-08-12 16:47:20 +0000

    Feature : creation of a CSV-file importer for POI data (public places).
    Issue : #45394

--- src/61_data_exchange/DataExchangeModuleRegister.cpp
+++ src/61_data_exchange/DataExchangeModuleRegister.cpp
@@ -23,6 +23,7 @@
 #include "OGTFileFormat.hpp"
 #include "ObitiFileFormat.hpp"
 #include "PegaseFileFormat.hpp"
+#include "POICSVFileFormat.hpp"
 #include "RSSFileFormat.hpp"
 #include "TridentFileFormat.h"
 #include "StopsShapeFileFormat.hpp"
@@ -95,6 +96,7 @@ void synthese::data_exchange::moduleRegister()
 	synthese::data_exchange::OGTFileFormat::integrate();
 	synthese::data_exchange::ObitiFileFormat::integrate();
 	synthese::data_exchange::PegaseFileFormat::integrate();
+    synthese::data_exchange::POICSVFileFormat::integrate();
 	synthese::data_exchange::RSSFileFormat::integrate();
 	synthese::data_exchange::TridentFileFormat::integrate();
 	synthese::data_exchange::StopsShapeFileFormat::integrate();
