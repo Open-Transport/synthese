9893289e572e8cdb469354c9f253b38933ee8adc
Marc Jambert <mjambert@gmail.com>
2014-12-16 10:27:47 +0100

    proper service id

--- src/62_regulation/CallbackRequestAlertProcessor.cpp
+++ src/62_regulation/CallbackRequestAlertProcessor.cpp
@@ -27,6 +27,7 @@
 #include "DB.hpp"
 #include "Env.h"
 #include "ScheduledService.h"
+#include "SelectQuery.hpp"
 
 using namespace std;
 
@@ -53,6 +54,9 @@ namespace synthese
                     )
                 );
             */
+
+            
+
             
 			stringstream query;
 			query <<
@@ -61,17 +65,95 @@ namespace synthese
                 " ON vc.vehicle_id" /*<< vehicle::VehicleCall<Vehicle>::FIELD.name*/ << " = vp." << vehicle::VehiclePositionTableSync::COL_VEHICLE_ID;
             util::Log::GetInstance().info(query.str());
             db::DBResultSPtr result(db::DBModule::GetDB()->execQuery(query.str()));
+            for (int i = 0 ; i < result->getNbColumns(); ++i) {
+                util::Log::GetInstance().info("-- col " + result->getColumnName(i));                
+            }
             while(result->next())
             {
-                int serviceId(result->get<util::RegistryKeyType>("service_id"));
+                util::RegistryKeyType serviceId(result->get<util::RegistryKeyType>("service_id"));
+                std::cerr << "service id " << serviceId << std::endl;
                 boost::shared_ptr<const pt::ScheduledService> scheduledService(util::Env::GetOfficialEnv().get<pt::ScheduledService>(serviceId));
+                std::cerr << "TOTO1" << std::endl;
                 const pt::JourneyPattern* journeyPattern(scheduledService->getRoute());
-				const pt::CommercialLine* commercialLine(journeyPattern->getCommercialLine());
+                /*
+                DBModule::LoadObjects(journeyPattern->getLinkedObjectsIds(*result), env, linkLevel);
+                obj->loadFromRecord(*rows, env);
+                */
                 
-                util::Log::GetInstance().info("journeyPattern " + journeyPattern->getKey());
+                std::cerr << "TOTO2" << journeyPattern << " " << std::endl;
+				const pt::CommercialLine* commercialLine(journeyPattern->getCommercialLine());
+                std::cerr << "TOTO3" << journeyPattern->getKey() << std::endl;
+                
+                std::cerr << "Found vehicle call for service id = " << serviceId << " journey pattern id = " << journeyPattern->getKey() << " ; commercial line id  = " << commercialLine << std::endl;
+
+
+
+                
+/*
+			DBModule::LoadObjects(obj->getLinkedObjectsIds(*rows), env, linkLevel);
+			obj->loadFromRecord(*rows, env);
+			if(linkLevel > util::FIELDS_ONLY_LOAD_LEVEL)
+			{
+				obj->link(env, linkLevel == util::ALGORITHMS_OPTIMIZATION_LOAD_LEVEL);
+			}
+*/  
+
                 
             }
             
+/*
+			SelectQuery<AlarmObjectLinkTableSync> query;
+			query.addTableField(AlarmObjectLinkTableSync::COL_ALARM_ID);
+			query.addTableAndEqualJoin<AlarmTableSync>(TABLE_COL_ID, AlarmObjectLinkTableSync::COL_ALARM_ID);
+			query.addTableAndEqualOtherJoin<ScenarioTableSync, AlarmTableSync>(TABLE_COL_ID, AlarmTableSync::COL_SCENARIO_ID);
+			query.addWhereField(AlarmObjectLinkTableSync::COL_OBJECT_ID, screenId);
+			query.addWhereFieldOther<ScenarioTableSync>(ScenarioTableSync::COL_ENABLED, 1);
+			query.addWhere(
+				SQLSingleOperatorExpression::Get(
+					SQLSingleOperatorExpression::OP_NOT,
+					FieldExpression::Get(
+						ScenarioTableSync::TABLE.NAME, ScenarioTableSync::COL_IS_TEMPLATE
+			)	)	);
+*/
+                
+                // TODO : pb commercial line not loaded . review query
+                // to use selectquery with proper joins
+
+                // probably the right way of doing it is implementing
+                // a search on vehiclecall table sync, adding proper
+                // joins to get vehicle, vehicle position, service,
+                // line, commercial line
+
+/*            db::SelectQuery<vehicle::VehicleCallTableSync> squery;
+            squery.addTableAndEqualJoin<vehicle::VehiclePositionTableSync>("vehicle_id",
+                                                                           vehicle::VehiclePositionTableSync::COL_VEHICLE_ID);
+            squery.addTableField(vehicle::VehiclePositionTableSync::COL_SERVICE_ID);
+*/          
+/*            
+            query.addTableField(AlarmObjectLinkTableSync::COL_ALARM_ID);
+			query.addTableAndEqualJoin<AlarmTableSync>(TABLE_COL_ID, AlarmObjectLinkTableSync::COL_ALARM_ID);
+			query.addTableAndEqualOtherJoin<ScenarioTableSync, AlarmTableSync>(TABLE_COL_ID, AlarmTableSync::COL_SCENARIO_ID);
+			query.addWhereField(AlarmObjectLinkTableSync::COL_OBJECT_ID, screenId);
+			query.addWhereFieldOther<ScenarioTableSync>(ScenarioTableSync::COL_ENABLED, 1);
+			query.addWhere(
+				SQLSingleOperatorExpression::Get(
+					SQLSingleOperatorExpression::OP_NOT,
+					FieldExpression::Get(
+						ScenarioTableSync::TABLE.NAME, ScenarioTableSync::COL_IS_TEMPLATE
+			)	)	);
+*/
+            // std::cerr << "query " << squery.toString() << std::endl;
+            
+            
+
+
+
+
+
+                
+
+                
+                
 
             // pour chq vehicle call
 
