1d8925c78bd028fa4bef285338bf6dfe7b6d9084
Ivan Ivanov <ivan.ivanov@elca.ch>
2015-05-13 10:18:35 +0200

    Implemented the initial version of the classes NotificationEvent and NotificationEventTableSync

---
+++ server/src/17_messages/NotificationEvent.cpp
@@ -0,0 +1,84 @@
+/** NotificationEvent class implementation.
+	@file NotificationEvent.cpp
+	@author Ivan Ivanov
+	@date 2015
+
+	This file belongs to the SYNTHESE project (public transportation specialized software)
+	Copyright (C) 2015 Hugues Romain - RCSmobility <contact@rcsmobility.com>
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#include "NotificationEvent.hpp"
+
+using namespace std;
+
+namespace synthese
+{
+	using namespace messages;
+	using namespace util;
+
+	CLASS_DEFINITION(NotificationEvent, "t121_notification_events", 121)
+
+	namespace messages {
+		FIELD_DEFINITION_OF_OBJECT(NotificationEvent, "notification_event_id", "notification_event_ids")
+
+		FIELD_DEFINITION_OF_TYPE(AlarmMessage, "alarm_id", SQL_INTEGER)
+		FIELD_DEFINITION_OF_TYPE(Expiration, "expiration", SQL_DATETIME)
+		FIELD_DEFINITION_OF_TYPE(LastAttempt, "last_attempt", SQL_DATETIME)
+		FIELD_DEFINITION_OF_TYPE(Attempts, "attempts", SQL_INTEGER)
+		FIELD_DEFINITION_OF_TYPE(Diffuser, "notification_provider_id", SQL_INTEGER)
+		FIELD_DEFINITION_OF_TYPE(TypeOfMessage, "message_type_id", SQL_INTEGER)
+		FIELD_DEFINITION_OF_TYPE(Status, "status", SQL_INTEGER)
+
+		/// Default constructor as a Registrable
+		/// @param id registry key type
+		NotificationEvent::NotificationEvent(
+			RegistryKeyType id /*= 0*/
+		) : Registrable(id),
+			Object<NotificationEvent, NotificationEventRecord>(
+				Schema(
+					FIELD_VALUE_CONSTRUCTOR(Key, id),
+					FIELD_DEFAULT_CONSTRUCTOR(AlarmMessage),
+					FIELD_VALUE_CONSTRUCTOR(Expiration, posix_time::not_a_date_time),
+					FIELD_VALUE_CONSTRUCTOR(LastAttempt, posix_time::not_a_date_time),
+					FIELD_VALUE_CONSTRUCTOR(Attempts, 0),
+					FIELD_DEFAULT_CONSTRUCTOR(Diffuser),
+					FIELD_DEFAULT_CONSTRUCTOR(TypeOfMessage),
+					FIELD_DEFAULT_CONSTRUCTOR(Status, NotificationStatus.READY)
+				)	)
+		{}
+
+		NotificationEvent::NotificationEvent(
+			RegistryKeyType id,
+			const Alarm& alarm,
+			const boost::posix_time::ptime& expiration,
+			const NotificationProvider& notificationProvider
+		) : Registrable(id),
+			Object<NotificationEvent, NotificationEventRecord>(
+				Schema(
+					FIELD_VALUE_CONSTRUCTOR(Key, id),
+					FIELD_VALUE_CONSTRUCTOR(AlarmMessage, &alarm),
+					FIELD_VALUE_CONSTRUCTOR(Expiration, expiration),
+					FIELD_VALUE_CONSTRUCTOR(LastAttempt, posix_time::not_a_date_time),
+					FIELD_VALUE_CONSTRUCTOR(Attempts, 0),
+					FIELD_VALUE_CONSTRUCTOR(Diffuser, &notificationProvider),
+					FIELD_VALUE_CONSTRUCTOR(TypeOfMessage, notificationProvider->getMessageType()),
+					FIELD_DEFAULT_CONSTRUCTOR(Status, READY)
+				)	)
+		{}
+	
+	}
+}
---
+++ server/src/17_messages/NotificationEvent.hpp
@@ -0,0 +1,113 @@
+/** NotificationEvent class header.
+	@file NotificationEvent.hpp
+	@author Ivan Ivanov
+	@date 2015
+
+	This file belongs to the SYNTHESE project (public transportation specialized software)
+	Copyright (C) 2015 Hugues Romain - RCSmobility <contact@rcsmobility.com>
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#ifndef SYNTHESE_messages_NotificationEvent_h__
+#define SYNTHESE_messages_NotificationEvent_h__
+
+#include "Object.hpp"
+
+#include "NumericField.hpp"
+#include "StringField.hpp"
+#include "SchemaMacros.hpp"
+#include "Alarm.h"
+#include "NotificationProvider.hpp"
+#include "MessageType.hpp"
+
+namespace synthese
+{
+	namespace messages
+	{
+		typedef enum
+		{
+			READY = 0,
+			IN_PROGRESS = 1,
+			FAILED = 2,
+			SUCCESS = 3
+		} NotificationStatus;
+
+		FIELD_POINTER(AlarmMessage, messages::Alarm)
+		FIELD_PTIME(Expiration)
+		FIELD_PTIME(LastAttempt)
+		FIELD_INT(Attempts)
+		FIELD_POINTER(Diffuser, messages::NotificationProvider)
+		FIELD_POINTER(TypeOfMessage, messages::MessageType)
+		FIELD_ENUM(Status, messages::NotificationStatus)
+
+		typedef boost::fusion::map<
+			FIELD(Key),
+
+			// Reference to the alarm message
+			FIELD(AlarmMessage),
+
+			// Expiration timestamp of the notification
+			FIELD(Expiration),
+
+			// Timestamp when the notification was attempted last time
+			FIELD(LastAttempt),
+
+			// Number of performed attempts
+			FIELD(Attempts),
+
+			// Reference to the provider responsible for notification
+			FIELD(Diffuser),
+
+			// Reference to the type of message
+			FIELD(TypeOfMessage),
+
+			// Technical status of the notification
+			FIELD(Status)
+		> NotificationEventRecord;
+
+		/**
+		@class NotificationEvent
+		@ingroup m17
+
+		Notification event.
+		*/
+		class NotificationEvent :
+			public Object<NotificationEvent, NotificationEventRecord>
+		{
+		public:
+			
+			/**
+			* NotificationEvent key in parameters map
+			*/
+			static const std::string ATTR_KEY;
+
+			NotificationEvent(
+				util::RegistryKeyType id = 0
+				);
+
+			NotificationEvent(
+				util::RegistryKeyType id,
+				const Alarm& alarm,
+				const boost::posix_time::ptime& expiration,
+				const NotificationProvider& notificationProvider
+				);
+
+			virtual ~NotificationEvent() {};
+		};
+	}
+}
+
+#endif // SYNTHESE_messages_NotificationEvent_h__
