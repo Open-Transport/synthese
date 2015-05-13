dea33963d0d68766bf0d7aa5680bc03baaacfdc5
Ivan Ivanov <ivan.ivanov@elca.ch>
2015-05-13 10:18:35 +0200

    Implemented the initial version of the classes NotificationEvent and NotificationEventTableSync

--- server/src/17_messages/MessagesModuleRegister.cpp
+++ server/src/17_messages/MessagesModuleRegister.cpp
@@ -9,6 +9,7 @@
 #include "MessageApplicationPeriodTableSync.hpp"
 #include "MessagesSectionTableSync.hpp"
 #include "MessageTypeTableSync.hpp"
+#include "NotificationEventTableSync.hpp"
 #include "ScenarioTableSync.h"
 #include "ScenarioCalendarTableSync.hpp"
 #include "ScenarioFolderTableSync.h"
@@ -59,6 +60,7 @@
 // Registries
 #include "AlarmObjectLink.h"
 #include "CustomBroadcastPoint.hpp"
+#include "NotificationEvent.hpp"
 #include "NotificationProvider.hpp"
 #include "MailingList.hpp"
 #include "MailingListSubscription.hpp"
@@ -91,6 +93,7 @@ void synthese::messages::moduleRegister()
 	synthese::messages::MessageApplicationPeriodTableSync::integrate();
 	synthese::messages::MessagesSectionTableSync::integrate();
 	synthese::messages::MessageTypeTableSync::integrate();
+	synthese::messages::NotificationEventTableSync::integrate();
 	synthese::messages::ScenarioCalendarTableSync::integrate();
 	synthese::messages::ScenarioFolderTableSync::integrate();
 	synthese::messages::ScenarioTableSync::integrate();
@@ -143,6 +146,7 @@ void synthese::messages::moduleRegister()
 
 	// Registries
 	synthese::util::Env::Integrate<synthese::messages::AlarmObjectLink>();
+	synthese::util::Env::Integrate<synthese::messages::NotificationEvent>();
 	synthese::util::Env::Integrate<synthese::messages::NotificationProvider>();
 	synthese::util::Env::Integrate<synthese::messages::MailingList>();
 	synthese::util::Env::Integrate<synthese::messages::MailingListSubscription>();
