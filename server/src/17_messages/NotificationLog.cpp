25efd9b52fa1017f1dc46453161cef75066e81b9
Yves Martin <ymartin1040@gmail.com>
2015-05-26 13:51:18 +0200

    Notification log support for category 'messagesnotification'

---
+++ server/src/17_messages/NotificationLog.cpp
@@ -0,0 +1,170 @@
+/** NotificationLog implementation.
+	@file NotificationLog.cpp
+	@author Yves Martin
+	@date 2015
+
+	This file belongs to the SYNTHESE project (public transportation specialized software)
+	Copyright (C) 2015 Hugues Romain - RCSmobility <contact@rcsmobility.com>
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#include <Alarm.h>
+#include <AlarmTableSync.h>
+#include <boost/lexical_cast.hpp>
+#include <boost/smart_ptr/shared_ptr.hpp>
+#include <DBLogEntry.h>
+#include <DBTableSync.hpp>
+#include <DBTableSyncTemplate.hpp>
+#include <FactorableTemplate.h>
+#include <NotificationEvent.hpp>
+#include <NotificationLog.hpp>
+#include <NotificationProvider.hpp>
+#include <NotificationProviderTableSync.hpp>
+#include <Object.hpp>
+#include <Registrable.h>
+#include <Scenario.h>
+
+#include <sstream>
+#include <vector>
+
+using namespace std;
+using namespace boost;
+
+namespace synthese
+{
+	using namespace util;
+	using namespace dblog;
+
+	namespace util
+	{
+		template<> const std::string FactorableTemplate<DBLog, messages::NotificationLog>::FACTORY_KEY("messagesnotification");
+	}
+
+	namespace messages
+	{
+		string NotificationLog::getName() const
+		{
+				return "Notifications";
+		}
+
+
+
+		DBLog::ColumnsVector NotificationLog::getColumnNames() const
+		{
+			DBLog::ColumnsVector v;
+			v.push_back("Message");
+			v.push_back("Action");
+			return v;
+		}
+
+
+
+		std::string NotificationLog::getObjectName(
+			RegistryKeyType id,
+			const server::Request& searchRequest
+		) const	{
+			try
+			{
+				RegistryTableType tableId = decodeTableId(id);
+				Env env;
+				if (tableId == AlarmTableSync::TABLE.ID)
+				{
+					boost::shared_ptr<const Alarm> alarm(AlarmTableSync::Get(id, env, FIELDS_ONLY_LOAD_LEVEL));
+					return alarm->getShortMessage();
+				}
+				else if (tableId == NotificationProviderTableSync::TABLE.ID)
+				{
+					boost::shared_ptr<const NotificationProvider> provider(NotificationProviderTableSync::Get(id, env, FIELDS_ONLY_LOAD_LEVEL));
+					return provider->getName();
+				}
+			}
+			catch (...)
+			{
+			}
+			return DBLog::getObjectName(id,searchRequest);
+		}
+
+
+
+		void NotificationLog::AddNotificationEventEntry(
+			const boost::shared_ptr<NotificationEvent>& event
+		) {
+			if (!event) return;
+			Alarm& alarm = event->get<Alarm>().get();
+			NotificationProvider& provider = event->get<NotificationProvider>().get();
+
+			DBLogEntry::Level level = DBLogEntry::DB_LOG_INFO;
+			DBLog::ColumnsVector content;
+			content.push_back(lexical_cast<string>(alarm.getKey()));
+
+			stringstream text;
+			text << "Notification depuis '" << provider.get<Name>();
+
+			if (event->get<Status>() == FAILED)
+			{
+				text << "' en échec pour le message '";
+				level = DBLogEntry::DB_LOG_ERROR;
+			}
+			else if (event->get<Status>() == IN_PROGRESS)
+			{
+				text << "' en reprise pour le message '";
+				level = DBLogEntry::DB_LOG_WARNING;
+			}
+			else if (event->get<Status>() == SUCCESS)
+			{
+				text << "' réussi pour le message '";
+			}
+			text << alarm.getShortMessage();
+			if (alarm.getScenario())
+			{
+				text << "' du scénario " << alarm.getScenario()->getName();
+			} else {
+				text << "'";
+			}
+			content.push_back(text.str());
+			_addEntry(FACTORY_KEY, level, content, NULL, provider.getKey(), alarm.getKey());
+		}
+
+		void NotificationLog::AddNotificationProviderFailure(
+			const NotificationProvider* provider,
+			const std::string details,
+			const Alarm* alarm /* = NULL */
+		) {
+			DBLog::ColumnsVector content;
+			if (alarm)
+			{
+				content.push_back(lexical_cast<string>(alarm->getKey()));
+			} else {
+				content.push_back(string());
+			}
+			stringstream text;
+			text << "Echec depuis '" << provider->getName();
+			if (alarm) {
+				text << "' pour le message '" << alarm->getShortMessage();
+				if (alarm->getScenario())
+				{
+					text << "' du scénario '" << alarm->getScenario()->getName();
+				}
+			}
+			text << "': " << details;
+			content.push_back(text.str());
+
+			_addEntry(FACTORY_KEY, DBLogEntry::DB_LOG_ERROR, content, NULL, provider->getKey(),
+					(alarm ? alarm->getKey() : 0));
+		}
+
+	} /* namespace messages */
+} /* namespace synthese */
---
+++ server/src/17_messages/NotificationLog.hpp
@@ -0,0 +1,96 @@
+/** NotificationLog class header.
+	@file NotificationLog.hpp
+	@author Yves Martin
+	@date 2015
+
+	This file belongs to the SYNTHESE project (public transportation specialized software)
+	Copyright (C) 2015 Hugues Romain - RCSmobility <contact@rcsmobility.com>
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#ifndef SYNTHESE_messages_NotificationLog_hpp__
+#define SYNTHESE_messages_NotificationLog_hpp__
+
+#include <DBLogTemplate.h>
+
+#include <string>
+
+namespace synthese
+{
+	namespace messages
+	{
+		// Forward declaration
+		class NotificationEvent;
+
+		/**
+			Log for notification events.
+
+			Entries are:
+			- NotificationEvent, success or failure after each attempt
+
+			Colums:
+			- Free text log "Message"
+			- Free text "Action"
+			- Link to source NotificationProvider
+			- Link to Alarm message
+
+			@ingroup m17Logs refLogs
+			@author Yves Martin
+			@date 2015
+		 */
+		class NotificationLog:
+			public dblog::DBLogTemplate<NotificationLog>
+		{
+		public:
+			/// Log name
+			std::string getName() const;
+
+			/// Terminus log columns
+			DBLog::ColumnsVector getColumnNames() const;
+
+			/// Converts registry key into object name
+			virtual std::string getObjectName(
+				util::RegistryKeyType id,
+				const server::Request& searchRequest
+			) const;
+
+			/**
+				Add an entry for a notification event. Current status and type
+				are used, references to alarm and provider are available.
+
+				@param event shared pointer to processed NotificationEvent to log
+			*/
+			static void AddNotificationEventEntry(
+				const boost::shared_ptr<NotificationEvent>& event
+			);
+
+			/**
+				Add an entry for a technical notification provider failure
+			*/
+			static void AddNotificationProviderFailure(
+				const NotificationProvider* provider,
+				const std::string details,
+				const Alarm* alarm = NULL
+			);
+
+		private:
+
+		};
+
+	} /* namespace messages */
+} /* namespace synthese */
+
+#endif /* SYNTHESE_messages_NotificationLog_hpp__ */
