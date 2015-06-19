49c302c60133e78aa7315ec98a6fd0e6daa678b5
Yves Martin <ymartin1040@gmail.com>
2015-06-19 11:44:07 +0200

    Add reference to scenario template in messageslibrary log entry

--- server/src/17_messages/MessagesLibraryLog.cpp
+++ server/src/17_messages/MessagesLibraryLog.cpp
@@ -105,7 +105,7 @@ namespace synthese
 			content.push_back(string());
 			content.push_back(text);
 
-			_addEntry(FACTORY_KEY, DBLogEntry::DB_LOG_INFO, content, user, 0);
+			_addEntry(FACTORY_KEY, DBLogEntry::DB_LOG_INFO, content, user, (scenario ? scenario->getKey() : 0));
 		}
 
 		void MessagesLibraryLog::addUpdateEntry(
