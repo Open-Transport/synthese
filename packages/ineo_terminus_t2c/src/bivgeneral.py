ea98c43d1f4dabb85a6a4bde910b20dc0347b109
Olivier Vernhet <vernhet.olivier@gmail.com>
2015-07-16 16:38:03 +0200

    Feature : T2C scripts for request generation and configuration of notification providers

---
+++ packages/ineo_terminus_t2c/src/bivgeneral.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python2
+
+import datetime
+import re
+import HTMLParser
+import synthese
+
+try:
+  from lxml import etree
+except ImportError:
+  print("la lib lxml n'est pas disponible")
+
+# Request headers
+root = etree.Element("BivGeneral" + type + "MessageRequest")
+childID = etree.SubElement(root, "ID")
+childID.text = ID
+childRequestTimeStamp = etree.SubElement(root, "RequestTimeStamp")
+now = datetime.datetime.now()
+childRequestTimeStamp.text = now.strftime("%d/%m/%Y %X")
+childRequestorRef = etree.SubElement(root, "RequestorRef")
+childRequestorRef.text = "Terminus"
+childMessaging = etree.SubElement(root, "Messaging")
+
+# Name
+# Ineo SAE requires the name to be defined as ‘XXXX aaaaaaaaaaaaaaaaaaaaaaaaaaa’ where 'XXXX' is a unique message id
+childName = etree.SubElement(childMessaging, "Name")
+messageID = int(message[0]["message_id"]) % 10000
+childName.text = "{:04d} {:.27s}".format(messageID, message[0]["title"])
+
+# {Start,Stop}{Date,Time}
+childStartDate = etree.SubElement(childMessaging, "StartDate")
+childStartDate.text = "01/01/1970"
+childStopDate = etree.SubElement(childMessaging, "StopDate")
+childStopDate.text = "31/12/2037"
+
+# Varying
+childVarying = etree.SubElement(childMessaging, "Varying")
+childVarying.text = "oui" if int(message[0]["priority"]) < 99 else "non"
+
+# Duration
+childDuration = etree.SubElement(childMessaging, "Duration")
+childDuration.text = message[0]["display_duration"]
+
+# DiodFlashing
+
+# Text
+# Split text around <br /> and \n
+# TODO : change to match BivGeneralMessagingTextType structure (details pending)
+childText = etree.SubElement(childMessaging, "Text")
+for line in re.split('<br />|\n',message[0]["content"]):
+  h = HTMLParser.HTMLParser()
+  childLine = etree.SubElement(childText, "Line")
+  childLine.text = h.unescape(line)
+
+# Recipients
+childRecipients = etree.SubElement(childMessaging, "Recipients")
+recipients = message[0]["recipients"][0]
+hasAllNetwork = False
+if 'line' in recipients:
+  # Scan the 'line' recipients to check if the whole transport network is selected
+  for line in recipients["line"]:
+    hasAllNetwork = hasAllNetwork or (line["id"] == networkId)
+  # If it is, use 'AllNetwork' tag
+  if hasAllNetwork:
+    childAllNetwork = etree.SubElement(childRecipients, "AllNetwork")
+  # Else add the Ineo code of each commercial line in the recipients
+  else:
+    childLines = etree.SubElement(childRecipients, "Lines")
+    for line in recipients["line"]:
+      parameters = { "roid": line["id"] }
+      linePM = synthese.service("LinesListFunction2", parameters)
+      lineCodesStr = linePM["line"][0]["creator_id"]
+      lineCodes = map(lambda x: x.split('|'), lineCodesStr.split(','))
+      for lineCode in lineCodes:
+        if lineCode[0] == dataSourceId:
+          childLine = etree.SubElement(childLines, "Line")
+          childLine.text = lineCode[1]
+
+# Print resulting XML to output stream
+print(etree.tostring(root, pretty_print=True, xml_declaration=True, encoding="iso-8859-1"))
---
+++ packages/ineo_terminus_t2c/src/bivlineman.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python2
+
+import datetime
+import re
+import HTMLParser
+import synthese
+
+try:
+  from lxml import etree
+except ImportError:
+  print("la lib lxml n'est pas disponible")
+
+  
+# TODO : pass variable as parameter
+networkId="6192453782601729"
+#dataSourceId="16607027920896001"
+dataSourceId="16607027920896002"
+
+# Request headers
+root = etree.Element("BivLineMan" + type + "MessageRequest")
+childID = etree.SubElement(root, "ID")
+childID.text = ID
+childRequestTimeStamp = etree.SubElement(root, "RequestTimeStamp")
+now = datetime.datetime.now()
+childRequestTimeStamp.text = now.strftime("%d/%m/%Y %X")
+childRequestorRef = etree.SubElement(root, "RequestorRef")
+childRequestorRef.text = "Terminus"
+childMessaging = etree.SubElement(root, "Messaging")
+
+# Name
+# Ineo SAE requires the name to be defined as ‘XXXX aaaaaaaaaaaaaaaaaaaaaaaaaaa’ where 'XXXX' is a unique message id
+childName = etree.SubElement(childMessaging, "Name")
+messageID = int(message[0]["message_id"]) % 10000
+childName.text = "{:04d} {:.27s}".format(messageID, message[0]["title"])
+
+# Tts
+if int(needs_play_tts) != 0:
+  if int(message[0]["play_tts"]) != 0:
+    childTts = etree.SubElement(childMessaging, "Tts")
+    childTts.text = message[0]["content"]
+
+# {Start,Stop}Date
+childStartDate = etree.SubElement(childMessaging, "StartDate")
+childStartDate.text = "01/01/1970"
+childStopDate = etree.SubElement(childMessaging, "StopDate")
+childStopDate.text = "31/12/2037"
+
+# {Start,Stop}Time
+
+# Alternance
+
+# Text
+# Split text around <br /> and \n
+childText = etree.SubElement(childMessaging, "Text")
+for line in re.split('<br />|\n',message[0]["content"]):
+  h = HTMLParser.HTMLParser()
+  childLine = etree.SubElement(childText, "Line")
+  childLine.text = h.unescape(line)
+
+# Recipients
+childRecipients = etree.SubElement(childMessaging, "Recipients")
+recipients = message[0]["recipients"][0]
+if 'line' in recipients:
+  childLines = etree.SubElement(childRecipients, "Lines")
+  for line in recipients["line"]:
+    if line["id"] != networkId:
+      parameters = { "roid": line["id"] }
+      linePM = synthese.service("LinesListFunction2", parameters)
+      lineCodesStr = linePM["line"][0]["creator_id"]
+      lineCodes = map(lambda x: x.split('|'), lineCodesStr.split(','))
+      for lineCode in lineCodes:
+        if lineCode[0] == dataSourceId:
+          childLine = etree.SubElement(childLines, "Line")
+          childLine.text = lineCode[1]
+
+# Print resulting XML to output stream
+print(etree.tostring(root, pretty_print=True, xml_declaration=True, encoding="iso-8859-1"))
