Open-Transport/synthese

commit 170baaa80d7583430b6aed406221dfbeae1786fa

Merge branch 'tc-84'
Alarms now have a tags field (comma separated values)

server/src/17_messages/ScenarioSaveAction.cpp
@@ -112,6 +112,8 @@ namespace synthese
 		const string ScenarioSaveAction::PARAMETER_MESSAGE_TO_CREATE = Action_PARAMETER_PREFIX + "me";
 		const string ScenarioSaveAction::PARAMETER_ENCODING = Action_PARAMETER_PREFIX + "_encoding";
 		const string ScenarioSaveAction::PARAMETER_LEVEL = Action_PARAMETER_PREFIX + "le";
+		const string ScenarioSaveAction::PARAMETER_TAGS = Action_PARAMETER_PREFIX + "tags";
+
 		const string ScenarioSaveAction::PARAMETER_DISPLAY_DURATION = Action_PARAMETER_PREFIX + "ddur";
 		const string ScenarioSaveAction::PARAMETER_DIGITIZED_VERSION = Action_PARAMETER_PREFIX + "dv";
 		const string ScenarioSaveAction::PARAMETER_RECIPIENT_ID = Action_PARAMETER_PREFIX + "re";
@@ -598,6 +600,7 @@ namespace synthese
 						_messageToCreateTitle = iconv.convert(map.get<string>(PARAMETER_CREATED_MESSAGE_TITLE));
 					}
 					_level = static_cast<AlarmLevel>(map.getDefault<int>(PARAMETER_LEVEL, static_cast<int>(ALARM_LEVEL_WARNING)));
+					_tags = map.get<string>(PARAMETER_TAGS);
 					_display_duration = static_cast<size_t>(map.getDefault<int>(PARAMETER_DISPLAY_DURATION));
 					_digitizedVersion = map.getDefault<string>(PARAMETER_DIGITIZED_VERSION);
 					if (map.isDefined(PARAMETER_MESSAGE_SECTION))
@@ -1051,6 +1054,10 @@ namespace synthese
 						message->setLongMessage(messageNode.second.get("content", string()));
 						message->setDisplayDuration(messageNode.second.get("displayDuration", 0));
 						message->setDigitizedVersion(messageNode.second.get("digitized_version", string()));
+						std::string tagsString = messageNode.second.get("tags", string());
+						std::set<string> tags;
+						boost::algorithm::split(tags, tagsString, is_any_of(", "), token_compress_on );
+						message->setTags(tags);
 						BOOST_FOREACH(const ptree::value_type& sectionNode, messageNode.second.get_child("section"))
 						{
 							RegistryKeyType sectionId(sectionNode.second.get("id", RegistryKeyType(0)));
@@ -1230,6 +1237,9 @@ namespace synthese
 				message->setShortMessage(_messageToCreateTitle ? *_messageToCreateTitle : "Unique message");
 				message->setLongMessage(*_messageToCreate);
 				message->setLevel(*_level);
+				std::set<string> tags;
+				boost::algorithm::split(tags, _tags, is_any_of(", "), token_compress_on );
+				message->setTags(tags);
 				if (_display_duration) message->setDisplayDuration(*_display_duration);
 				message->setDigitizedVersion(_digitizedVersion);
 				message->setSection(_messageSection.get());

packages/terminus/src/functions/diffusion
@@ -305,6 +305,13 @@ template=
             </select>
           </label>
         </div>
+        <div class="row-fluid">
+          <div class="span12">
+            <label>Tags<br />
+              <input type="text" field="tags" onkeyup="activateForm()" onpaste="activateForm()" oncut="activateForm()">
+            </label>
+          </div>
+        </div>
         </div><div class="row-fluid">
         <label>Durée d'affichage
           <input class="input-mini" value="" type="number" maxlength="3" field="displayDuration" min="1" max="100" onkeyup="activateForm()" onpaste="activateForm()" oncut="activateForm()">
@@ -685,6 +692,7 @@ template=
     title: "<@title@>",
     content: "<@content_c@>",
     level: "<@priority@>",
+    tags: "<@tags@>",
     displayDuration :<?if&cond=<@display_duration@>&then="<@display_duration@>"&else=""?>,
     digitized_version :"<@digitized_version@>",
     alternative:[

packages/terminus/src/js/event.js
@@ -481,6 +481,7 @@ function open_message(message)
   // When opening a message, format its content, i.e. \" becomes "
   tinyMCE.get('tinymce').setContent(message.content.replace(/\\\"/g,"\""));
   $('#message select[field=level]').val(message.level);
+  $('#message input[field=tags]').val(message.tags);
   $('#message [field=displayDuration]').val(message.displayDuration);
   if (message.digitized_version != "")
   {
@@ -556,6 +557,7 @@ function close_message()
     // when closing a message, format its content to make it SYNTHESE registrabled, i.e. " becomes \"
     current_message.content = tinyMCE.get('tinymce').getContent().replace(/\"/g,"\\\"");
     current_message.level = $('#message select[field=level]').val();
+    current_message.tags = $('#message input[field=tags]').val();
     current_message.displayDuration = $('#message [field=displayDuration]').val();
     if ($('#message [field=digitized_version]').val() == $('#message input[field=def_digital_msg_url]').val())
     {
@@ -601,6 +603,7 @@ function copy_message_click()
    title:"Copie de "+ message.title,
    content: message.content,
    level: message.level,
+   tags: message.tags,
    displayDuration: message.displayDuration,
    digitized_version: message.digitized_version,
    alternative:[],
@@ -675,6 +678,7 @@ function create_message_click()
    title:"",
    content:"",
    level:10,
+   tags:"",
    displayDuration:"",
    digitized_version: $('#message input[field=def_digital_msg_url]').val() != undefined ? $('#message input[field=def_digital_msg_url]').val() : "",
    alternative:[],

server/src/17_messages/Alarm.cpp
@@ -56,6 +56,7 @@ namespace synthese
 		const string Alarm::DATA_DONE = "done";
 		const string Alarm::DATA_DISPLAY_DURATION("display_duration");
 		const string Alarm::DATA_DIGITIZED_VERSION("digitized_version");
+		const string Alarm::DATA_TAGS("tags");
 
 		const string Alarm::TAG_MESSAGE_ALTERNATIVE = "message_alternative";
 		const string Alarm::TAG_RECIPIENTS = "recipients";
@@ -93,7 +94,8 @@ namespace synthese
 			_section(source._section),
 			_calendar(source._calendar),
 			_displayDuration(source._displayDuration),
-			_digitizedVersion(source._digitizedVersion)
+			_digitizedVersion(source._digitizedVersion),
+			_tags(source._tags)
 		{}
 
 
@@ -112,7 +114,8 @@ namespace synthese
 			_section(source._section),
 			_calendar(calendar),
 			_displayDuration(source._displayDuration),
-			_digitizedVersion(source._digitizedVersion)
+			_digitizedVersion(source._digitizedVersion),
+			_tags(source._tags)
 		{}
 
 
@@ -136,6 +139,10 @@ namespace synthese
 			pm.insert(prefix + DATA_PRIORITY, static_cast<int>(getLevel()));
 			pm.insert(prefix + DATA_DISPLAY_DURATION, getDisplayDuration());
 			pm.insert(prefix + DATA_DIGITIZED_VERSION, getDigitizedVersion());
+
+			string tagsString = boost::algorithm::join(_tags, ", ");
+			pm.insert(prefix + DATA_TAGS, tagsString);
+
 			if(withScenario && getScenario())
 			{
 				pm.insert(prefix + DATA_SCENARIO_ID, getScenario()->getKey());

server/src/17_messages/Alarm.h
@@ -67,6 +67,7 @@ namespace synthese
 			static const std::string DATA_DONE;
 			static const std::string DATA_DISPLAY_DURATION;
 			static const std::string DATA_DIGITIZED_VERSION;
+			static const std::string DATA_TAGS;
 
 			static const std::string TAG_MESSAGE_ALTERNATIVE;
 			static const std::string TAG_RECIPIENTS;
@@ -97,6 +98,7 @@ namespace synthese
 			const ScenarioCalendar* _calendar;
 			std::size_t			_displayDuration;
 			std::string			_digitizedVersion;
+			std::set<std::string> _tags;
 
 			// Links
 			//@{
@@ -137,6 +139,7 @@ namespace synthese
 				const ScenarioCalendar* getCalendar() const { return _calendar; }
 				std::size_t				getDisplayDuration() const { return _displayDuration;}
 				const std::string&		getDigitizedVersion()	const { return _digitizedVersion; }
+				const std::set<std::string>& getTags() const { return _tags; }
 			//@}
 
 			//! @name Setters
@@ -154,6 +157,7 @@ namespace synthese
 				void setCalendar(const ScenarioCalendar* value){ _calendar = value; }
 				void setDisplayDuration(std::size_t value){ _displayDuration = value; }
 				void setDigitizedVersion( const std::string& value){ _digitizedVersion = value; }
+				void setTags(const std::set<std::string> tags) { _tags = tags; }
 			//@}
 
 			//! @name Services

server/src/17_messages/AlarmTableSync.cpp
@@ -80,6 +80,7 @@ namespace synthese
 		const string AlarmTableSync::COL_DATASOURCE_LINKS = "datasource_links";
 		const string AlarmTableSync::COL_DISPLAY_DURATION = "display_duration";
 		const string AlarmTableSync::COL_DIGITIZED_VERSION = "digitized_version";
+		const string AlarmTableSync::COL_TAGS = "tags";
 	}
 
 	namespace db
@@ -104,6 +105,7 @@ namespace synthese
 			Field(AlarmTableSync::COL_DATASOURCE_LINKS, SQL_TEXT),
 			Field(AlarmTableSync::COL_DISPLAY_DURATION, SQL_INTEGER),
 			Field(AlarmTableSync::COL_DIGITIZED_VERSION, SQL_TEXT),
+			Field(AlarmTableSync::COL_TAGS, SQL_TEXT),
 			Field()
 		};
 
@@ -145,6 +147,12 @@ namespace synthese
 			alarm->setDone(rows->getBool(AlarmTableSync::COL_DONE));
 			alarm->setDisplayDuration(rows->getInt (AlarmTableSync::COL_DISPLAY_DURATION));
 			alarm->setDigitizedVersion(rows->getText(AlarmTableSync::COL_DIGITIZED_VERSION));
+
+			std::string tagsString(rows->getText(AlarmTableSync::COL_TAGS));
+			std::set<string> tags;
+			boost::algorithm::split(tags, tagsString, is_any_of(", "), token_compress_on );
+			alarm->setTags(tags);
+
 			// Section
 			if(linkLevel > FIELDS_ONLY_LOAD_LEVEL)
 			{
@@ -304,6 +312,10 @@ namespace synthese
 			)	);
 			query.addField(object->getDisplayDuration());
 			query.addField(object->getDigitizedVersion());
+
+			string tagsString = boost::algorithm::join(object->getTags(), ", ");
+			query.addField(tagsString);
+
 			query.execute(transaction);
 		}
 

server/src/17_messages/AlarmTableSync.h
@@ -77,6 +77,7 @@ namespace synthese
 			static const std::string COL_DATASOURCE_LINKS;
 			static const std::string COL_DISPLAY_DURATION;
 			static const std::string COL_DIGITIZED_VERSION;
+			static const std::string COL_TAGS;
 
 			AlarmTableSync() {}
 			~AlarmTableSync() {}

server/src/17_messages/ScenarioSaveAction.h
@@ -96,6 +96,7 @@ namespace synthese
 				static const std::string PARAMETER_CREATED_MESSAGE_TITLE;
 				static const std::string PARAMETER_RECIPIENT_ID;
 				static const std::string PARAMETER_LEVEL;
+				static const std::string PARAMETER_TAGS;
 				static const std::string PARAMETER_DISPLAY_DURATION;
 				static const std::string PARAMETER_DIGITIZED_VERSION;
 				static const std::string PARAMETER_RECIPIENT_DATASOURCE_ID;
@@ -154,6 +155,7 @@ namespace synthese
 				Recipients _recipients;
 				std::string											_recipientType;
 				boost::optional<AlarmLevel>							_level;
+				std::string											_tags;
 				boost::optional<std::size_t>						_display_duration;
 				std::string											_digitizedVersion;
 				boost::shared_ptr<const MessagesSection>			_messageSection;

commit 37896e8726f0ff728c3e46843216f719f197716a

fixed regression introduced in 170baaa80d7583430b6aed406221dfbeae1786fa

server/src/17_messages/ScenarioSaveAction.cpp
@@ -600,7 +600,7 @@ namespace synthese
 						_messageToCreateTitle = iconv.convert(map.get<string>(PARAMETER_CREATED_MESSAGE_TITLE));
 					}
 					_level = static_cast<AlarmLevel>(map.getDefault<int>(PARAMETER_LEVEL, static_cast<int>(ALARM_LEVEL_WARNING)));
-					_tags = map.get<string>(PARAMETER_TAGS);
+					_tags = map.getDefault<string>(PARAMETER_TAGS);
 					_display_duration = static_cast<size_t>(map.getDefault<int>(PARAMETER_DISPLAY_DURATION));
 					_digitizedVersion = map.getDefault<string>(PARAMETER_DIGITIZED_VERSION);
 					if (map.isDefined(PARAMETER_MESSAGE_SECTION))