f406b58a1b4965d72f8180bc51df468291d22df9
Olivier Vernhet <vernhet.olivier@gmail.com>
2015-08-05 15:08:21 +0200

    Feature : Configuration for Ineo fake broadcast points * added a subdirectory to gather those fake broadcast points

--- packages/ineo_terminus_t2c/notification_providers/fake_broadcast_points.sql
+++ packages/ineo_terminus_t2c/notification_providers/fake_broadcast_points.sql
@@ -1,11 +1,12 @@
-INSERT INTO t106_custom_broadcast_points VALUES ('29836416250806273', '0', '0', '0', 'Bandeaux passager origine Ineo', '0', '');
-INSERT INTO t106_custom_broadcast_points VALUES ('29836416250806274', '0', '0', '0', 'Pupitres conducteur origine Ineo', '0', '');
-INSERT INTO t106_custom_broadcast_points VALUES ('29836416250806275', '0', '0', '0', 'Pupitres prise de service conducteur origine Ineo', '0', '');
-INSERT INTO t106_custom_broadcast_points VALUES ('29836416250806276', '0', '0', '0', 'Girouettes Ineo', '0', '');
-INSERT INTO t106_custom_broadcast_points VALUES ('29836416250806277', '0', '0', '0', 'Annonces sonores passagers origine Ineo', '0', '');
-INSERT INTO t106_custom_broadcast_points VALUES ('29836416250806278', '0', '0', '0', 'Message sonore conducteur origine Ineo', '0', '');
-INSERT INTO t106_custom_broadcast_points VALUES ('29836416250806279', '0', '0', '0', 'Annonces sonores en station origine Ineo', '0', '');
-INSERT INTO t106_custom_broadcast_points VALUES ('29836416250806280', '0', '0', '0', 'Bornes IV Inéo, zone des messages origine Ineo', '0', '');
-INSERT INTO t106_custom_broadcast_points VALUES ('29836416250806281', '0', '0', '0', 'Bornes IV Inéo, zone des horaires origine Ineo', '0', '');
-INSERT INTO t106_custom_broadcast_points VALUES ('29836416250806282', '0', '0', '0', 'Message automatique de ligne origine Ineo', '0', '');
+INSERT INTO t106_custom_broadcast_points VALUES ('29836416250806273', '0', '0', '0', 'Faux diffuseurs Ineo', '0', '');
+INSERT INTO t106_custom_broadcast_points VALUES ('29836416250806274', '0', '29836416250806273', '0', 'Bandeaux passager origine Ineo', '0', '');
+INSERT INTO t106_custom_broadcast_points VALUES ('29836416250806275', '0', '29836416250806273', '0', 'Pupitres conducteur origine Ineo', '0', '');
+INSERT INTO t106_custom_broadcast_points VALUES ('29836416250806276', '0', '29836416250806273', '0', 'Pupitres prise de service conducteur origine Ineo', '0', '');
+INSERT INTO t106_custom_broadcast_points VALUES ('29836416250806277', '0', '29836416250806273', '0', 'Girouettes Ineo', '0', '');
+INSERT INTO t106_custom_broadcast_points VALUES ('29836416250806278', '0', '29836416250806273', '0', 'Annonces sonores passagers origine Ineo', '0', '');
+INSERT INTO t106_custom_broadcast_points VALUES ('29836416250806279', '0', '29836416250806273', '0', 'Message sonore conducteur origine Ineo', '0', '');
+INSERT INTO t106_custom_broadcast_points VALUES ('29836416250806280', '0', '29836416250806273', '0', 'Annonces sonores en station origine Ineo', '0', '');
+INSERT INTO t106_custom_broadcast_points VALUES ('29836416250806281', '0', '29836416250806273', '0', 'Bornes IV Inéo, zone des messages origine Ineo', '0', '');
+INSERT INTO t106_custom_broadcast_points VALUES ('29836416250806282', '0', '29836416250806273', '0', 'Bornes IV Inéo, zone des horaires origine Ineo', '0', '');
+INSERT INTO t106_custom_broadcast_points VALUES ('29836416250806283', '0', '29836416250806273', '0', 'Message automatique de ligne origine Ineo', '0', '');
 
