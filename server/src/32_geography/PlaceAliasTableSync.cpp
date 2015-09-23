eb949e1c55d79327608f5902986961c495972c70
chue <chue@rcsmobility.com>
2015-09-23 09:25:44 +0200

    Feature tc-283 : Refactoring of PublicPlace using boost fusion

--- server/src/32_geography/PlaceAliasTableSync.cpp
+++ server/src/32_geography/PlaceAliasTableSync.cpp
@@ -48,14 +48,6 @@ namespace synthese
 		template<> const string FactorableTemplate<DBTableSync, PlaceAliasTableSync>::FACTORY_KEY("32.10.01 Places Alias");
 	}
 
-	namespace geography
-	{
-		const string PlaceAliasTableSync::COL_NAME ("name");
-		const string PlaceAliasTableSync::COL_ALIASEDPLACEID ("aliased_place_id");
-		const string PlaceAliasTableSync::COL_CITYID ("city_id");
-		const string PlaceAliasTableSync::COL_ISCITYMAINCONNECTION ("is_city_main_connection");
-	}
-
 	namespace db
 	{
 		template<> const DBTableSync::Format DBTableSyncTemplate<PlaceAliasTableSync>::TABLE(
@@ -64,11 +56,6 @@ namespace synthese
 
 		template<> const Field DBTableSyncTemplate<PlaceAliasTableSync>::_FIELDS[]=
 		{
-			Field(TABLE_COL_ID, SQL_INTEGER),
-			Field(PlaceAliasTableSync::COL_NAME, SQL_TEXT),
-			Field(PlaceAliasTableSync::COL_ALIASEDPLACEID, SQL_INTEGER),
-			Field(PlaceAliasTableSync::COL_CITYID, SQL_INTEGER),
-			Field(PlaceAliasTableSync::COL_ISCITYMAINCONNECTION, SQL_BOOLEAN),
 			Field()
 		};
 
@@ -76,90 +63,6 @@ namespace synthese
 		DBTableSync::Indexes DBTableSyncTemplate<PlaceAliasTableSync>::GetIndexes()
 		{
 			return DBTableSync::Indexes();
-		}
-
-
-
-		template<>
-		void OldLoadSavePolicy<PlaceAliasTableSync,PlaceAlias>::Load(
-			PlaceAlias* obj,
-			const db::DBResultSPtr& rows,
-			Env& env,
-			LinkLevel linkLevel
-		){
-			obj->setName (rows->getText (PlaceAliasTableSync::COL_NAME));
-
-			if (linkLevel > FIELDS_ONLY_LOAD_LEVEL)
-			{
-				// Because the fetch place has no equivalent in database read mode
-//				assert(temporary == GET_REGISTRY);
-
-				RegistryKeyType aliasedPlaceId (
-					rows->getLongLong (PlaceAliasTableSync::COL_ALIASEDPLACEID));
-				RegistryKeyType cityId (
-					rows->getLongLong (PlaceAliasTableSync::COL_CITYID));
-				City* city(CityTableSync::GetEditable(cityId, env, linkLevel).get());
-
-				obj->setCity(city);
-				obj->setAliasedPlace(Fetcher<NamedPlace>::Fetch(aliasedPlaceId, env, linkLevel).get());
-
-
-				bool isCityMainConnection (rows->getBool ( PlaceAliasTableSync::COL_ISCITYMAINCONNECTION));
-				if (isCityMainConnection)
-				{
-					city->addIncludedPlace(*obj);
-				}
-				city->addPlaceToMatcher(env.getEditableSPtr(obj));
-
-				// Registration to all places matcher
-				if(	&env == &Env::GetOfficialEnv() &&
-					obj->getCity() &&
-					linkLevel == ALGORITHMS_OPTIMIZATION_LOAD_LEVEL
-				){
-					GeographyModule::GetGeneralAllPlacesMatcher().add(
-						obj->getFullName(),
-						env.getEditableSPtr(obj)
-					);
-				}
-
-				if(	&env == &Env::GetOfficialEnv() &&
-					obj->getCity() &&
-					linkLevel == ALGORITHMS_OPTIMIZATION_LOAD_LEVEL &&
-					dynamic_cast<const pt::StopArea*>(obj->getAliasedPlace())
-				){
-					const pt::StopArea* stopArea = static_cast<const pt::StopArea*>(obj->getAliasedPlace());
-					pt::PTModule::GetGeneralStopsMatcher().add(
-						obj->getFullName(),
-						env.getEditableSPtr(const_cast<pt::StopArea*>(stopArea))
-					);
-				}
-			}
-		}
-
-		template<>
-		void OldLoadSavePolicy<PlaceAliasTableSync,PlaceAlias>::Unlink(
-			PlaceAlias* obj
-		){
-			City* city(const_cast<City*>(obj->getCity()));
-			if (city != NULL)
-			{
-				city->removePlaceFromMatcher(*obj);
-				obj->setCity(NULL);
-				city->removeIncludedPlace(*obj);
-			}
-		}
-
-		template<>
-		void OldLoadSavePolicy<PlaceAliasTableSync,PlaceAlias>::Save(
-			PlaceAlias* object,
-			optional<DBTransaction&> transaction
-		){
-			ReplaceQuery<PlaceAliasTableSync> query(*object);
-			query.addField(object->getName());
-			query.addField(object->getAliasedPlace() ? object->getAliasedPlace()->getKey() : RegistryKeyType(0));
-			query.addField(object->getCity() ? object->getCity()->getKey() : RegistryKeyType(0));
-			query.addField(object->getCity() ? object->getCity()->includes(*object) : false);
-			query.execute(transaction);
 		}
 
 
@@ -209,7 +112,7 @@ namespace synthese
 			SelectQuery<PlaceAliasTableSync> query;
 			if(aliasedPlaceId)
 			{
-				query.addWhereField(COL_ALIASEDPLACEID, *aliasedPlaceId);
+				query.addWhereField(AliasedPlaceId::FIELD.name, *aliasedPlaceId);
 			}
 			if (number)
 			{
