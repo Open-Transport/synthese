e14a7e8ebd2526fcf2696715dcc68adc9b282618
Camille Hue <chue@rcsmobility.com>
2015-12-03 11:11:25 +0100

    Bugfix for feature smile-2 : Use of instance coordinate system to create geometries. Set projected point to write in database attributes

--- server/src/35_pt/StopPoint.cpp
+++ server/src/35_pt/StopPoint.cpp
@@ -541,7 +541,7 @@ namespace synthese
 				if ((_projectedPoint.getRoadChunk() != get<ProjectedRoadChunk>().get_ptr()) ||
 					(_projectedPoint.getMetricOffset() != get<ProjectedMetricOffset>()))
 				{
-					setProjectedPoint(Address(get<ProjectedRoadChunk>().get(), get<ProjectedMetricOffset>()));
+					_projectedPoint = Address(get<ProjectedRoadChunk>().get(), get<ProjectedMetricOffset>());
 				}
 			}
 			else
@@ -607,6 +607,16 @@ namespace synthese
 		void StopPoint::setName(const std::string& value)
 		{
 			set<Name>(value);
+		}
+
+		void StopPoint::setProjectedPoint(const road::Address& value)
+		{
+			_projectedPoint = value;
+			set<ProjectedRoadChunk>(value.getRoadChunk()
+				? boost::optional<road::RoadChunk&>(*const_cast<road::RoadChunk*>(value.getRoadChunk()))
+				: boost::none
+			);
+			set<ProjectedMetricOffset>(value.getMetricOffset());
 		}
 
 
