cc17a6bd171e9cdf4169745e884670e0375cb074
Camille Hue <chue@rcsmobility.com>
2015-12-04 14:35:59 +0100

    Change index of schedules for TRAVYS

--- server/src/61_data_exchange/HeuresFileFormat.cpp
+++ server/src/61_data_exchange/HeuresFileFormat.cpp
@@ -637,13 +637,13 @@ namespace synthese
 
 							const DeadRunRoute& route(it->second);
 							time_duration departureSchedule(
-								lexical_cast<int>(line.substr(i+15, 2)),
-								lexical_cast<int>(line.substr(i+17, 2)),
+								lexical_cast<int>(line.substr(i+10, 2)),
+								lexical_cast<int>(line.substr(i+12, 2)),
 								0
 							);
 							time_duration arrivalSchedule(
-								lexical_cast<int>(line.substr(i+19, 2)),
-								lexical_cast<int>(line.substr(i+21, 2)),
+								lexical_cast<int>(line.substr(i+14, 2)),
+								lexical_cast<int>(line.substr(i+16, 2)),
 								0
 							);
 
@@ -743,7 +743,7 @@ namespace synthese
 							}
 							JourneyPattern* route(it->second);
 
-							string serviceNumber(trim_copy(line.substr(i+8,3)));
+							string serviceNumber(trim_copy(line.substr(i+6,3)));
 							SchedulesMap::iterator itS(services.find(make_pair(route, serviceNumber)));
 							if(itS != services.end())
 							{
@@ -786,7 +786,7 @@ namespace synthese
 							size_t rank(0);
 							bool alreadyNonNull(false);
 							bool alreadyNull(false);
-							for(i+=11; i<line.size() && line[i]!=';'; i+=8, ++rank)
+							for(i+=6; i<line.size() && line[i]!=';'; i+=8, ++rank)
 							{
 								string arrivalSchedule(line.substr(i, 4));
 								string departureSchedule(line.substr(i+4, 4));
