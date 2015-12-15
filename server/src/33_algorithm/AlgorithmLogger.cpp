1600b84216967aee4b5f9b5a8c718fdeb695ed99
Marc Jambert <mjambert@gmail.com>
2015-12-15 14:28:57 +0100

    added column titles on journey planner log file

--- server/src/33_algorithm/AlgorithmLogger.cpp
+++ server/src/33_algorithm/AlgorithmLogger.cpp
@@ -306,6 +306,12 @@ namespace synthese
 			_journeyPlannerFile = _openNewFile();
 			*_journeyPlannerFile << "<html><head><title>Journey Planner File</title></head><body>";
 			*_journeyPlannerFile << _journeyPlannerTable.open();
+			*_journeyPlannerFile << _journeyPlannerTable.row();
+			*_journeyPlannerFile << _journeyPlannerTable.col() << "rank" << _journeyPlannerTable.col() << "place" << _journeyPlannerTable.col()
+				<< "time" << _journeyPlannerTable.col() << "jyscore" << _journeyPlannerTable.col() << "dist" 
+				<< _journeyPlannerTable.col() << "minsp / dst" << _journeyPlannerTable.col() << "plscore" << _journeyPlannerTable.col() 
+				<< "size" << _journeyPlannerTable.col() << "cleaned" << _journeyPlannerTable.col() << "updated" << _journeyPlannerTable.col()
+				<< "new" << _journeyPlannerTable.col() << "result" << _journeyPlannerTable.col() << "duration";
 
 			if(_timeSlotJourneyPlannerFile)
 			{
