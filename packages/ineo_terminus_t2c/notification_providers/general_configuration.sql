fac4107d1e4db69576b53bb9142703f2845c4de3
Olivier Vernhet <vernhet.olivier@gmail.com>
2016-01-25 17:27:28 +0100

    Ineo-Terminus : SQL scripts for parameters of table t999_config (cannot be saved as intersynthese package)

---
+++ packages/ineo_terminus_t2c/notification_providers/general_configuration.sql
@@ -0,0 +1,16 @@
+INSERT INTO t999_config VALUES ('ineo_terminus_port', '4563');
+INSERT INTO t999_config VALUES ('ineo_terminus_tick_interval', '1');
+INSERT INTO t999_config VALUES ('ineo_terminus_network', '6192453782601729');
+INSERT INTO t999_config VALUES ('ineo_terminus_datasource', '16607027920896001');
+INSERT INTO t999_config VALUES ('ineo_terminus_xsd_location', 'http://schemas.open-transport.org/ineo-terminus/types-t2c.xsd');
+INSERT INTO t999_config VALUES ('ineo_terminus_passenger_fake_broadcast', '29839207979548674');
+INSERT INTO t999_config VALUES ('ineo_terminus_driver_fake_broadcast', '29839207979548675');
+INSERT INTO t999_config VALUES ('ineo_terminus_ppds_fake_broadcast', '29839207979548676');
+INSERT INTO t999_config VALUES ('ineo_terminus_girouette_fake_broadcast', '29839207979548677');
+INSERT INTO t999_config VALUES ('ineo_terminus_sonopassenger_fake_broadcast', '29839207979548678');
+INSERT INTO t999_config VALUES ('ineo_terminus_sonodriver_fake_broadcast', '29839207979548679');
+INSERT INTO t999_config VALUES ('ineo_terminus_sonostoppoint_fake_broadcast', '29839207979548680');
+INSERT INTO t999_config VALUES ('ineo_terminus_bivgeneral_fake_broadcast', '29839207979548681');
+INSERT INTO t999_config VALUES ('ineo_terminus_bivlineman_fake_broadcast', '29839207979548682');
+INSERT INTO t999_config VALUES ('ineo_terminus_bivlineauto_fake_broadcast', '29839207979548683');
+
